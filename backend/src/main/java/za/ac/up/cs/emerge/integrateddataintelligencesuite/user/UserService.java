9ca4caa0406fe1bd7069fee3a2063b0f1511a569
Shrey Mandalia <shreymandalia@gmail.com>
2021-06-01 21:06:06 +0200

    created empty User Service and login request and response classes

---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/user/UserService.java
@@ -0,0 +1,4 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.user;
+
+public interface UserService {
+}
---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/user/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.user;
+
+public class UserServiceImpl implements UserService{
+}
