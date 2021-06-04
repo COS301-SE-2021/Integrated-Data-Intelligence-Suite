35a02c24718f004b85839c699178ebf71d584eb9
shrey-99 <shreymandalia@gmail.com>
2021-06-04 23:46:58 +0200

    Created ParseImportedDataRequest constructor; accepts DataSource and JSON string

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/request/ParseImportedDataRequest.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/request/ParseImportedDataRequest.java
@@ -1,4 +1,13 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.request;
 
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.importing.DataSource;
+
 public class ParseImportedDataRequest {
+    DataSource type;
+    String jsonString;
+
+    public ParseImportedDataRequest(DataSource type, String jsonString) {
+        this.type = type;
+        this.jsonString = jsonString;
+    }
 }
