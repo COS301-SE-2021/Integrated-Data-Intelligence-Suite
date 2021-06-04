5c54438d0ee7f2288626f29582dd0e14c6d1127d
shrey-99 <shreymandalia@gmail.com>
2021-06-05 00:19:04 +0200

    skeleton for parseImportedData use case

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/ParsingService.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/ParsingService.java
@@ -1,5 +1,10 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing;
 
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.request.ParseImportedDataRequest;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.response.ParseImportedDataResponse;
+
 public interface ParsingService {
+
+    ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request);
 
 }
--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/ParsingServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/ParsingServiceImpl.java
@@ -1,5 +1,12 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing;
 
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.request.ParseImportedDataRequest;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.response.ParseImportedDataResponse;
+
 public class ParsingServiceImpl implements ParsingService{
 
+    @Override
+    public ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) {
+        return null;
+    }
 }
