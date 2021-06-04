174e520da4a4789bf8033110c4f2d0b63bf1ff29
shrey-99 <shreymandalia@gmail.com>
2021-06-04 23:33:33 +0200

    created Parser and TwitterParser for strategy pattern

---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/rri/Parser.java
@@ -0,0 +1,5 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.rri;
+
+public interface Parser {
+    public void parse();
+}
---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parsing/rri/TwitterParser.java
@@ -0,0 +1,8 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.parsing.rri;
+
+public class TwitterParser implements Parser{
+
+    public void parse() {
+
+    }
+}
