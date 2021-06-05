0de4c7d7335dbf16ae677996592c7edbb5380dc9
Steve Mbuguje <u18008390@tuks.co.za>
2021-06-05 20:55:17 +0200

    added exceptions to implementation of ParseImportedData function

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingServiceImpl.java
@@ -3,6 +3,8 @@ package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.ParseImportedDataRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.ParseImportedDataResponse;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.importer.DataSource;
+import java.util.*;
 
 public class ParsingServiceImpl implements ParsingService{
 
@@ -12,8 +14,19 @@ public class ParsingServiceImpl implements ParsingService{
             throw new InvalidRequestException("ParseImportedDataRequest Object is null");
         }
 
+        if (request.getJsonString() == null){
+            throw new InvalidRequestException("Imported string is null");
+        }
+
+        if (request.getType() == null){
+            throw new InvalidRequestException("Imported type is null");
+        }
+
+        if (request.getType() == DataSource.TWITTER){
+
+        }
 
 
-        return null;
+        return new ParseImportedDataResponse();
     }
 }
