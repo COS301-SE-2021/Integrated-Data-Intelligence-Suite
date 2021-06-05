05603e7db9035a0f77fce38a6540ed374754c64a
Steve Mbuguje <u18008390@tuks.co.za>
2021-06-05 20:39:53 +0200

    implementation of ParseImportedData function

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingService.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingService.java
@@ -1,10 +1,10 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser;
 
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.ParseImportedDataRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.ParseImportedDataResponse;
 
 public interface ParsingService {
-
-    ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request);
+    ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) throws InvalidRequestException;
 
 }
--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingServiceImpl.java
@@ -1,12 +1,19 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser;
 
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.ParseImportedDataRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.ParseImportedDataResponse;
 
 public class ParsingServiceImpl implements ParsingService{
 
     @Override
-    public ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) {
+    public ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) throws InvalidRequestException {
+        if (request == null) {
+            throw new InvalidRequestException("ParseImportedDataRequest Object is null");
+        }
+
+
+
         return null;
     }
 }
