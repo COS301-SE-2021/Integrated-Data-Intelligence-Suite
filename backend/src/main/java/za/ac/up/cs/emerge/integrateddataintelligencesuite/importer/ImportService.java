b6642c3858f5ed9197316c7e8f7d0bb9e8225d43
wandile <u15219900@tuks.co.za>
2021-06-05 15:49:26 +0200

    added ImportService and ImportServiceImpl classes

---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/importer/ImportService.java
@@ -0,0 +1,4 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.importer;
+
+public interface ImportService {
+}
---
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/importer/ImportServiceImpl.java
@@ -0,0 +1,4 @@
+package za.ac.up.cs.emerge.integrateddataintelligencesuite.importer;
+
+public class ImportServiceImpl implements ImportService{
+}
