179fe46c6f86936526737f729f91ab3f0b2d2136
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-05 22:38:08 +0200

    Edited NodeData class

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/dataclass/NodeData.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/dataclass/NodeData.java
@@ -2,9 +2,36 @@ package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.dataclass;
 
 public class NodeData {
 
-    public NodeData node;
+    private String textMessage;
+    private String[] mention;
+    private String date;
 
-    public NodeData(NodeData node) {
-        this.node = node;
+    public NodeData() {
+        
+    }
+
+
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    public String[] getMention() {
+        return mention;
+    }
+
+    public void setMention(String[] mention) {
+        this.mention = mention;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
