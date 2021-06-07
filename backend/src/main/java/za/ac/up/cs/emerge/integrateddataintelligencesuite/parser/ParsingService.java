7840418e0161defbffe0bacdf5c18f391b89cfd0
abDivergent <u15219900@tuks.co.za>
2021-06-07 03:22:51 +0200

    [Working] integrated NL and Controller

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingService.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/ParsingService.java
@@ -1,11 +1,11 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
+
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.ParseImportedDataRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.ParseImportedDataResponse;
 
 public interface ParsingService {
-    ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) throws InvalidRequestException, JSONException;
+    ParseImportedDataResponse parseImportedData(ParseImportedDataRequest request) throws InvalidRequestException;
 
 }
