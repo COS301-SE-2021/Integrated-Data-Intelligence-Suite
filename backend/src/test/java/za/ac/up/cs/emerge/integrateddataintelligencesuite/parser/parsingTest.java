9a96e534c2f831837d103581e79516b894ddee52
Steve Mbuguje <u18008390@tuks.co.za>
2021-06-07 02:00:53 +0200

    created unit test for null request object for parseImported data function

--- backend/src/test/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/parsingTest.java
+++ backend/src/test/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/parsingTest.java
@@ -1,4 +1,32 @@
 package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.rri.TwitterExtractor;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+//@RunWith(MockitoJUnitRunner.class)
 public class parsingTest {
+    private TwitterExtractor twitterMock;
+    private ParsingServiceImpl parsingServiceTest= new ParsingServiceImpl();
+
+    //@Before
+    /*public void setup(){
+        parsingServiceTest = new ParsingServiceImpl();
+        twitterMock = mock(TwitterExtractor.class);
+        parsingServiceTest.t
+    }*/
+
+    @Test
+    public void testParsingImportedDataNUll(){
+        //parsingServiceTest = new ParsingServiceImpl();
+        Assert.assertThrows(InvalidRequestException.class, ()->parsingServiceTest.parseImportedData(null));
+    }
+
 }
