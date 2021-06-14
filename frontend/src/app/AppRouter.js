9c367bf7ba992dc027b0308b4bfba0bb6c907ae4
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-14 04:16:48 +0200

    added more routing.

--- frontend/src/app/AppRouter.js
+++ frontend/src/app/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { Component,Suspense, lazy } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, Link, useLocation } from 'react-router-dom';
 
 import Spinner from '../app/components/Spinner';
 
@@ -21,6 +21,8 @@ const Error500 = lazy(() => import('./error-pages/Error500'));*/
 
 const Login = lazy(() => import('./pages/Login'));
 const Register1 = lazy(() => import('./pages/Register'));
+
+const ValidateLogin = lazy(() => import('./functions/ValidateLogin'));
 
 
 class AppRoutes extends Component {
@@ -46,11 +48,10 @@ class AppRoutes extends Component {
                     <Route path="/error-pages/error-500" component={ Error500 } />
                     */}
 
-
                     <Route path="/pages/Login" component={ Login } />
                     <Route path="/pages/Register" component={ Register1 } />
 
-
+                    <Route path="/functions/ValidateLogin" component={ VLogin} />}
 
 
                     <Redirect to="/dashboard" />
@@ -58,6 +59,50 @@ class AppRoutes extends Component {
             </Suspense>
         );
     }
+}
+
+function VLogin (){
+    //let userName,userPassword;
+    const location = useLocation();
+
+    let userName = location.state.userName;
+    let userPassword = location.state.userPassword;
+
+    //console.log("Checking HHHHH: " +  handle);
+    //const { fromNotifications } = this.props.location.state;
+    /*/fetch(`https://api.twitter.com/user/${handle}`)
+         .then((user) => {
+             this.setState(() => ({ user }))
+         })*/
+    console.log("Checking U-N : " + userName);
+    console.log("Checking U-P : " + userPassword);
+
+    /*React.useEffect(() => {
+
+        //console.log(this)
+        //console.log(this.props.location.state.userName)
+
+        //const { handle } = this.props.match.params;
+
+        userName = location.state.userName;
+        userPassword = location.state.userPassword;
+
+        //console.log("Checking HHHHH: " +  handle);
+        //const { fromNotifications } = this.props.location.state;
+       /*fetch(`https://api.twitter.com/user/${handle}`)
+            .then((user) => {
+                this.setState(() => ({ user }))
+            })*
+        console.log("Checking U-N : " + userName);
+        console.log("Checking U-P : " + userPassword);
+    }, [])*/
+
+
+    return (
+        <ValidateLogin userName={userName} userPassword ={userPassword} />
+    )
+
+
 }
 
 export default AppRoutes;
