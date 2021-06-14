27bd0ee321d814aba10476ee7d05a38fe489ad8a
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-15 01:38:55 +0200

    Added register

--- frontend/src/app/pages/Register.js
+++ frontend/src/app/pages/Register.js
@@ -1,7 +1,24 @@
 import React, { Component }from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/Login.css';
 
 class Register extends  Component{
+
+
+    constructor() {
+        super();
+        this.state = {
+            userName : "",
+            userPassword: "",
+            userPasswordRepeat: ""
+        }
+    }
+
+
+    componentDidMount () {
+        let sbutton = document.getElementById("submitButton");
+        //sbutton.disabled = true;
+    }
 
     render() {
         return (
@@ -9,24 +26,45 @@ class Register extends  Component{
                 <form action="" method="post">
                     <div className="container">
                         <label htmlFor="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required/>
+                        <input type="text" placeholder="Enter Username" id="uname" required/>
 
                         <label htmlFor="uname"><b>Nickname</b></label>
-                        <input type="text" placeholder="Enter Nickname" name="uname" required/>
+                        <input type="text" placeholder="Enter Nickname" id="nname" required/>
 
                         <label htmlFor="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required/>
+                        <input type="password" placeholder="Enter Password" id="upassword" required/>
 
                         <label htmlFor="psw"><b>Password repeat</b></label>
-                        <input type="password" placeholder="Re-Enter Password" name="psw" required/>
+                        <input type="password" placeholder="Re-Enter Password" id="upasswordR" required/>
 
-                        <button type="submit">Register</button>
+                        <Link to={{
+                            pathname: "/functions/ValidateRegister",
+                            state: this.state
+                        }}>
+                            <button type="submit"  onClick={() =>{this.checkUserCreds()}} > Register</button>
+                        </Link>
                     </div>
 
                 </form>
             </div>
         );
     }
+
+    checkUserCreds(object) {
+        //console.log(object)
+        console.log(document.getElementById("uname"));
+        console.log(document.getElementById("upassword"));
+
+
+        this.state.userPassword = document.getElementById("upassword").value;
+        this.state.userName = document.getElementById("uname").value;
+        this.state.userPasswordRepeat = document.getElementById("upasswordR").value;
+
+
+        console.log("user name : " + this.state.userName);
+        console.log("user password : " + this.state.userPassword);
+    }
+
 }
 
 export default Register;
