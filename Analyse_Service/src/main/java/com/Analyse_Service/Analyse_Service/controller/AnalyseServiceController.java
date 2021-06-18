92860193873d00d7650471d0253ce970a1eea7f5
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-18 20:25:34 +0200

    Structural edits to Analyse Service, and version change for the applications

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/controller/AnalyseServiceController.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/controller/AnalyseServiceController.java
@@ -6,17 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Analyse")
+@RequestMapping(value = "/Analyse")
 public class AnalyseServiceController {
 
     @Autowired
     private AnalyseServiceImpl service;
 
-    @GetMapping("/")
-    public TweetWithSentiment findSentiment(String line) throws Exception {
+    @GetMapping("/findSentiment")
+    public TweetWithSentiment findSentiment(@RequestParam("line") String line) throws Exception {
         return service.findSentiment(line);
     }
+
+
 }
--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/service/AnalyseService.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/service/AnalyseService.java
@@ -1,14 +1,9 @@
 package com.Analyse_Service.Analyse_Service.service;
 
-
 import com.Analyse_Service.Analyse_Service.dataclass.TweetWithSentiment;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 
-@Service
-@FeignClient(name = "Analyse-Service")
 public interface AnalyseService {
-    TweetWithSentiment findSentiment(String line);
 
-    String toCss(int sentiment);
 }
