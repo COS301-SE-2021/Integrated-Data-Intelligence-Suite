800798c17c1c2e71d9144859e20b661532ff7566
abDivergent <u15219900@tuks.co.za>
2021-06-19 01:59:25 +0200

    Fixed the url implementation on AnalyseService

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/AnalyseService.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/AnalyseService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
-@FeignClient(name = "Analyse-Service" ,  url = "localhost/Analyse:9002" , fallback = AnalyseServiceFallback.class)
+@FeignClient(name = "Analyse-Service" ,  url = "localhost:9002/Analyse" , fallback = AnalyseServiceFallback.class)
 public interface AnalyseService {
     @GetMapping("/findSentiment")
     TweetWithSentiment findSentiment(@RequestParam("line") String line) throws Exception;
