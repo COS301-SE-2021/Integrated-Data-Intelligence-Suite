cef5fabb0efc3245a2d13093f9e314bdd7eafef6
abDivergent <u15219900@tuks.co.za>
2021-06-19 02:08:13 +0200

    Added function to directly communicate with Importer

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceController.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceController.java
@@ -5,17 +5,25 @@ import com.Analyse_Service.Analyse_Service.dataclass.TweetWithSentiment;
 import com.Gateway_Service.Gateway_Service.service.AnalyseService;
 import com.Gateway_Service.Gateway_Service.service.ImportService;
 import com.Gateway_Service.Gateway_Service.service.ParseService;
+
 import com.Import_Service.Import_Service.request.ImportTwitterRequest;
 import com.Import_Service.Import_Service.response.ImportTwitterResponse;
 import com.Parse_Service.Parse_Service.dataclass.DataSource;
 import com.Parse_Service.Parse_Service.request.ParseImportedDataRequest;
 import com.Parse_Service.Parse_Service.response.ParseImportedDataResponse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 
 @RestController
@@ -34,9 +42,29 @@ public class GatewayServiceController {
     @Autowired
     private AnalyseService analyseClient;
 
-    //GatewayServiceController(){ }
 
-    @GetMapping(value = "/{key}", produces = "application/json")
+
+    @GetMapping(value ="/{key}", produces = "application/json")
+    public String nothing(String key){
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:9001/Import/importData")
+                .method("GET", null)
+                .build();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            return Objects.requireNonNull(response.body()).string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  "";
+    }
+
+    @GetMapping(value = "/man/{key}", produces = "application/json")
     //@CrossOrigin
     //@HystrixCommand(fallbackMethod = "fallback")
     public Collection<String> init(@PathVariable String key) throws Exception {
