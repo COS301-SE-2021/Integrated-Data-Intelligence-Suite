7dd2c96747ece9644b3c58d56890d8b4e2ef20b4
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-20 01:00:11 +0200

    Edits to parse service (controller)

--- Parse_Service/src/main/java/com/Parse_Service/Parse_Service/controller/ParseServiceController.java
+++ Parse_Service/src/main/java/com/Parse_Service/Parse_Service/controller/ParseServiceController.java
@@ -7,10 +7,8 @@ import com.Parse_Service.Parse_Service.response.ParseImportedDataResponse;
 import com.Parse_Service.Parse_Service.service.ParseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Parse")
@@ -26,10 +24,11 @@ public class ParseServiceController {
         return service.parseImportedData(req);
     }*/
 
-    @GetMapping("/parseImportedData")
-    public ParseImportedDataResponse parseImportedData(@RequestParam("request") ParseImportedDataRequest request) throws Exception {
+    @PostMapping("/parseImportedData")
+    public ParseImportedDataResponse parseImportedData(RequestEntity<ParseImportedDataRequest> requestEntity) throws Exception {
         //DataSource type = DataSource.TWITTER;
         //ParseImportedDataRequest req = new ParseImportedDataRequest(type,jsonString);
+        ParseImportedDataRequest request = requestEntity.getBody();
         return service.parseImportedData(request);
     }
 
