dcc53f1844cd8c434c2e4e05f785126aa2ada30a
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-20 00:56:49 +0200

    Added response and request object functionally between services

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/GatewayServiceApplication.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/GatewayServiceApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -24,5 +27,16 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getTemplate(){
+		return new RestTemplate();
+	}
+
+	/*@Bean
+	public WebClient.Builder getWebClientBuilde(){
+		return new WebClient.Builder();
+	}*/
 }
 
--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceController.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceController.java
@@ -2,27 +2,42 @@ package com.Gateway_Service.Gateway_Service.controller;
 
 
 import com.Analyse_Service.Analyse_Service.dataclass.TweetWithSentiment;
+import com.Gateway_Service.Gateway_Service.dataclass.*;
+
+
 import com.Gateway_Service.Gateway_Service.service.AnalyseService;
 import com.Gateway_Service.Gateway_Service.service.ImportService;
 import com.Gateway_Service.Gateway_Service.service.ParseService;
 
-import com.Import_Service.Import_Service.request.ImportTwitterRequest;
-import com.Import_Service.Import_Service.response.ImportTwitterResponse;
-import com.Parse_Service.Parse_Service.dataclass.DataSource;
-import com.Parse_Service.Parse_Service.request.ParseImportedDataRequest;
-import com.Parse_Service.Parse_Service.response.ParseImportedDataResponse;
 
+
+
+
+//import com.netflix.discovery.DiscoveryClient;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.net.ssl.SSLContext;
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -44,16 +59,47 @@ public class GatewayServiceController {
 
 
 
-    @GetMapping(value ="/{key}", produces = "application/json")
-    public String nothing(String key){
+    @Autowired
+    private RestTemplate restTemplate;
 
-        OkHttpClient client = new OkHttpClient().newBuilder()
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /*@RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        List<ServiceInstance> Listing = this.discoveryClient.getInstances(applicationName);
+
+        ServiceInstance inst = Listing.get(0);
+        inst.getServiceId();
+        String clientURI = inst.getUri().toString();
+
+        return Listing;
+    }*/
+
+    GatewayServiceController() {
+        //importTemplate = new RestTemplate();
+        //restTemplate.getForObject("http://localhost:9001/Import", FallbackController.class); //http://Import-Service/Import
+        //restTemplate = new RestTemplate();
+    }
+
+    private String getServiceURL(String serviceName){
+        return this.discoveryClient.getInstances(serviceName).get(0).getUri().toString();
+    }
+
+
+    //TEST FUNCTION
+    @GetMapping(value ="/{key}", produces = "application/json")
+    public String nothing(@PathVariable String key) throws NoSuchAlgorithmException {
+
+        /*OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
                 .url("http://localhost:9001/Import/importData")
                 .method("GET", null)
                 .build();
         Response response = null;
+
         try {
             response = client.newCall(request).execute();
             return Objects.requireNonNull(response.body()).string();
@@ -62,50 +108,113 @@ public class GatewayServiceController {
         }
 
         return  "";
+
+
+
+        HttpClientBuilder httpClientBuilder = HttpClients.custom()
+                .setSSLContext(SSLContext.getDefault())
+                .useSystemProperties();
+
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClientBuilder.build()));*/
+
+
+        //String body = "";
+        //System.out.println(getServiceURL("Import-Service") +"/Import/importData");
+        //String body = restTemplate.getForObject("http://Import-Service/Import/importData", String.class);
+
+
+        /*String url = "http://Import-Service/Import/getTwitterDataJson/keyword";
+        ImportTwitterRequest importReq = new ImportTwitterRequest(key,10);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("key",key);
+
+        System.out.println("BUILDER!!!!!!!!!!!!!!!!.................." + builder.toUriString() );
+
+        String Data =  restTemplate.getForObject("http://Import-Service/Import/importData",  String.class);
+
+        System.out.println();
+        System.out.println(Data);*/
+
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        //set up HTTP Basic Authentication Header
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+        //requestHeaders.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        ImportTwitterRequest importReq = new ImportTwitterRequest(key,10);
+        HttpEntity<ImportTwitterRequest> requestEntity =new HttpEntity<>(importReq,requestHeaders);
+
+
+
+        //ImportTwitterResponse ImportRes = restTemplate.getForObject("https://Import-Service/Import/getTwitterDataJson", ImportTwitterResponse.class, importReq);
+        ResponseEntity<ImportTwitterResponse> responseEntity = restTemplate.exchange("http://Import-Service/Import/getTwitterDataJson",  HttpMethod.POST, requestEntity,ImportTwitterResponse.class);
+        //ImportTwitterResponse ImportRes =  restTemplate.getForObject("http://localhost:9001/Import/getTwitterDataJson/keyword",  ImportTwitterResponse.class);
+        ImportTwitterResponse ImportRes = responseEntity.getBody();
+        return ImportRes.getJsonData();
     }
+
 
     @GetMapping(value = "/man/{key}", produces = "application/json")
     //@CrossOrigin
     //@HystrixCommand(fallbackMethod = "fallback")
     public Collection<String> init(@PathVariable String key) throws Exception {
 
-        ArrayList <String> Data = new ArrayList<>();
 
-        ImportTwitterRequest importReq = new ImportTwitterRequest(key,10);
-        ImportTwitterResponse ImportRes = importClient.getTwitterDataJson(importReq);
+        ArrayList <String> outputData = new ArrayList<>();
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        /*********************IMPORT*************************/
+
+        /*ImportDataRequest importReq =  new ImportDataRequest(key,10);
+        ImportDataResponse ImportRes = importClient.importData(importReq)
 
         if (ImportRes == null) {
             Data.add("Import Service Fail");
             return Data;
+        }*/
+
+        //String url = "http://Import-Service/Import/importData";
+        //UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("value",key);
+        ImportDataRequest importRequest = new ImportDataRequest(key,10);
+        HttpEntity<ImportDataRequest> requestImportEntity =new HttpEntity<>(importRequest,requestHeaders);
+
+        ResponseEntity<ImportDataResponse> responseImportEntity = restTemplate.exchange("http://Import-Service/Import/importData",  HttpMethod.POST, requestImportEntity,ImportDataResponse.class);
+        ImportDataResponse importResponse = responseImportEntity.getBody();
+
+
+        /*********************PARSE*************************/
+
+        ParseImportedDataRequest parseRequest = new ParseImportedDataRequest(DataSource.TWITTER, importResponse.getList().get(0).getData());//    DataSource.TWITTER,ImportResponse. getJsonData());
+        HttpEntity<ParseImportedDataRequest> requestParseEntity =new HttpEntity<>(parseRequest,requestHeaders);
+
+        ResponseEntity<ParseImportedDataResponse> responseParseEntity = restTemplate.exchange("http://Parse-Service/Parse/parseImportedData",  HttpMethod.POST, requestParseEntity,ParseImportedDataResponse.class);
+        ParseImportedDataResponse parseResponse = responseParseEntity.getBody();
+
+
+        /*********************ANALYSE*************************/
+
+        for(int i =0; i < parseResponse.getDataList().size();i++){
+
+            String line = parseResponse.getDataList().get(i).getTextMessage();
+
+            String url = "http://Analyse-Service/Analyse/findSentiment";
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("line",line);
+
+            //ParseImportedDataRequest parseRequest = new ParseImportedDataRequest(com.Gateway_Service.Gateway_Service.dataclass.DataSource.TWITTER, importResponse.getList().get(0).getData());//    DataSource.TWITTER,ImportResponse. getJsonData());
+            //HttpEntity<ParseImportedDataRequest> requestParseEntity =new HttpEntity<>(parseRequest,requestHeaders);
+
+            ResponseEntity<AnalyseDataResponse> responseAnalyseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null,AnalyseDataResponse.class);
+            AnalyseDataResponse analyseResponse = responseAnalyseEntity.getBody();
         }
 
-        ParseImportedDataRequest parseReq = new ParseImportedDataRequest(DataSource.TWITTER,ImportRes.getJsonData());
-        ParseImportedDataResponse ParseRes = parseClient.parseImportedData(parseReq);
-
-        if (ParseRes == null) {
-            Data.add("Import Service Fail");
-            return Data;
-        }
 
 
-        for(int i = 0 ; i < ParseRes.getDataList().size(); i++) {
-            TweetWithSentiment sentiment = analyseClient.findSentiment(ParseRes.getDataList().get(i).getTextMessage());
-            if (sentiment == null) {
-                Data.add("Analyse-sentiment Service Fail");
-                return Data;
-            }
-
-            Data.add(sentiment.toString());
-        }
-
-        return Data;
+        //List<TweetWithSentiment> sentiments = new ArrayList<>();
+        //sentiments.add(tweetWithSentiment);
 
 
-        /*return carClient.readCars()
-                .getContent()
-                .stream()
-                .filter(this::isCool)
-                .collect(Collectors.toList());*/
+        return outputData;
+
     }
 
 
