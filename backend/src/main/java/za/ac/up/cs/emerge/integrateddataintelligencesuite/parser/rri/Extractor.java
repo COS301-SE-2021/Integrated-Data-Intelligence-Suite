bae5d12097a5793a9e32af6b46692b691470f654
shrey-99 <shreymandalia@gmail.com>
2021-06-19 18:49:24 +0200

    added get likes abstract method and fixed names of request parameters in Extractor

--- backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/rri/Extractor.java
+++ backend/src/main/java/za/ac/up/cs/emerge/integrateddataintelligencesuite/parser/rri/Extractor.java
@@ -2,18 +2,20 @@ package za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.rri;
 
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.exceptions.InvalidRequestException;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.GetDateRequest;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.GetLikesRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.GetLocationRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.request.GetTextRequest;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.GetDateResponse;
+import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.GetLikesResponse;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.GetLocationResponse;
 import za.ac.up.cs.emerge.integrateddataintelligencesuite.parser.response.GetTextResponse;
 
 
 public interface Extractor {
-    GetTextResponse getText(GetTextRequest jsonString) throws InvalidRequestException;
-    GetDateResponse getDate(GetDateRequest jsonString) throws InvalidRequestException;
-    GetLocationResponse getLocation(GetLocationRequest jsonString) throws InvalidRequestException;
-
+    GetTextResponse getText(GetTextRequest request) throws InvalidRequestException;
+    GetDateResponse getDate(GetDateRequest request) throws InvalidRequestException;
+    GetLocationResponse getLocation(GetLocationRequest request) throws InvalidRequestException;
+    GetLikesResponse getLikes(GetLikesRequest request) throws  InvalidRequestException;
 
 }
 
