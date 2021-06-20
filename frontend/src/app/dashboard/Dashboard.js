bdedce7b074116a04b779e5a1c2e53782ff7ea08
Myron Lopes <u18246932@tuks.co.za>
2021-06-20 06:40:02 +0200

    Removed stale comments

--- frontend/src/app/dashboard/Dashboard.js
+++ frontend/src/app/dashboard/Dashboard.js
@@ -35,7 +35,7 @@ class Dashboard extends Component {
                                 <h4>Views</h4>
                             </div>
                         </div>
-                        <div className="w3-quarter">
+                        {/* <div className="w3-quarter">
                             <div className="w3-container w3-teal w3-padding-16">
                                 <div className="w3-left"><i className="fa fa-share-alt w3-xxxlarge"></i></div>
                                 <div className="w3-right">
@@ -44,7 +44,7 @@ class Dashboard extends Component {
                                 <div className="w3-clear"></div>
                                 <h4>Shares</h4>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="w3-quarter">
                             <div className="w3-container w3-orange w3-text-white w3-padding-16">
                                 <div className="w3-left"><i className="fa fa-users w3-xxxlarge"></i></div>
