f31d75f53531f8858179eb9a837dcd2fdd6f1f45
Myron Lopes <u18246932@tuks.co.za>
2021-06-21 01:58:58 +0200

    Added Default Initial State to Network Graph

--- frontend/src/app/components/NetworkGraph.js
+++ frontend/src/app/components/NetworkGraph.js
@@ -6,48 +6,48 @@ require('highcharts/modules/networkgraph')(Highcharts);
 require('highcharts/modules/exporting')(Highcharts);
 
 //Network Graph
-Highcharts.addEvent(
-    Highcharts.Series,
-    'afterSetOptions',
-    function (e) {
-        var colors = Highcharts.getOptions().colors,
-            i = 0,
-            nodes = {};
+// Highcharts.addEvent(
+//     Highcharts.Series,
+//     'afterSetOptions',
+//     function (e) {
+//         var colors = Highcharts.getOptions().colors,
+//             i = 0,
+//             nodes = {};
 
-        if (
-            this instanceof Highcharts.seriesTypes.networkgraph &&
-            e.options.id === 'lang-tree'
-        ) {
-            e.options.data.forEach(function (link) {
+//         if (
+//             this instanceof Highcharts.seriesTypes.networkgraph &&
+//             e.options.id === 'lang-tree'
+//         ) {
+//             e.options.data.forEach(function (link) {
 
-                if (link[0] === 'Proto Indo-European') {
-                    nodes['Proto Indo-European'] = {
-                        id: 'Proto Indo-European',
-                        marker: {
-                            radius: 20
-                        }
-                    };
-                    nodes[link[1]] = {
-                        id: link[1],
-                        marker: {
-                            radius: 10
-                        },
-                        color: colors[i++]
-                    };
-                } else if (nodes[link[0]] && nodes[link[0]].color) {
-                    nodes[link[1]] = {
-                        id: link[1],
-                        color: nodes[link[0]].color
-                    };
-                }
-            });
+//                 if (link[0] === 'Proto Indo-European') {
+//                     nodes['Proto Indo-European'] = {
+//                         id: 'Proto Indo-European',
+//                         marker: {
+//                             radius: 20
+//                         }
+//                     };
+//                     nodes[link[1]] = {
+//                         id: link[1],
+//                         marker: {
+//                             radius: 10
+//                         },
+//                         color: colors[i++]
+//                     };
+//                 } else if (nodes[link[0]] && nodes[link[0]].color) {
+//                     nodes[link[1]] = {
+//                         id: link[1],
+//                         color: nodes[link[0]].color
+//                     };
+//                 }
+//             });
 
-            e.options.nodes = Object.keys(nodes).map(function (id) {
-                return nodes[id];
-            });
-        }
-    }
-);
+//             e.options.nodes = Object.keys(nodes).map(function (id) {
+//                 return nodes[id];
+//             });
+//         }
+//     }
+// );
 
 let network_graph_options = {
 	chart: {
@@ -86,17 +86,70 @@ let network_graph_options = {
             ['Anatolian', 'Lydian'],   
         ]
     }]
-
 };
 
 class NetworkGraph extends Component {
+    //Define the initial state of the Network Graph
+    state = {
+        chartOptions:{
+            chart: {
+                type: 'networkgraph',
+                height: '100%'
+            },
+
+            title: {
+                text: 'The Indo-European Language Tree'
+            },
+
+            subtitle: {
+                text: 'A Force-Directed Network Graph'
+            },
+            
+            plotOptions: {
+                networkgraph: {
+                    keys: ['from', 'to'],
+                    layoutAlgorithm: {
+                        enableSimulation: true,
+                        friction: -0.9
+                    }
+                }
+            },
+            series: [{
+                dataLabels: {
+                    enabled: true,
+                    linkFormat: ''
+                },
+                id: 'lang-tree',
+                data: [
+                    ['Proto Indo-European', 'Balto-Slavic'],
+                    ['Proto Indo-European', 'Anatolian'],
+                    ['Latino-Faliscan', 'Latin'],
+        
+                    // Leaves:
+                    ['Proto Indo-European', 'Phrygian'],
+                    ['Anatolian', 'Luwic'],
+                    ['Anatolian', 'Lydian'],   
+                ]
+            }]
+        }
+    };
+    
+
+    changeChartOptions = (newChartOption) => {
+        this.setState({
+            chartOptions: newChartOption
+        });
+    }
+
+
     render () {
         return (
 			<div id = 'network_graph_div'>
-				<HighchartsReact highcharts={Highcharts} options={network_graph_options} />
+				<HighchartsReact highcharts={Highcharts} options={this.state.chartOptions} />
 			</div>
         );
     }
+
 }
 
 export default NetworkGraph;
