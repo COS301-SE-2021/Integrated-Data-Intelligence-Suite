29ee10ddaae4e83e2c90f751ddae5f46447eb96a
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-20 20:42:54 +0200

    implemented parse service with fallback method

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ParseService.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ParseService.java
@@ -1,16 +1,44 @@
 package com.Gateway_Service.Gateway_Service.service;
 
 
-import com.Parse_Service.Parse_Service.request.ParseImportedDataRequest;
-import com.Parse_Service.Parse_Service.response.ParseImportedDataResponse;
+import com.Gateway_Service.Gateway_Service.dataclass.*;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-@FeignClient(name = "Parse-Service" ,  url = "localhost/Parse:9003" , fallback = ParseServiceFallback.class)
-public interface ParseService {
-    @GetMapping("/parseImportedData")
-    public ParseImportedDataResponse parseImportedData(@RequestParam("request") ParseImportedDataRequest request) throws Exception;
+//@FeignClient(name = "Parse-Service" ,  url = "localhost/Parse:9003" , fallback = ParseServiceFallback.class)
+public class ParseService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+
+    @HystrixCommand(fallbackMethod = "parseImportedDataFallback")
+    public ParseImportedDataResponse parseImportedData(ParseImportedDataRequest parseRequest) {
+
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<ParseImportedDataRequest> requestEntity =new HttpEntity<>(parseRequest,requestHeaders);
+
+        ResponseEntity<ParseImportedDataResponse> responseEntity = restTemplate.exchange("http://Parse-Service/Parse/parseImportedData",  HttpMethod.POST, requestEntity, ParseImportedDataResponse.class);
+        ParseImportedDataResponse parseResponse = responseEntity.getBody();
+
+        return parseResponse;
+    }
+
+
+    public ParseImportedDataResponse parseImportedDataFallback(ParseImportedDataRequest parseRequest){
+        ParseImportedDataResponse parseImportedDataResponse =  new ParseImportedDataResponse(null);
+        parseImportedDataResponse.setFallback(true);
+        parseImportedDataResponse.setFallbackMessage("{Failed to get parse data}");
+        return parseImportedDataResponse;
+    }
 }
