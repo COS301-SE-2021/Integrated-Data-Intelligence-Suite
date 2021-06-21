2f4b7c4eb34d436539ccd47309b5d214e4fa5eb3
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-21 12:50:34 +0200

    added simple unit test function as a skeleton

--- Gateway_Service/src/test/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceControllerTest.java
+++ Gateway_Service/src/test/java/com/Gateway_Service/Gateway_Service/controller/GatewayServiceControllerTest.java
@@ -3,11 +3,15 @@ package com.Gateway_Service.Gateway_Service.controller;
 import com.Analyse_Service.Analyse_Service.controller.AnalyseServiceController;
 import com.Gateway_Service.Gateway_Service.GatewayServiceApplication;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
@@ -21,5 +25,13 @@ public class GatewayServiceControllerTest {
     @Before
     public void setup(){
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    @DisplayName("When Gateway is requested")
+    public void gatewayRequest() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/")
+        ).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
