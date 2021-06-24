4658531908a413c1c2a0155fc4c47b70904fac61
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-24 16:22:28 +0200

    created functionality for visualize-service module

---
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/exception/InvalidVisualizeRequestException.java
@@ -0,0 +1,4 @@
+package com.Visualize_Service.Visualize_Service.exception;
+
+public class InvalidVisualizeRequestException extends  Exception{
+}
--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
@@ -1,7 +1,12 @@
 package com.Visualize_Service.Visualize_Service.service;
 
+import com.Visualize_Service.Visualize_Service.request.VisualizeDataRequest;
+import com.Visualize_Service.Visualize_Service.response.VisualizeAnalyseDataResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VisualizeServiceImpl {
+    public VisualizeAnalyseDataResponse visualizeData(VisualizeDataRequest request) throws Exception{
+        return new VisualizeAnalyseDataResponse();
+    }
 }
