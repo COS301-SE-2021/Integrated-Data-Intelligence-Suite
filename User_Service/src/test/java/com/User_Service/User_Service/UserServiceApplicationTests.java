b00cdb699fbcb2f356ef62b1369942e22c679257
u18003517-Rhuli <u18003517@tuks.co.za>
2021-06-24 14:40:23 +0200

    added user-service module

---
+++ User_Service/src/test/java/com/User_Service/User_Service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.User_Service.User_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
