e45b669f556262294fb5537b7ae856d1b2e17691
shrey-99 <shreymandalia@gmail.com>
2021-06-29 18:57:31 +0200

    user getters and setters, and function comments

--- User_Service/src/main/java/com/User_Service/User_Service/dataclass/User.java
+++ User_Service/src/main/java/com/User_Service/User_Service/dataclass/User.java
@@ -1,4 +1,60 @@
 package com.User_Service.User_Service.dataclass;
 
+import java.util.UUID;
+
 public class User {
+    String firstName;
+    String lastName;
+    String username;
+    String email;
+    String password;
+    UUID id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
--- User_Service/src/main/java/com/User_Service/User_Service/service/UserServiceImpl.java
+++ User_Service/src/main/java/com/User_Service/User_Service/service/UserServiceImpl.java
@@ -14,22 +14,48 @@ public class UserServiceImpl {
 
     }
 
+    /**
+     * This function logs the user in.
+     * @param request This class contains the user information for login.
+     * @return This returns a response contains the exit code**
+     */
     public LoginResponse login(LoginRequest request) {
         return null;
     }
 
+    /**
+     * This function registers the user to the platform.
+     * @param request This class contains the information to store the user within the system.
+     * @return This returns a response containing the exit code**
+     */
     public RegisterResponse register(RegisterResponse request) {
         return null;
     }
 
+    /**
+     * This function verifies the user's authenticity.
+     * @param request This class contains the information of the user.
+     * @return The return class returns if the verification process was successful**
+     */
     public VerifyAccountResponse verifyAccount(VerifyAccountRequest request) {
         return null;
     }
 
+    /**
+     * This function will allow the user to reset their password and store the new password
+     * in the database.
+     * @param request This class will contain the new password of the user.
+     * @return This class will contain if the password reset process was successful.
+     */
     public ResetPasswordResponse resetPassword(ResetPasswordRequest request) {
         return null;
     }
 
+    /**
+     * This function will return the current user logged onto the system.
+     * @param request This is the request for the use case.
+     * @return This class will contain the current user logged on.
+     */
     public GetCurrentUserResponse getCurrentUser(GetCurrentUserRequest request) {
         return null;
     }
