218e67c6d8c5cb1e8d0379bed0a053a1c78f0ac7
shrey-99 <shreymandalia@gmail.com>
2021-06-29 16:10:30 +0200

    empty functions for user use cases

--- User_Service/src/main/java/com/User_Service/User_Service/service/UserServiceImpl.java
+++ User_Service/src/main/java/com/User_Service/User_Service/service/UserServiceImpl.java
@@ -1,7 +1,36 @@
 package com.User_Service.User_Service.service;
 
+import com.User_Service.User_Service.request.GetCurrentUserRequest;
+import com.User_Service.User_Service.request.LoginRequest;
+import com.User_Service.User_Service.request.ResetPasswordRequest;
+import com.User_Service.User_Service.request.VerifyAccountRequest;
+import com.User_Service.User_Service.response.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl {
+
+    public UserServiceImpl() {
+
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public RegisterResponse register(RegisterResponse request) {
+        return null;
+    }
+
+    public VerifyAccountResponse verifyAccount(VerifyAccountRequest request) {
+        return null;
+    }
+
+    public ResetPasswordResponse resetPassword(ResetPasswordRequest request) {
+        return null;
+    }
+
+    public GetCurrentUserResponse getCurrentUser(GetCurrentUserRequest request) {
+        return null;
+    }
 }
