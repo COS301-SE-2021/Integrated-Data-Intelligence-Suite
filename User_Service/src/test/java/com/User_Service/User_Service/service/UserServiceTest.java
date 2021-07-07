d0618143d097ebfab2579e3f29c990e9f5ffe721
u18003517-Rhuli <u18003517@tuks.co.za>
2021-07-07 17:00:06 +0200

    Added default unit tests for user-Service application

---
+++ User_Service/src/test/java/com/User_Service/User_Service/service/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.User_Service.User_Service.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+public class UserServiceTest {
+    @InjectMocks
+    private UserServiceImpl service ;
+
+    @BeforeEach
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Dummy test")
+    public void dummyTest(){
+        Assertions.assertTrue(true);
+    }
+}
