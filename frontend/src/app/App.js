6b5d7c9a30b02507d5ec04812b1c7a798573f32d
Myron Lopes <u18246932@tuks.co.za>
2021-07-24 19:26:17 +0200

    Refactored Navbar to TopNavBar in multiple files

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,78 +1,34 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import AppRouter from './AppRouter';
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import TopNavBar from "./components/TopNavBar";
 
-import Navbar from './components/Navbar';
-
-import Footer from './components/Footer';
 import './App.scss';
 
 import GraphList from './components/GraphList';
-
-
+import Login from './pages/Login';
 
 
 class App extends Component {
     state = {}
 
-    componentDidMount() {
-        this.onRouteChanged();
-    }
+    // componentDidMount() {
+    //     this.onRouteChanged();
+    // }
 
     render() {
-
         let graph_list_component = !this.state.isFullPageLayout ? <GraphList/> : '';
 
-
         return (
-            <div className="container-scroller">
-
-
-                <div className="container-fluid page-body-wrapper">
-                   
-                    <div className="main-panel">
-                        <div className="content-wrapper" >
-                            <AppRouter/>
-                            {graph_list_component}
-
-                        </div>
-                        {/*{footerComponent}*/}
-                    </div>
-                </div>
-            </div>
+            <>
+                <TopNavBar/>
+                <Switch>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/' component={GraphList}/>
+                </Switch>
+            </>
         );
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.location !== prevProps.location) {
-            this.onRouteChanged();
-        }
-    }
-
-    onRouteChanged() {
-        // console.log("ROUTE CHANGED");
-
-        window.scrollTo(0, 0);
-        const fullPageLayoutRoutes = ['/pages/Login', '/pages/Register', '/functions/ValidateLogin', '/functions/ValidateRegister'];
-        for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
-            if (this.props.location.pathname === fullPageLayoutRoutes[i]) {
-                this.setState({
-                    isFullPageLayout: true
-                })
-                document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
-                break;
-            } else {
-                this.setState({
-                    isFullPageLayout: false
-                })
-                document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
-            }
-        }
-    }
-
-
-
 }
 
-
-export default withRouter(App);
+export default App;
--- frontend/src/app/functions/ValidateLogin.js
+++ frontend/src/app/functions/ValidateLogin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/TopNavBar";
 
 
 class ValidateLogin extends Component{
