72dc153055ef1cf5da1673b7b48c20867dada27a
shrey-99 <shreymandalia@gmail.com>
2021-07-29 18:55:20 +0200

    added message and success attributes

--- User_Service/src/main/java/com/User_Service/User_Service/response/ManagePersmissionsResponse.java
+++ User_Service/src/main/java/com/User_Service/User_Service/response/ManagePersmissionsResponse.java
@@ -1,4 +1,27 @@
 package com.User_Service.User_Service.response;
 
 public class ManagePersmissionsResponse {
+    private String message;
+    private boolean success;
+
+    public ManagePersmissionsResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
