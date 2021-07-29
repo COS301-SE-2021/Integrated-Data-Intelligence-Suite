2d73169de7eb054378333d02672e3a02e8dfe0a5
shrey-99 <shreymandalia@gmail.com>
2021-07-29 21:03:17 +0200

    added manage permissions in controller

--- User_Service/src/main/java/com/User_Service/User_Service/controller/UserServiceController.java
+++ User_Service/src/main/java/com/User_Service/User_Service/controller/UserServiceController.java
@@ -1,10 +1,30 @@
 package com.User_Service.User_Service.controller;
 
 
+import com.User_Service.User_Service.request.ManagePermissionsRequest;
+import com.User_Service.User_Service.response.ManagePersmissionsResponse;
+import com.User_Service.User_Service.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/User")
 public class UserServiceController {
+
+    @Autowired
+    private UserServiceImpl service;
+    /**
+     * This function will allow the admin to change the permissions of a user.
+     * @param requestEntity The request containing the necessary information about the user.
+     * @return A class that contains if the update was successful or not.
+     * @throws Exception Thrown when any exceptions are encountered
+     */
+    @PostMapping(value = "/changepermission")
+    public ManagePersmissionsResponse managePermissions(RequestEntity<ManagePermissionsRequest> requestEntity) throws Exception {
+        ManagePermissionsRequest request = requestEntity.getBody();
+        return service.managePermissions(request);
+    }
 }
