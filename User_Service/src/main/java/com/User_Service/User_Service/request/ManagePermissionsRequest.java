fb7fec0e434172458c6c1ab036c0c2020b2efc7e
shrey-99 <shreymandalia@gmail.com>
2021-07-29 18:54:41 +0200

    added attributes for changing permission

--- User_Service/src/main/java/com/User_Service/User_Service/request/ManagePermissionsRequest.java
+++ User_Service/src/main/java/com/User_Service/User_Service/request/ManagePermissionsRequest.java
@@ -1,4 +1,29 @@
 package com.User_Service.User_Service.request;
 
+import com.User_Service.User_Service.rri.Permission;
+
 public class ManagePermissionsRequest {
+    private String username;
+    private Permission newPermission;
+
+    public ManagePermissionsRequest(String username, Permission newPermission) {
+        this.username = username;
+        this.newPermission = newPermission;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Permission getNewPermission() {
+        return newPermission;
+    }
+
+    public void setNewPermission(Permission newPermission) {
+        this.newPermission = newPermission;
+    }
 }
