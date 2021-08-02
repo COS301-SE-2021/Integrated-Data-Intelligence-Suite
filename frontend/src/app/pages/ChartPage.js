76421512337ffb379f8f265b6d1f6346e929f408
Myron Lopes <u18246932@tuks.co.za>
2021-08-02 19:47:23 +0200

    added map card to charts page content section

--- frontend/src/app/pages/ChartPage.js
+++ frontend/src/app/pages/ChartPage.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import SideBar from "../components/SideBar/SideBar";
 import {
-    Input, Layout
+    Input,
+    Layout,
+    Card,
+    Typography
 } from 'antd';
-import {Typography} from 'antd';
 import {Route, Switch} from "react-router-dom";
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
@@ -23,31 +25,41 @@ class ChartPage extends Component {
                     <Route exact path='/chart'>
                         <Layout id={'outer_layout'} className={'chart-page'}>
                             <SideBar/>
-                            <Layout>
+                            <Layout id={'inner_layout_div'}>
                                 <Header id={'top_bar'}>
-                                    <Title level={1}>ChartPage</Title>
+                                    <Title level={1}>Chart Page Title</Title>
                                 </Header>
 
                                 <Content id={'content_section'}>
-                                    <MapContainer
-                                        center={[51.505, -0.09]}
-                                        zoom={13}
-                                        scrollWheelZoom={false}
-                                        style={{height: 300, width: 300, border: '3px solid yellow'}}
+                                    <Card title="Default size card"
+                                          extra={<p>Tooltip</p>}
+                                          id={'map_card'}
                                     >
-                                        <TileLayer
-                                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                        />
-                                        <Marker position={[51.505, -0.09]}>
-                                            <Popup>
-                                                A pretty CSS3 popup. <br/> Easily customizable.
-                                            </Popup>
-                                        </Marker>
-                                    </MapContainer>
+                                        <p>Card content</p>
+                                        <MapContainer
+                                            center={[51.505, -0.09]}
+                                            zoom={13}
+                                            scrollWheelZoom={false}
+                                            style={{
+                                                height: 300,
+                                                width: 300,
+                                                border: '3px solid yellow'
+                                            }}
+                                        >
+                                            <TileLayer
+                                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                            />
+                                            <Marker position={[51.505, -0.09]}>
+                                                <Popup>
+                                                    A pretty CSS3 popup. <br/> Easily customizable.
+                                                </Popup>
+                                            </Marker>
+                                        </MapContainer>
+                                    </Card>
+
                                 </Content>
 
-                                {/*<Footer id={'footer_section'}>Footer</Footer>*/}
                             </Layout>
                         </Layout>
                     </Route>
