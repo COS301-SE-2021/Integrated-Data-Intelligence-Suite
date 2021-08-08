c8bea694ed019f95b899af58cb161e5f8c59ede3
Shrey Mandalia <shreymandalia@gmail.com>
2021-08-08 15:44:26 +0200

    added register response and request classes

---
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/RegisterResponse.java
@@ -0,0 +1,27 @@
+package com.Gateway_Service.Gateway_Service.dataclass;
+
+public class RegisterResponse {
+    private boolean success;
+    private String message;
+
+    public RegisterResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
