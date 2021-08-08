327d5aef2f7265531c355c1a7082919fe6f0fb8d
Myron Lopes <u18246932@tuks.co.za>
2021-08-08 20:23:41 +0200

    Created Login Page

--- frontend/src/app/pages/LoginPage/LoginPage.js
+++ frontend/src/app/pages/LoginPage/LoginPage.js
@@ -1,27 +1,99 @@
 import React, {Component} from 'react';
+import {Form, Input, Button, Checkbox, Card, Divider} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
 import {Link} from "react-router-dom";
 
-class LoginPage extends Component {
+
+class LoginPage extends React.Component {
     render() {
         return (
             <>
-                <div style={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: "red",
-                }}>
-                    This is the Login Page
-                </div>
-
-                <span
-                    style={{
-                        width: 50,
-                        height: 50,
-                        backgroundColor: 'yellow'
-                    }}
+                <Card
+                    id={"login_card"}
+                    className={"loginCard"}
+                    title="Data Intelligence Suite"
+                    style={{width: 300}}
                 >
-                    <Link to="/">Come back home son.</Link>
-                </span>
+
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={null}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: false,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<UserOutlined className="site-form-item-icon"/>}
+                                placeholder="email"
+                            />
+
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: false,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                prefix={<LockOutlined className="site-form-item-icon"/>}
+                                type="password"
+                                placeholder="Password"
+                                allowClear={false}
+                            />
+                        </Form.Item>
+
+
+                        <Form.Item>
+                            {/*<Form.Item name="remember" valuePropName="checked" noStyle>*/}
+                            {/*    <Checkbox>Remember me</Checkbox>*/}
+                            {/*</Form.Item>*/}
+
+                            <a className="forgot_password_link" href="">
+                                Forgot password
+                            </a>
+                        </Form.Item>
+
+
+                        <Form.Item>
+                            <Link to="/">
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    className="login_button">
+                                    Log in
+                                </Button>
+                            </Link>
+
+                        </Form.Item>
+
+                        <Divider className={'or_divider'}>
+                            OR
+                        </Divider>
+
+                        <Form.Item>
+                            <a href="#">register now!</a>
+
+                        </Form.Item>
+
+                    </Form>
+
+                </Card>
+
+
             </>
         );
     }
