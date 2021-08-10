8b04f260439cdf72ad4a7983d60654fdf43350a6
Myron Lopes <u18246932@tuks.co.za>
2021-08-10 13:58:06 +0200

    Setup Layout for Registration page

---
+++ frontend/src/app/pages/LoginPage/functions/validateLoginDetails.js
@@ -0,0 +1,13 @@
+
+
+export function validateLoginDetails() {
+    console.log('validate login details is running after input fields have submitted');
+    // this.state.name_validation_status = 'validating';
+    // this.state.password_validation_status = 'validating';
+    // window.location.href='/';
+
+
+
+
+
+}
---
+++ frontend/src/app/pages/RegisterPage/LoginButton.js
@@ -0,0 +1,57 @@
+import {Button} from 'antd';
+import React, {Component} from 'react';
+// import {validateLoginDetails} from "./functions/validateLoginDetails";
+
+class LoginButton extends React.Component {
+    constructor() {
+        super();
+    }
+
+    state = {
+        is_loading: false,
+        username: '',
+        password: '',
+    }
+
+
+    enterLoading(val) {
+
+        this.setState({
+            is_loading: !this.state.is_loading,
+        })
+
+
+
+        this.setState({
+            is_loading: false,
+        })
+
+
+
+
+
+        // console.log(user)
+    }
+
+
+    render() {
+        return (
+            <>
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login_button"
+                    loading={this.state.is_loading}
+                    onClick={() => this.enterLoading(!this.state.is_loading)}
+
+                >
+                    Log in
+                </Button>
+
+
+            </>
+        );
+    }
+}
+
+export default LoginButton;
