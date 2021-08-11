2370ede1addb87fa998a1e50e4284e6a1bf6df14
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-11 23:22:56 +0200

    edited FindTrendsRequest object

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/FindTrendsRequest.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/FindTrendsRequest.java
@@ -3,13 +3,13 @@ package com.Analyse_Service.Analyse_Service.request;
 import java.util.ArrayList;
 
 public class FindTrendsRequest {
-    ArrayList<String> dataList;
+    ArrayList<ArrayList> dataList;
 
-    public FindTrendsRequest(ArrayList<String> dataList){
+    public FindTrendsRequest(ArrayList<ArrayList> dataList){
         this.dataList = dataList;
     }
 
-    public ArrayList<String> getDataList(){
+    public ArrayList<ArrayList> getDataList(){
         return dataList;
     }
 }
