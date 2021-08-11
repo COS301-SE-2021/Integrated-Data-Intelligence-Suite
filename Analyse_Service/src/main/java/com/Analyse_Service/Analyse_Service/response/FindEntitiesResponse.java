ab3d6438f07beb8e3664369cf5c5f5b96fa15c18
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-11 22:40:34 +0200

    updated find Entities Response object function according to the changes made to find entities function

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindEntitiesResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindEntitiesResponse.java
@@ -3,13 +3,13 @@ package com.Analyse_Service.Analyse_Service.response;
 import java.util.ArrayList;
 
 public class FindEntitiesResponse {
-    ArrayList<String> entitiesList;
+    ArrayList<ArrayList> entitiesList;
 
-    public FindEntitiesResponse(ArrayList<String> entitiesList){
+    public FindEntitiesResponse(ArrayList<ArrayList> entitiesList){
         this.entitiesList = entitiesList;
     }
 
-    public ArrayList<String> getEntitiesList(){
+    public ArrayList<ArrayList> getEntitiesList(){
         return this.entitiesList;
     }
 }
