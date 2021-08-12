0ca31dd86b40ec394aa4d09d96f52cb9029275a8
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-12 14:10:09 +0200

    edits to FindEntitiesResponse

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindEntitiesResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindEntitiesResponse.java
@@ -1,9 +1,20 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreSentence;
+
 import java.util.ArrayList;
 
 public class FindEntitiesResponse {
     ArrayList<ArrayList> entitiesList;
+
+    String sentiment ;
+    String[] parts_of_speech;
+    String[] lemmanation;
+    ArrayList<ArrayList> named_entities;
+
+
 
     public FindEntitiesResponse(ArrayList<ArrayList> entitiesList){
         this.entitiesList = entitiesList;
