65275c68ca75a181216f7be7e774c79797d54231
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-13 22:57:15 +0200

    implemented map graph request object.

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateMapGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateMapGraphRequest.java
@@ -1,4 +1,15 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class CreateMapGraphRequest {
+    public ArrayList<ArrayList> dataList;
+
+    public CreateMapGraphRequest(ArrayList<ArrayList> dataList){
+        this.dataList = dataList;
+    }
+
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
+    }
 }
