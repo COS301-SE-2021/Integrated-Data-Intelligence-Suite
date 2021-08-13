de493d9db2aed338466c4095b20ba0f5b75ea8fa
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-13 22:20:16 +0200

    edits to line graph

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/LineGraph.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/LineGraph.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class LineGraph extends Graph {
     public String name;
+
     //public ArrayList<String> marker = new ArrayList<>();
 
     public static class marker {
-        public String symbol = "square";
+        public String symbol;
     }
 
     public ArrayList<Object> data  = new ArrayList<>();
