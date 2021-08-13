54c0501350345758e59563fda559d5e1aa435276
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:27:52 +0200

    updated CreateLineGraphRequest object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateLineGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateLineGraphRequest.java
@@ -1,4 +1,17 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import com.Visualize_Service.Visualize_Service.dataclass.Graph;
+
+import java.util.ArrayList;
+
 public class CreateLineGraphRequest {
+    public ArrayList<ArrayList> dataList;
+
+    public CreateLineGraphRequest(ArrayList<ArrayList> dataList){
+        this.dataList = dataList;
+    }
+
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
+    }
 }
