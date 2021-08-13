97b8dfba59383a6626dec88149022bbf523149fa
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:36:54 +0200

    updated CreateNetworkGraphRequest object with necessary functions

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateNetworkGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateNetworkGraphRequest.java
@@ -1,4 +1,15 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class CreateNetworkGraphRequest {
+    public ArrayList<ArrayList> dataList;
+
+    public CreateNetworkGraphRequest(ArrayList<ArrayList> dataList){
+        this.dataList = dataList;
+    }
+
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
+    }
 }
