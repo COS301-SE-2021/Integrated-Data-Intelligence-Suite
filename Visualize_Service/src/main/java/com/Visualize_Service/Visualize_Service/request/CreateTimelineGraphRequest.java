00307b07f0461f1b1a7a3b372e5860a42e583170
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-13 22:56:59 +0200

    implemented timeline graph request object.

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateTimelineGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateTimelineGraphRequest.java
@@ -1,4 +1,15 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class CreateTimelineGraphRequest {
+    public ArrayList<ArrayList> dataList;
+
+    public CreateTimelineGraphRequest(ArrayList<ArrayList> dataList){
+        this.dataList = dataList;
+    }
+
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
+    }
 }
