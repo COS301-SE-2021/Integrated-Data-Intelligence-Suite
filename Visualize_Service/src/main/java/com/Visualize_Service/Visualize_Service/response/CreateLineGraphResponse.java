c327b0030871748a7a2b94f6a786d8cb12ee2634
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:30:08 +0200

    updated CreateLineGraphResponse object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/CreateLineGraphResponse.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/CreateLineGraphResponse.java
@@ -1,4 +1,16 @@
 package com.Visualize_Service.Visualize_Service.response;
 
+import com.Visualize_Service.Visualize_Service.dataclass.Graph;
+
+import java.util.ArrayList;
+
 public class CreateLineGraphResponse {
+    public ArrayList<Graph> LineGraphArray;
+    public CreateLineGraphResponse(ArrayList<Graph> LineGraphArray){
+        this.LineGraphArray = LineGraphArray;
+    }
+
+    public ArrayList<Graph> getLineGraphArray(){
+        return LineGraphArray;
+    }
 }
