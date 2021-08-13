470425750e189a25c4ca16d53bc7809aaaf50c2c
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:03:32 +0200

    added error checking to visualizeData function

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.Visualize_Service.Visualize_Service.service;
 
 import com.Visualize_Service.Visualize_Service.dataclass.Graph;
+import com.Visualize_Service.Visualize_Service.exception.InvalidRequestException;
 import com.Visualize_Service.Visualize_Service.request.VisualizeDataRequest;
 import com.Visualize_Service.Visualize_Service.response.VisualizeAnalyseDataResponse;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,21 @@ import java.util.ArrayList;
 
 @Service
 public class VisualizeServiceImpl {
-    public VisualizeAnalyseDataResponse visualizeData(VisualizeDataRequest request) throws Exception{
-        return new VisualizeAnalyseDataResponse();
+    public VisualizeAnalyseDataResponse visualizeData(VisualizeDataRequest request) throws InvalidRequestException {
+        if (request == null) {
+            throw new InvalidRequestException("FindEntitiesRequest Object is null");
+        }
+        if (request.getPatternList() == null){
+            throw new InvalidRequestException("Arraylist is null");
+        }
+        if (request.getRelationshipList() == null){
+            throw new InvalidRequestException("Arraylist is null");
+        }
+        if (request.getPredictionList() == null){
+            throw new InvalidRequestException("Arraylist is null");
+        }
+
+        return new VisualizeAnalyseDataResponse(null);
     }
 
 
