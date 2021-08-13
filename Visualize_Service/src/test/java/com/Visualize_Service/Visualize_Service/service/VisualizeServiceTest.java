3a7c548955086a782951ea899be0f6f566cde321
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 23:13:40 +0200

    created unittest for VisualizeData Null Request

--- Visualize_Service/src/test/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceTest.java
+++ Visualize_Service/src/test/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceTest.java
@@ -1,11 +1,15 @@
 package com.Visualize_Service.Visualize_Service.service;
 
+import com.Visualize_Service.Visualize_Service.exception.InvalidRequestException;
+import com.Visualize_Service.Visualize_Service.request.VisualizeDataRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 public class VisualizeServiceTest {
     @InjectMocks
@@ -21,4 +25,11 @@ public class VisualizeServiceTest {
     public void dummyTest(){
         Assertions.assertTrue(true);
     }
+
+    @Test
+    @DisplayName("When findTrendsNullRequest is Null")
+    public void visualizeDataNullRequest(){
+        Assertions.assertThrows(InvalidRequestException.class, () -> service.visualizeData(null));
+    }
+
 }
