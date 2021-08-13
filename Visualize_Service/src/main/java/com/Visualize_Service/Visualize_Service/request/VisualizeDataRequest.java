29d5e13ce50407419b3b8570a465cced5692227f
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:04:08 +0200

    updated VisualizeDataRequest object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/VisualizeDataRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/VisualizeDataRequest.java
@@ -1,4 +1,25 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class VisualizeDataRequest {
+    public ArrayList<ArrayList> patternList;
+    public ArrayList<ArrayList> relationshipList;
+    public ArrayList<ArrayList> predictionList;
+
+    public VisualizeDataRequest(ArrayList<ArrayList>patternList, ArrayList<ArrayList> relationshipList, ArrayList<ArrayList> predictionList){
+        this.patternList = patternList;
+        this.relationshipList = relationshipList;
+        this.predictionList = predictionList;
+    }
+
+    public ArrayList<ArrayList> getPatternList(){
+        return patternList;
+    }
+    public ArrayList<ArrayList>  getRelationshipList(){
+        return relationshipList;    }
+    public ArrayList<ArrayList> getPredictionList(){
+        return predictionList;
+    }
+
 }
