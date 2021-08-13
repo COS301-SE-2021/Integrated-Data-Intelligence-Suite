e952885d9d96032ba9d74a6f7c03c249ef265ebe
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-13 22:04:23 +0200

    updated VisualizeAnalyseDataResponse object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/VisualizeAnalyseDataResponse.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/VisualizeAnalyseDataResponse.java
@@ -1,4 +1,18 @@
 package com.Visualize_Service.Visualize_Service.response;
 
+import com.Visualize_Service.Visualize_Service.dataclass.Graph;
+
+import java.util.ArrayList;
+
 public class VisualizeAnalyseDataResponse {
+    public ArrayList<ArrayList> outputData;
+
+    public VisualizeAnalyseDataResponse(ArrayList<ArrayList> outputData){
+        this.outputData = outputData;
+    }
+
+    public ArrayList<ArrayList> getOutputData(){
+        return outputData;
+    }
+
 }
