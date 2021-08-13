2d61a19583fea5e07cc3723ccf02bd13705266ee
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-13 22:19:05 +0200

    implemented line graph

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/LineGraph.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/LineGraph.java
@@ -1,4 +1,14 @@
 package com.Visualize_Service.Visualize_Service.dataclass;
 
-public class LineGraph {
+import java.util.ArrayList;
+
+public class LineGraph extends Graph {
+    public String name;
+    //public ArrayList<String> marker = new ArrayList<>();
+
+    public static class marker {
+        public String symbol = "square";
+    }
+
+    public ArrayList<Object> data  = new ArrayList<>();
 }
