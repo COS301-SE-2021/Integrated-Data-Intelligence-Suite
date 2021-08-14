5393b631b1d1a1b23510a01ce3e1e10cfc8536a8
shrey-99 <shreymandalia@gmail.com>
2021-08-14 17:24:26 +0200

    completed ResetPasswordResponse

--- User_Service/src/main/java/com/User_Service/User_Service/response/ResetPasswordResponse.java
+++ User_Service/src/main/java/com/User_Service/User_Service/response/ResetPasswordResponse.java
@@ -1,4 +1,31 @@
 package com.User_Service.User_Service.response;
 
 public class ResetPasswordResponse {
+    private String message;
+    private boolean success;
+
+    public ResetPasswordResponse() {
+
+    }
+
+    public ResetPasswordResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
