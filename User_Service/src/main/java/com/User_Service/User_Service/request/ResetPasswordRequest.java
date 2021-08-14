f9a987f0530e9fced5dff976891d8c325c3db2e2
shrey-99 <shreymandalia@gmail.com>
2021-08-14 17:20:30 +0200

    completed ResetPasswordRequest

--- User_Service/src/main/java/com/User_Service/User_Service/request/ResetPasswordRequest.java
+++ User_Service/src/main/java/com/User_Service/User_Service/request/ResetPasswordRequest.java
@@ -1,4 +1,31 @@
 package com.User_Service.User_Service.request;
 
 public class ResetPasswordRequest {
+    private String newPassword;
+    private String email;
+
+    public ResetPasswordRequest() {
+
+    }
+
+    public ResetPasswordRequest(String newPassword, String email) {
+        this.newPassword = newPassword;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
