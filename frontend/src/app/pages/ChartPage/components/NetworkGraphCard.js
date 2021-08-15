b21728571c8bb435523f7a9efcd90f015683e60e
Myron Lopes <u18246932@tuks.co.za>
2021-08-15 13:07:27 +0200

    Added Code to render Nodes and styles from Received .json file

--- frontend/src/app/pages/ChartPage/components/NetworkGraphCard.js
+++ frontend/src/app/pages/ChartPage/components/NetworkGraphCard.js
@@ -1,55 +1,65 @@
 import cytoscape from "cytoscape/dist/cytoscape.cjs";
 import React, {useEffect, useRef, useState} from 'react';
+import timelineNodes from "../resources/networkGraphNodes.json";
+import "../styles/NetworkGraph.css";
+import network_stylesheet from "../resources/networkGraphNodesStyleSheet.json";
 
 function NetworkGraphCard() {
 
     useEffect(() => {
         var cy = cytoscape({
             container: document.getElementById('network_graph_card_content'), // container to render in
-            elements: [ // list of graph elements to start with
-                { // node a
-                    data: {id: 'a'}
-                },
-                { // node b
-                    data: {id: 'b'}
-                },
-                { // edge ab
-                    data: {id: 'ab', source: 'a', target: 'b'}
-                }
-            ],
+            // elements: [ // list of graph elements to start with
+            //     { // node a
+            //         data: {id: 'a'}
+            //     },
+            //     { // node b
+            //         data: {id: 'b'}
+            //     },
+            //     { // edge ab
+            //         data: {id: 'ab', source: 'a', target: 'b'}
+            //     }
+            // ],
 
-            style: [ // the stylesheet for the graph
-                {
-                    selector: 'node',
-                    style: {
-                        'background-color': '#666',
-                        'label': 'data(id)'
-                    }
-                },
-
-                {
-                    selector: 'edge',
-                    style: {
-                        'width': 3,
-                        'line-color': '#ccc',
-                        'target-arrow-color': '#ccc',
-                        'target-arrow-shape': 'triangle',
-                        'curve-style': 'bezier'
-                    }
-                }
-            ],
+            // style: [ // the stylesheet for the graph
+            //     {
+            //         selector: 'node',
+            //         style: {
+            //             'background-color': 'red',
+            //             'label': 'data(id)'
+            //         }
+            //     },
+            //
+            //     {
+            //         selector: 'edge',
+            //         style: {
+            //             'width': 3,
+            //             'line-color': '#ccc',
+            //             'target-arrow-color': '#ccc',
+            //             'target-arrow-shape': 'triangle',
+            //             'curve-style': 'bezier'
+            //         }
+            //     }
+            // ],
 
             layout: {
-                name: 'grid',
+                name: 'random',
                 rows: 1
             }
         });
+
+        //Adding nodes to the layout
+        cy.add(timelineNodes);
+
+        //Adding a stylesheet to the network graph
+        cy.style(network_stylesheet);
     })
 
-    return (
-        <div></div>
-    );
+    // return (
+    //     <div></div>
+    // );
 
+    return null; // not rendering anything
 }
 
 export default NetworkGraphCard;
