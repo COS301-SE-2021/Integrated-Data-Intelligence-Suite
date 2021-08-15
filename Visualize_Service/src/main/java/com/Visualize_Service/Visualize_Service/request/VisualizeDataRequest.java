62b97c058892e6f93598fd0d5d5594509ad6a038
Steve Mbuguje <u18008390@tuks.co.za>
2021-08-16 01:36:35 +0200

    updated VisualizeDataRequest object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/VisualizeDataRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/VisualizeDataRequest.java
@@ -6,11 +6,15 @@ public class VisualizeDataRequest {
     public ArrayList<ArrayList> patternList;
     public ArrayList<ArrayList> relationshipList;
     public ArrayList<ArrayList> predictionList;
+    public ArrayList<ArrayList> TrendList;
+    public ArrayList<ArrayList> AnomalyList;
 
-    public VisualizeDataRequest(ArrayList<ArrayList>patternList, ArrayList<ArrayList> relationshipList, ArrayList<ArrayList> predictionList){
+    public VisualizeDataRequest(ArrayList<ArrayList>patternList, ArrayList<ArrayList> relationshipList, ArrayList<ArrayList> predictionList,ArrayList<ArrayList> TrendList,ArrayList<ArrayList> AnomalyList){
         this.patternList = patternList;
         this.relationshipList = relationshipList;
         this.predictionList = predictionList;
+        this.AnomalyList = AnomalyList;
+        this.TrendList = TrendList;
     }
 
     public ArrayList<ArrayList> getPatternList(){
@@ -21,5 +25,14 @@ public class VisualizeDataRequest {
     public ArrayList<ArrayList> getPredictionList(){
         return predictionList;
     }
+
+    public ArrayList<ArrayList> getTrendList(){
+        return TrendList;
+    }
+
+    public ArrayList<ArrayList> getAnomalyList(){
+        return AnomalyList;
+    }
+
 
 }
