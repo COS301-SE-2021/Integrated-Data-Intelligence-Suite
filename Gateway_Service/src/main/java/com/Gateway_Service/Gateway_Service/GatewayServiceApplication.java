ea7005936b6289149dbdc75fcf4c2ef5dc23f704
shrey-99 <shreymandalia@gmail.com>
2021-08-16 16:37:11 +0200

    removed openfeign imports

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/GatewayServiceApplication.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/GatewayServiceApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- Parse_Service/src/main/java/com/Parse_Service/Parse_Service/controller/ParseServiceController.java
+++ Parse_Service/src/main/java/com/Parse_Service/Parse_Service/controller/ParseServiceController.java
@@ -6,7 +6,7 @@ import com.Parse_Service.Parse_Service.request.ParseImportedDataRequest;
 import com.Parse_Service.Parse_Service.response.ParseImportedDataResponse;
 import com.Parse_Service.Parse_Service.service.ParseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
+
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.*;
 
