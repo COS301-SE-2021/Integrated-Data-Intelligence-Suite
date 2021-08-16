7df103e18ed9fb2db1be6108d945ec95c151392e
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-16 23:24:33 +0200

    finalised the NodeNetworkGraph object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/NodeNetworkGraph.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/NodeNetworkGraph.java
@@ -4,12 +4,16 @@ public class NodeNetworkGraph extends NetworkGraph{
 
     public String group = "nodes";
     public data data = new data();
+    public position position = new position();
 
     public void setData(String id){
         this.data.id = id;
     }
 
-    //public position position = new position();
+    public void setPosition(int x, int y){
+        this.position.x = x;
+        this.position.y = y;
+    }
 
     class data {
         public String id;
