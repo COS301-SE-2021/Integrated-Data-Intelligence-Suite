57cf1243c971107ac1bbb257a2d603fbdd84cd56
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-16 12:13:19 +0200

    created and implemented timeline graph object.

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/TimelineGraph.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/TimelineGraph.java
@@ -1,8 +1,7 @@
 package com.Visualize_Service.Visualize_Service.dataclass;
 
 public class TimelineGraph extends Graph{
-    public String x;
-    public String name;
-    public String label;
-    public String description;
+    public String title;
+    public String cardTitle;
+    public String cardSubtitle;
 }
