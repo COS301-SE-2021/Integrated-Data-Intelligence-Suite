bb0d724d9bd22c1c5bc90778c2380b37d9ece737
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-16 17:21:49 +0200

    added helper graph objects

---
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/data.java
@@ -0,0 +1,9 @@
+package com.Visualize_Service.Visualize_Service.dataclass;
+
+public class data {
+    public String id;
+
+    public String source;
+
+    public String target;
+}
---
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/dataclass/position.java
@@ -0,0 +1,6 @@
+package com.Visualize_Service.Visualize_Service.dataclass;
+
+public class position {
+    public int x;
+    public int y;
+}
