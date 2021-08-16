7393cc836706f9d5fe52970854145327a7434291
Myron Lopes <u18246932@tuks.co.za>
2021-08-16 10:14:05 +0200

    Improved Styling of Search Bar and User Info Card

--- frontend/src/app/pages/ChartPage/components/TimelineGraph.js
+++ frontend/src/app/pages/ChartPage/components/TimelineGraph.js
@@ -1,32 +1,15 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import "../styles/TimelineGraph.css"
-import ScriptTag from 'react-script-tag';
 import {Chrono} from "react-chrono";
 import timeline_cards from "../resources/data.json"
 
-
 function TimelineGraph() {
-    // const items = [{
-    //     title: "May 1940",
-    //     cardTitle: "Dunkirk",
-    //     cardSubtitle: "Men of the British Expeditionary Force (BEF) wade out to..",
-    //     cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-    //     media: {
-    //         type: "IMAGE",
-    //         source: {
-    //             url: "http://someurl/image.jpg"
-    //         }
-    //     }
-    // }];
-
     return (
-        // <div style={{width: "500px", height: "400px"}}>
-            <Chrono
-                items={timeline_cards}
-                mode={"VERTICAL_ALTERNATING"}
-                scrollable={true}
-            />
-        // </div>
+        <Chrono
+            items={timeline_cards}
+            mode={"VERTICAL_ALTERNATING"}
+            scrollable={true}
+        />
     )
 }
 
