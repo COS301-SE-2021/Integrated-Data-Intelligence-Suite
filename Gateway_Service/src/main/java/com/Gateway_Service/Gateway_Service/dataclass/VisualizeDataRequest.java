864a900cea2a18f13f17201ae0460f00af42d75e
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-16 14:55:24 +0200

    added visualize objects to gateway

---
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/VisualizeDataRequest.java
@@ -0,0 +1,40 @@
+package com.Gateway_Service.Gateway_Service.dataclass;
+
+import java.util.ArrayList;
+
+public class VisualizeDataRequest {
+    public ArrayList<ArrayList> patternList;
+    public ArrayList<ArrayList> relationshipList;
+    public ArrayList<ArrayList> predictionList;
+    public ArrayList<ArrayList> trendList;
+    public ArrayList<String> anomalyList;
+
+    public VisualizeDataRequest(ArrayList<ArrayList> patternList,
+                                ArrayList<ArrayList> relationshipList,
+                                ArrayList<ArrayList> predictionList,
+                                ArrayList<ArrayList> trendList,
+                                ArrayList<String> anomalyList){
+        this.patternList = patternList;
+        this.relationshipList = relationshipList;
+        this.predictionList = predictionList;
+        this.trendList = trendList;
+        this.anomalyList = anomalyList;
+    }
+
+    public ArrayList<ArrayList> getPatternList(){
+        return patternList;
+    }
+    public ArrayList<ArrayList>  getRelationshipList(){
+        return relationshipList;    }
+    public ArrayList<ArrayList> getPredictionList(){
+        return predictionList;
+    }
+
+    public ArrayList<ArrayList> getTrendList(){
+        return trendList;
+    }
+
+    public ArrayList<String> getAnomalyList(){
+        return anomalyList;
+    }
+}
