043833c17874d770b99a41baaa510711593cbbf5
abDivergent <u15219900@tuks.co.za>
2021-08-16 05:01:09 +0200

    updated permissions page to correctly link through settings

--- frontend/src/app/pages/PermissionsPage/Permissions.js
+++ frontend/src/app/pages/PermissionsPage/Permissions.js
@@ -1,6 +1,6 @@
 import UserList from "../../components/ContentSection/UserList";
 import SideBar from "../../components/SideBar/SideBar";
-import {Layout} from "antd";
+import {Divider, Layout} from "antd";
 import {Content, Footer, Header} from "antd/es/layout/layout";
 import Title from "antd/es/typography/Title";
 import useGet from "../../functions/useGet";
@@ -14,24 +14,18 @@ const Permissions = () => {
     const { data:users, isPending, error } = useGet('/user/getAll');
 
     return (
-
-    <Layout id={'outer_layout'}>
-        <SideBar/>
         <Layout>
-            <Header id={'top_bar'}>
-                {/*<SearchBar/>*/}
-                <Title level={1}>Permission Management</Title>
-            </Header>
+            <div className={"clear-background"} id={"permissions_header"}>
+                <h1 id={"permissions-title"}>Permissions</h1>
+            </div>
             <Content className={"permissions-content-section"}>
-                <div className={"permissions"}>
+                <div className={"permissions user"}>
                     { error && <div>{ error }</div> }
                     { isPending && <div>Loading...</div> }
                     { users && <UserList users={ users.users }/> }
                 </div>
             </Content>
-            <Footer id={'footer_section'}>Footer</Footer>
         </Layout>
-    </Layout>
     );
 };
 
