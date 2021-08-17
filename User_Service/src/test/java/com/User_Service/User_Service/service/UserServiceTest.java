43464d01701f848025d7cb98bcd730f82368c0c6
shrey-99 <shreymandalia@gmail.com>
2021-08-17 12:55:43 +0200

    updated pom to fix errors

--- User_Service/src/test/java/com/User_Service/User_Service/service/UserServiceTest.java
+++ User_Service/src/test/java/com/User_Service/User_Service/service/UserServiceTest.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserServiceTest {
 
     @InjectMocks
