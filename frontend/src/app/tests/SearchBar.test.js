13fa4796b33f2795e56deeaae8febccba1e86963
Myron Lopes <u18246932@tuks.co.za>
2021-08-18 19:43:54 +0200

    Implemented SearchBar Snapshot using purely Jest instead of enzyme

--- frontend/src/app/tests/SearchBar.test.js
+++ frontend/src/app/tests/SearchBar.test.js
@@ -1,11 +1,22 @@
 // ComponentName.test.js
+// import React from 'react';
+// import Enzyme, { shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import SearchBar from '../components/SearchBar/SearchBar';
+// describe("SearchBar", () => {
+//   Enzyme.configure({ adapter: new Adapter() });
+//   it("should render my component", () => {
+//     const wrapper = shallow(<SearchBar />);
+//   });
+// });
+
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 import SearchBar from '../components/SearchBar/SearchBar';
-describe("SearchBar", () => {
-  Enzyme.configure({ adapter: new Adapter() });
-  it("should render my component", () => {
-    const wrapper = shallow(<SearchBar />);
-  });
+
+it('renders correctly', () => {
+    const tree = renderer
+        .create(<SearchBar/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
 });
