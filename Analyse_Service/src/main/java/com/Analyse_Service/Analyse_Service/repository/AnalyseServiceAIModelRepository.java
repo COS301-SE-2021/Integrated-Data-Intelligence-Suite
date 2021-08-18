dbefa4a5b3feac74b98d6dfb32fbe177631f8db6
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-18 12:46:13 +0200

    implemented repository using the A.I. model object

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/repository/AnalyseServiceAIModelRepository.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/repository/AnalyseServiceAIModelRepository.java
@@ -1,4 +1,17 @@
 package com.Analyse_Service.Analyse_Service.repository;
 
-public interface AnalyseServiceAIModelRepository {
+import com.Analyse_Service.Analyse_Service.dataclass.AIModel;
+import com.Analyse_Service.Analyse_Service.dataclass.AIType;
+import com.Analyse_Service.Analyse_Service.dataclass.ParsedData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AnalyseServiceAIModelRepository extends JpaRepository<AIModel,Long> {
+
+    @Query(
+            value = "SELECT MAX(accuracy) FROM AIModel a WHERE P.type = ?1 ",
+            nativeQuery = true)
+    List<ParsedData> findHighestAccuracyByType(AIType type);
 }
