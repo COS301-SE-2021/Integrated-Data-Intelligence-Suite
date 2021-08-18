d3e3d8020d6bdcdaef3447775460047154df3e75
Myron Lopes <u18246932@tuks.co.za>
2021-08-18 17:07:57 +0200

    Added an enzyme, Adapter to the Search bar test to solve Parse error

--- frontend/src/app/tests/SearchBar.test.js
+++ frontend/src/app/tests/SearchBar.test.js
@@ -1,8 +1,10 @@
 // ComponentName.test.js
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import SearchBar from '../components/SearchBar/SearchBar';
 describe("SearchBar", () => {
+  Enzyme.configure({ adapter: new Adapter() });
   it("should render my component", () => {
     const wrapper = shallow(<SearchBar />);
   });
