3c17d9e6370583d4eabc25bb9c9552d06535710d
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-18 17:23:47 +0200

    implemented saveAiModel request and response objects

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/SaveAIModelRequest.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/SaveAIModelRequest.java
@@ -1,4 +1,16 @@
 package com.Analyse_Service.Analyse_Service.request;
 
+import com.Analyse_Service.Analyse_Service.dataclass.AIModel;
+
 public class SaveAIModelRequest {
+
+    AIModel saveAIModel;
+
+    public SaveAIModelRequest(AIModel saveAIModel){
+        this.saveAIModel = saveAIModel;
+    }
+
+    public AIModel getSaveAIModel() {
+        return saveAIModel;
+    }
 }
--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/SaveAIModelResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/SaveAIModelResponse.java
@@ -1,4 +1,15 @@
 package com.Analyse_Service.Analyse_Service.response;
 
 public class SaveAIModelResponse {
+    boolean modelSaved;
+
+    public SaveAIModelResponse(boolean modelSaved){
+        this.modelSaved = modelSaved;
+    }
+
+    public boolean getModelSave(){
+        return this.modelSaved;
+    }
+
+
 }
