b16d5a17fb7f71fa02b78f43c50aeb39abebfb03
abDivergent <u15219900@tuks.co.za>
2021-08-18 02:55:33 +0200

    Revert "Completed new twitter endpoint in gateway [host/collect/{key}/{from}/{to}"
    This reverts commit e1d83c07a5c2cefddde740a02c370a0847114542.

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ImportService.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ImportService.java
@@ -94,11 +94,16 @@ public class ImportService {
 
 
     public ImportTwitterResponse importDatedData(ImportTwitterRequest importRequest) {
+
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<ImportTwitterRequest> requestEntity = new HttpEntity<ImportTwitterRequest>(importRequest, requestHeaders);
+
+        HttpEntity<ImportTwitterRequest> requestEntity = new HttpEntity<>(importRequest, requestHeaders);
+
         ResponseEntity<ImportTwitterResponse> responseEntity = restTemplate.exchange("http://Import-Service/Import/importDatedData", HttpMethod.POST, requestEntity, ImportTwitterResponse.class);
-        return responseEntity.getBody();
+        ImportTwitterResponse importTwitterResponse = responseEntity.getBody();
+
+        return  importTwitterResponse;
     }
 
     public ImportTwitterResponse getDatedDataFallback(ImportTwitterRequest importRequest){
