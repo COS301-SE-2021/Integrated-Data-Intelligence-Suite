f2b2af05a95b379f5f82846314145e0679e9ed75
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 00:31:12 +0200

    bug-fix: Import in Sidebar changed to use alias throught 'const'

--- frontend/src/app/components/SideBar/SideBar.js
+++ frontend/src/app/components/SideBar/SideBar.js
@@ -1,9 +1,10 @@
-import Sider from "antd/es/layout/Sider";
-import {Menu} from "antd";
+import {Menu, Layout} from "antd";
 import {BarChartOutlined, HomeOutlined, SettingOutlined} from "@ant-design/icons";
 import UserInfoCard from "./UserInfoCard";
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import {BrowserRouter, Link, Router} from "react-router-dom";
+
+const {Header, Footer, Sider, Content} = Layout;
 
 
 class SideBar extends React.Component {
@@ -24,37 +25,39 @@ class SideBar extends React.Component {
     render() {
         const {collapsed} = this.state;
         return (
-            <Sider collapsible={false} collapsed={collapsed} onCollapse={this.onCollapse} id={'sidebar_div'}>
-                <div id="logo"/>
-                <Menu
-                    id={'sidebar_menu'}
-                    theme="light"
-                    defaultSelectedKeys={['1']}
-                    mode="inline"
-                >
-                    <Menu.Item key="1" icon={<HomeOutlined/>}>
-                        Home
-                        <Link to='/'/>
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<BarChartOutlined/>}>
-                        Charts
-                        <Link to='/chart'/>
-                    </Menu.Item>
-                    {/*<SubMenu key="sub1" icon={<UserOutlined/>} title="User">*/}
-                    {/*    <Menu.Item key="3">User 1</Menu.Item>*/}
-                    {/*    <Menu.Item key="4"> User 2</Menu.Item>*/}
-                    {/*</SubMenu>*/}
-                    {/*<SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">*/}
-                    {/*    <Menu.Item key="6">Team 1</Menu.Item>*/}
-                    {/*    <Menu.Item key="8">Team 2</Menu.Item>*/}
-                    {/*</SubMenu>*/}
-                    <Menu.Item key="9" icon={<SettingOutlined/>}>
-                        Settings
-                        <Link to={"/settings"}/>
-                    </Menu.Item>
-                </Menu>
+            <>
 
-            </Sider>
+                <Sider collapsible={false} collapsed={collapsed} onCollapse={this.onCollapse} id={'sidebar_div'}>
+                    <div id="logo"/>
+                    <Menu
+                        id={'sidebar_menu'}
+                        theme="light"
+                        defaultSelectedKeys={['1']}
+                        mode="inline"
+                    >
+                        <Menu.Item key="1" icon={<HomeOutlined/>}>
+                            Home
+                            <Link to='/'/>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<BarChartOutlined/>}>
+                            Charts
+                            <Link to='/chart'/>
+                        </Menu.Item>
+                        {/*<SubMenu key="sub1" icon={<UserOutlined/>} title="User">*/}
+                        {/*    <Menu.Item key="3">User 1</Menu.Item>*/}
+                        {/*    <Menu.Item key="4"> User 2</Menu.Item>*/}
+                        {/*</SubMenu>*/}
+                        {/*<SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">*/}
+                        {/*    <Menu.Item key="6">Team 1</Menu.Item>*/}
+                        {/*    <Menu.Item key="8">Team 2</Menu.Item>*/}
+                        {/*</SubMenu>*/}
+                        <Menu.Item key="9" icon={<SettingOutlined/>}>
+                            Settings
+                                <Link to={"/settings"}/>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+            </>
         );
     }
 }
