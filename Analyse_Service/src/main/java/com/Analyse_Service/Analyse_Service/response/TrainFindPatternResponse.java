9e366eea4713c011cbaca0403b82ef416bc40980
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-18 12:01:06 +0200

    implemented training function response objects

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindPatternResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindPatternResponse.java
@@ -1,4 +1,15 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import java.util.ArrayList;
+
 public class TrainFindPatternResponse {
+    ArrayList<ArrayList> pattenList;
+
+    public TrainFindPatternResponse(ArrayList<ArrayList> pattenList){
+        this.pattenList = pattenList;
+    }
+
+    public ArrayList<ArrayList> getPattenList(){
+        return this.pattenList;
+    }
 }
--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindTrendsResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindTrendsResponse.java
@@ -1,4 +1,15 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import java.util.ArrayList;
+
 public class TrainFindTrendsResponse {
+    ArrayList<ArrayList> pattenList;
+
+    public TrainFindTrendsResponse(ArrayList<ArrayList> pattenList){
+        this.pattenList = pattenList;
+    }
+
+    public ArrayList<ArrayList> getPattenList(){
+        return this.pattenList;
+    }
 }
