8206b8d53bc8730be55fc89edf7c243336bc0c04
abDivergent <u15219900@tuks.co.za>
2021-08-19 04:03:49 +0200

    fixed infinite wait problem (kind of)

--- frontend/src/app/components/SideBar/SideBar.js
+++ frontend/src/app/components/SideBar/SideBar.js
@@ -2,58 +2,64 @@ import {Menu, Layout} from "antd";
 import {BarChartOutlined, HomeOutlined, SettingOutlined} from "@ant-design/icons";
 import UserInfoCard from "./UserInfoCard";
 import React, {Component} from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import ChartPage from "../../pages/ChartPage/components/ChartPage";
-import SettingsPage from "../../pages/SettingsPage/SettingsPage";
-import HomePage from "../../pages/HomePage/HomePage";
+import {BrowserRouter, Link, Router} from "react-router-dom";
 
 const {Header, Footer, Sider, Content} = Layout;
 
-export default function SideBar() {
-    return (
-        <Router>
-            <Sider collapsible={false} id={'sidebar_div'}>
-                <div id="logo"/>
-                <Menu
-                    id={'sidebar_menu'}
-                    theme="light"
-                    defaultSelectedKeys={['1']}
-                    mode="inline"
-                >
-                    <Menu.Item key="1" icon={<HomeOutlined/>}>
-                        Home
-                        <Link to='/'/>
-                    </Menu.Item>
 
-                    <Menu.Item key="2" icon={<BarChartOutlined/>}>
-                        Charts
-                        <Link to='/chart'/>
-                    </Menu.Item>
+class SideBar extends React.Component {
 
-                    <Menu.Item key="9" icon={<SettingOutlined/>}>
-                        Settings
-                        <Link to={"/settings"}/>
-                    </Menu.Item>
-                </Menu>
-                <Switch>
-                    <Route exact path="/">
-                        <HomePage/>
-                    </Route>
-                    <Route path="/chart">
-                        <ChartPage/>
-                    </Route>
-                    <Route path="/settings">
-                        <SettingsPage/>
-                    </Route>
-                </Switch>
-            </Sider>
-        </Router>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: false,
+        };
+    }
+
+    onCollapse = collapsed => {
+        console.log("IsCOllapsed: " + collapsed);
+        this.setState({collapsed});
+    };
+
+    render() {
+        const {collapsed} = this.state;
+        return (
+            <>
+
+                <Sider collapsible={false} collapsed={collapsed} onCollapse={this.onCollapse} id={'sidebar_div'}>
+                    <div id="logo"/>
+                    <Menu
+                        id={'sidebar_menu'}
+                        theme="light"
+                        defaultSelectedKeys={['1']}
+                        mode="inline"
+                    >
+                        <Menu.Item key="1" icon={<HomeOutlined/>}>
+                            Home
+                            <Link to='/'/>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<BarChartOutlined/>}>
+                            Charts
+                            <Link to='/chart'/>
+                        </Menu.Item>
+                        {/*<SubMenu key="sub1" icon={<UserOutlined/>} title="User">*/}
+                        {/*    <Menu.Item key="3">User 1</Menu.Item>*/}
+                        {/*    <Menu.Item key="4"> User 2</Menu.Item>*/}
+                        {/*</SubMenu>*/}
+                        {/*<SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">*/}
+                        {/*    <Menu.Item key="6">Team 1</Menu.Item>*/}
+                        {/*    <Menu.Item key="8">Team 2</Menu.Item>*/}
+                        {/*</SubMenu>*/}
+                        <Menu.Item key="9" icon={<SettingOutlined/>}>
+                            Settings
+                            <Link to={"/settings"}/>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+            </>
+        );
+    }
 }
 
-
+export default SideBar;
