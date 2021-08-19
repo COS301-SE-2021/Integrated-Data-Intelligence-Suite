5c3df458ff29dfc8e36e5ac5ff900409acde84f3
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 13:39:43 +0200

    bug-fixed: wrapped loginPage with browserRouter to pass unit test

--- frontend/src/app/tests/LoginPage.test.js
+++ frontend/src/app/tests/LoginPage.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import LoginPage from "../pages/LoginPage/LoginPage";
+import {BrowserRouter} from "react-router-dom";
 
 it('renders correctly', () => {
     const tree = renderer
-        .create(<LoginPage/>)
+        .create(<BrowserRouter>
+
+                <LoginPage/>
+            </BrowserRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
