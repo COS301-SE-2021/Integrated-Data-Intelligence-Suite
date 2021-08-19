c0a11a79009960e315d903bd158786e211584a2b
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 02:19:46 +0200

    bug-fix: Added browser router to Sidebar.js

--- frontend/src/app/components/SideBar/SideBar.js
+++ frontend/src/app/components/SideBar/SideBar.js
@@ -2,64 +2,75 @@ import {Menu, Layout} from "antd";
 import {BarChartOutlined, HomeOutlined, SettingOutlined} from "@ant-design/icons";
 import UserInfoCard from "./UserInfoCard";
 import React, {Component} from 'react';
-import {BrowserRouter, Link, Router} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import ChartPage from "../../pages/ChartPage/components/ChartPage";
+import SettingsPage from "../../pages/SettingsPage/SettingsPage";
+import HomePage from "../../pages/HomePage/HomePage";
 
 const {Header, Footer, Sider, Content} = Layout;
 
 
-class SideBar extends React.Component {
+// class SideBar extends React.Component {
+export default function SideBar() {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         collapsed: false,
+    //     };
+    // }
 
-    constructor(props) {
-        super(props);
+    // onCollapse = collapsed => {
+    //     console.log("IsCOllapsed: " + collapsed);
+    //     this.setState({collapsed});
+    // };
 
-        this.state = {
-            collapsed: false,
-        };
-    }
+    // render() {
+    //     const {collapsed} = this.state;
+    return (
+        <Router>
+            <Sider collapsible={false} id={'sidebar_div'}>
+                <div id="logo"/>
+                <Menu
+                    id={'sidebar_menu'}
+                    theme="light"
+                    defaultSelectedKeys={['1']}
+                    mode="inline"
+                >
+                    <Menu.Item key="1" icon={<HomeOutlined/>}>
+                        Home
+                        <Link to='/'/>
+                    </Menu.Item>
 
-    onCollapse = collapsed => {
-        console.log("IsCOllapsed: " + collapsed);
-        this.setState({collapsed});
-    };
+                    <Menu.Item key="2" icon={<BarChartOutlined/>}>
+                        Charts
+                        <Link to='/chart'/>
+                    </Menu.Item>
 
-    render() {
-        const {collapsed} = this.state;
-        return (
-            <>
-
-                <Sider collapsible={false} collapsed={collapsed} onCollapse={this.onCollapse} id={'sidebar_div'}>
-                    <div id="logo"/>
-                    <Menu
-                        id={'sidebar_menu'}
-                        theme="light"
-                        defaultSelectedKeys={['1']}
-                        mode="inline"
-                    >
-                        <Menu.Item key="1" icon={<HomeOutlined/>}>
-                            Home
-                            <Link to='/'/>
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<BarChartOutlined/>}>
-                            Charts
-                            <Link to='/chart'/>
-                        </Menu.Item>
-                        {/*<SubMenu key="sub1" icon={<UserOutlined/>} title="User">*/}
-                        {/*    <Menu.Item key="3">User 1</Menu.Item>*/}
-                        {/*    <Menu.Item key="4"> User 2</Menu.Item>*/}
-                        {/*</SubMenu>*/}
-                        {/*<SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">*/}
-                        {/*    <Menu.Item key="6">Team 1</Menu.Item>*/}
-                        {/*    <Menu.Item key="8">Team 2</Menu.Item>*/}
-                        {/*</SubMenu>*/}
-                        <Menu.Item key="9" icon={<SettingOutlined/>}>
-                            Settings
-                                <Link to={"/settings"}/>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-            </>
-        );
-    }
+                    <Menu.Item key="9" icon={<SettingOutlined/>}>
+                        Settings
+                        <Link to={"/settings"}/>
+                    </Menu.Item>
+                </Menu>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage/>
+                    </Route>
+                    <Route path="/chart">
+                        <ChartPage/>
+                    </Route>
+                    <Route path="/settings">
+                        <SettingsPage/>
+                    </Route>
+                </Switch>
+            </Sider>
+        </Router>
+    );
 }
 
-export default SideBar;
+
