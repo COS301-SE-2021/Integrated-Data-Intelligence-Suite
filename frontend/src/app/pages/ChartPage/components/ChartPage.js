f4ce3945f0c112b885c924325d1cf54e947e2497
abDivergent <u15219900@tuks.co.za>
2021-08-19 22:09:03 +0200

    updated side-bar to dynamically highlight the active component

--- frontend/src/app/pages/ChartPage/components/ChartPage.js
+++ frontend/src/app/pages/ChartPage/components/ChartPage.js
@@ -6,7 +6,7 @@ import {
     Card,
     Typography
 } from 'antd';
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import MapCard from "./MapCard/MapCard";
 import DetailsCard from "./DetailsCard/DetailsCard";
 import NetworkGraphCard from "./NetworkGraph/NetworkGraphCard";
@@ -62,85 +62,90 @@ constructor(props){
     }
 
     render() {
-        return (
-            <>
-                <Switch>
-                    <Route exact path='/chart'>
-                        <Layout
-                            id={'outer_layout'}
-                            className={'chart-page'}
-                        >
-                            <Header id={'top_bar'}>
-                                {/*<Title level={1}>Chart Page Title</Title>*/}
-                                <SearchBar
-                                    text={this.state.text}
-                                    handleTextChange={this.handleTextChange}
-                                />
-                                <UserInfoCard
-                                    name="s"
-                                />
-                            </Header>
-                            <Layout id={'inner_layout_div'}>
-                            <SideBar/>
+        if (this.state.user){
+            return (
+                <>
+                    <Switch>
+                        <Route exact path='/chart'>
+                            <Layout
+                                id={'outer_layout'}
+                                className={'chart-page'}
+                            >
+                                <Header id={'top_bar'}>
+                                    {/*<Title level={1}>Chart Page Title</Title>*/}
+                                    <SearchBar
+                                        text={this.state.text}
+                                        handleTextChange={this.handleTextChange}
+                                    />
+                                    <UserInfoCard
+                                        name="s"
+                                    />
+                                </Header>
+                                <Layout id={'inner_layout_div'}>
+                                    <SideBar/>
 
-                                {/*The Map Graph*/}
-                                <Content id={'content_section'}>
-                                    <Layout
-                                        id={'map_card_content_layout_div'}
-                                        className={'map_card'}
-                                    >
-                                        <Content
-                                            id={'map_card_content'}
+                                    {/*The Map Graph*/}
+                                    <Content id={'content_section'}>
+                                        <Layout
+                                            id={'map_card_content_layout_div'}
                                             className={'map_card'}
                                         >
-                                            <MapCard text={this.state.text}/>
-                                        </Content>
+                                            <Content
+                                                id={'map_card_content'}
+                                                className={'map_card'}
+                                            >
+                                                <MapCard text={this.state.text}/>
+                                            </Content>
 
-                                        <Sider
-                                            id={'map_card_sidebar'}
-                                            className={'map_card'}
-                                            style={{display:"none"}}
-                                        >
-                                            {<DetailsCard/>}
-                                        </Sider>
-                                    </Layout>
+                                            <Sider
+                                                id={'map_card_sidebar'}
+                                                className={'map_card'}
+                                                style={{display:"none"}}
+                                            >
+                                                {<DetailsCard/>}
+                                            </Sider>
+                                        </Layout>
 
-                                    {/*The Network Graph*/}
-                                    <Layout
-                                        id={'network_graph_layout_div'}
-                                        className={'network_card'}
-                                    >
-                                        <Content
-                                            id={'network_graph_card_content'}
+                                        {/*The Network Graph*/}
+                                        <Layout
+                                            id={'network_graph_layout_div'}
                                             className={'network_card'}
                                         >
-                                            <NetworkGraphCard text={this.state.text}/>
-                                        </Content>
-                                    </Layout>
+                                            <Content
+                                                id={'network_graph_card_content'}
+                                                className={'network_card'}
+                                            >
+                                                <NetworkGraphCard text={this.state.text}/>
+                                            </Content>
+                                        </Layout>
 
-                                    {/*The timeline Graph*/}
-                                    <Layout
-                                        id={'timeline_graph_div'}
-                                        className={'timeline_card'}
-                                    >
-                                        <Content
-                                            id={'timeline_graph_card_content'}
+                                        {/*The timeline Graph*/}
+                                        <Layout
+                                            id={'timeline_graph_div'}
                                             className={'timeline_card'}
                                         >
-                                            <TimelineGraph
-                                                text={this.state.text}
-                                                key={this.state.text}
-                                            />
-                                        </Content>
-                                    </Layout>
+                                            <Content
+                                                id={'timeline_graph_card_content'}
+                                                className={'timeline_card'}
+                                            >
+                                                <TimelineGraph
+                                                    text={this.state.text}
+                                                    key={this.state.text}
+                                                />
+                                            </Content>
+                                        </Layout>
 
-                                </Content>
+                                    </Content>
+                                </Layout>
                             </Layout>
-                        </Layout>
-                    </Route>
-                </Switch>
-            </>
-        );
+                        </Route>
+                    </Switch>
+                </>
+            );
+        }else{
+            return <Redirect to={'/login'}/>
+        }
+
     }
 
 }
