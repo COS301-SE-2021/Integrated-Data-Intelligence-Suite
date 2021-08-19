79149f3517a743d0ff6f4c79c6d05173dc1397fa
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 13:34:53 +0200

    bug-fixed: wrapped logoutPage with <Link> child with <BrowserRouter> to pass unit test

--- frontend/src/app/tests/LogoutPage.test.js
+++ frontend/src/app/tests/LogoutPage.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import LogoutPage from "../pages/LogoutPage/LogoutPage";
+import {BrowserRouter} from "react-router-dom";
 
 it('renders correctly', () => {
     const tree = renderer
-        .create(<LogoutPage/>)
+        .create(<BrowserRouter>
+                <LogoutPage/>
+            </BrowserRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
