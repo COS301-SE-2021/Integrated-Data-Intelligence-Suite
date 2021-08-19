92e8adb158b9e15ca6152827f2fa12db02611a70
abDivergent <u15219900@tuks.co.za>
2021-08-19 22:07:01 +0200

    fixed Logout text

--- frontend/src/app/tests/ExitMenuTooltip.test.js
+++ frontend/src/app/tests/ExitMenuTooltip.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import ExitMenuTooltip from "../components/ExitMenuTooltip/ExitMenuTooltip";
+import {BrowserRouter} from "react-router-dom";
 
 it('ExitMenuTooltip renders correctly', () => {
     const tree = renderer
-        .create(<ExitMenuTooltip/>)
+        .create(
+            <BrowserRouter>
+                <ExitMenuTooltip/>
+            </BrowserRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
