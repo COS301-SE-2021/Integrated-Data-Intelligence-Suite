315f2403289d5fab4a43870a4d9a3c9ea59643ed
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 13:42:22 +0200

    bug-fixed: wrapped homePage with browserRouter to pass unit test

--- frontend/src/app/tests/HomePage.test.js
+++ frontend/src/app/tests/HomePage.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import HomePage from "../pages/HomePage/HomePage";
+import {BrowserRouter} from "react-router-dom";
 
 it('renders correctly', () => {
     const tree = renderer
-        .create(<HomePage/>)
+        .create(<BrowserRouter>
+
+                <HomePage/>
+            </BrowserRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
