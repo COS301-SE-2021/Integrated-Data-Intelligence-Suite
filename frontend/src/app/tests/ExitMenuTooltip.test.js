8b974e1f32fc5aa908831d0dee0e9410243d734b
Myron Lopes <u18246932@tuks.co.za>
2021-08-19 22:53:28 +0200

    fixed the exitMenuTooltip unit test

--- frontend/src/app/tests/ExitMenuTooltip.test.js
+++ frontend/src/app/tests/ExitMenuTooltip.test.js
@@ -2,25 +2,30 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import ExitMenuTooltip from "../components/ExitMenuTooltip/ExitMenuTooltip";
 import {unmountComponentAtNode} from "react-dom";
+import {BrowserRouter} from "react-router-dom";
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
 });
 
 
 it('ExitMenuTooltip renders correctly', () => {
     const tree = renderer
-        .create(<ExitMenuTooltip/>)
+        .create(
+            <BrowserRouter>
+                <ExitMenuTooltip/>
+            </BrowserRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
