ed5d541d1c781480f7e6f9697411ab4ee82ed721
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-19 20:38:39 +0200

    completed unit test of finding user by id and email, for user Service

--- User_Service/src/test/java/com/User_Service/User_Service/repository/UserRepositoryTest.java
+++ User_Service/src/test/java/com/User_Service/User_Service/repository/UserRepositoryTest.java
@@ -5,6 +5,9 @@ import com.User_Service.User_Service.rri.Permission;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.Modifying;
@@ -38,25 +41,51 @@ public class UserRepositoryTest {
         userRepository.save(testUser);
 
         Optional<User> testUser2 = userRepository.findUserByUsername("UserNameTest");
-        Assertions.assertEquals(testUser,testUser);
+        Assertions.assertEquals(testUser,testUser2.get());
     }
 
     @Test
     @DisplayName("find User By Email")
-    public void findUserByEmail(String email){
+    public void findUserByEmail(){
         //findUserByEmail(String email)
+
+        User testUser = new User();
+
+        testUser.setFirstName("FirstNameTest");
+        testUser.setLastName("LastNameTest");
+        testUser.setUsername("UserNameTest");
+        testUser.setEmail("email@test.com");
+        testUser.setPassword("passwordTest");
+        testUser.setPermission(Permission.VIEWING);
+
+        userRepository.save(testUser);
+
+        Optional<User> testUser2 = userRepository.findUserByEmail("email@test.com");
+        Assertions.assertEquals(testUser,testUser2.get());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("find User By Id")
     public void findUserById(UUID userID){
-        //findUserById(UUID userID)
+        
     }
 
     @Test
     @DisplayName("update Permission")
     public void updatePermission(@Param("id")UUID userID, @Param("perm") Permission perm){
         //updatePermission(@Param("id")UUID userID, @Param("perm") Permission perm)
+        User testUser = new User();
+
+        testUser.setFirstName("FirstNameTest");
+        testUser.setLastName("LastNameTest");
+        testUser.setUsername("UserNameTest");
+        testUser.setEmail("email@test.com");
+        testUser.setPassword("passwordTest");
+        testUser.setPermission(Permission.VIEWING);
+
+        userRepository.save(testUser);
+
+
     }
 
     @Test
