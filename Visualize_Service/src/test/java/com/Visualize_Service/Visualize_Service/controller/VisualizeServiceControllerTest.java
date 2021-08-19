d291d24b7fa0723770ffdd86bc476faa8dfdc74b
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-19 23:53:39 +0200

    setup changes to visualise service controller, enable integration test changes

--- Visualize_Service/src/test/java/com/Visualize_Service/Visualize_Service/controller/VisualizeServiceControllerTest.java
+++ Visualize_Service/src/test/java/com/Visualize_Service/Visualize_Service/controller/VisualizeServiceControllerTest.java
@@ -3,22 +3,24 @@ package com.Visualize_Service.Visualize_Service.controller;
 import com.Visualize_Service.Visualize_Service.VisualizeServiceApplication;
 import com.Visualize_Service.Visualize_Service.service.VisualizeServiceImpl;
 
-import org.junit.Before;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = VisualizeServiceApplication.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(VisualizeServiceController.class)
 public class VisualizeServiceControllerTest {
     @InjectMocks
     private VisualizeServiceController controller;
@@ -28,10 +30,10 @@ public class VisualizeServiceControllerTest {
 
     private MockMvc mockMvc;
 
-    @Before
+    /*@Before
     public void setup(){
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-    }
+    }*/
 
     @Test
     @DisplayName("When visualize is requested")
