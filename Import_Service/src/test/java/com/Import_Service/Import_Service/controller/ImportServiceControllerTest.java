5d486e86dd41bb416b6850414b78411a35f8e985
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-20 02:57:51 +0200

    setup controller test class for import service ( only needs test functions)

--- Import_Service/src/test/java/com/Import_Service/Import_Service/controller/ImportServiceControllerTest.java
+++ Import_Service/src/test/java/com/Import_Service/Import_Service/controller/ImportServiceControllerTest.java
@@ -38,95 +38,21 @@ import java.util.Random;
 public class ImportServiceControllerTest {
 
     @Autowired
-    private ImportServiceController controller;
-
-    @Autowired
     private ImportServiceImpl service;
 
+    @Autowired
     private MockMvc mockMvc;
-
-
-    /*@Before
-    public void setup(){
-        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-    }*/
 
 
     @Test
     @DisplayName("When import is requested")
     public void importRequest() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/Import/importData")
-                .content(asJsonString(new ImportDataRequest(generateRandomString(), generateRandNum())))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-        ).andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 
     @Test
     @DisplayName("When dated data is requested")
     public void  datedDataRequest() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/Import/importDatedData")
-                .content(asJsonString(new ImportTwitterRequest(generateRandomString(), generateRandomDate() , LocalDate.now())))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-        ).andExpect(MockMvcResultMatchers.status().is4xxClientError());
-    }
-
-
-    private String generateRandomString(){
-
-
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        StringBuilder sb = new StringBuilder();
-
-        Random random = new Random();
-
-        int length = 7;
-
-        for(int i = 0; i < length; i++) {
-
-            int index = random.nextInt(alphabet.length());
-
-            char randomChar = alphabet.charAt(index);
-
-            sb.append(randomChar);
-        }
-        return sb.toString();
 
     }
-
-    private int generateRandNum(){
-        int max = 28;
-        int min = 1;
-        int range = max - min + 1;
-        return (int)(Math.random() * range) + min;
-    }
-
-    private LocalDate generateRandomDate(){
-        int max = 28;
-        int min = 1;
-        int range = max - min + 1;
-        int day = (int)(Math.random() * range) + min;
-
-        max = 12;
-        min = 1;
-        range = max - min + 1;
-        int month = (int)(Math.random() * range) + min;
-
-        int year = 2021- month;
-        return LocalDate.of(year,month,day);
-
-    }
-
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
