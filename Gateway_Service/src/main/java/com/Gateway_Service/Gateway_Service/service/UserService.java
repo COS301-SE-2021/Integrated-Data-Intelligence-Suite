2a2d5ed1c4dc414cf80e569bc0f2b5a380d890bf
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-20 02:04:10 +0200

    implemented user service client, communicates with endpoints

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/UserService.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/UserService.java
@@ -1,6 +1,9 @@
 package com.Gateway_Service.Gateway_Service.service;
 
 import com.Gateway_Service.Gateway_Service.dataclass.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,67 +17,145 @@ public class UserService {
     /**
      * This functions sends a request to the user service using REST to change the permission
      * of a specific user.
-     * @param request This class contains the required information of a specific user to
+     * @param userRequest This class contains the required information of a specific user to
      *                change the permission of that user.
      * @return This class will contain the information whether or not the request was successfull
      *         or not.
      */
-    public ManagePermissionsResponse managePermissions(ManagePermissionsRequest request) {
-        HttpHeaders requestHeaders = new HttpHeaders();
+    public ManagePermissionsResponse managePermissions(ManagePermissionsRequest userRequest) {
+        /*HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<ManagePermissionsRequest> requestEntity = new HttpEntity<ManagePermissionsRequest>(request, requestHeaders);
         ResponseEntity<ManagePermissionsResponse> responseEntity = restTemplate.exchange("http://User-Service/User/changepermission", HttpMethod.POST, requestEntity, ManagePermissionsResponse.class);
-        return responseEntity.getBody();
+        return responseEntity.getBody();*/
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(mapper.writeValueAsString(userRequest),requestHeaders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        ManagePermissionsResponse userResponse = restTemplate.postForObject("http://User-Service/User/changepermission", request, ManagePermissionsResponse.class);
+
+        return userResponse;
     }
 
     /**
      * This function sends a request to the user service to get all users saved on the database.
-     * @param request ***
+     * @param userRequest ***
      * @return This class contains a list of users saved on the system.
      */
-    public GetAllUsersResponse getAllUsers(GetAllUsersRequest request) {
+    public GetAllUsersResponse getAllUsers(GetAllUsersRequest userRequest) {
+
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<GetAllUsersRequest> requestEntity = new HttpEntity<GetAllUsersRequest>(request, requestHeaders);
-        ResponseEntity<GetAllUsersResponse> responseEntity = restTemplate.exchange("http://User-Service/User/getAll", HttpMethod.GET, requestEntity, GetAllUsersResponse.class);
-        return responseEntity.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(mapper.writeValueAsString(userRequest),requestHeaders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        GetAllUsersResponse userResponse = restTemplate.postForObject("http://User-Service/User/getAll", request, GetAllUsersResponse.class);
+
+        return userResponse;
     }
 
     /**
      * This function is used to connect to the user service to allow the user to register
      * to the system. It sends a request to the user controller and send the request
      * class to user service.
-     * @param request This class contains all the information of the user to be saved.
+     * @param userRequest This class contains all the information of the user to be saved.
      * @return This class contains the information if the saving of the user was successful.
      */
-    public RegisterResponse register(RegisterRequest request) {
-        HttpHeaders requestHeaders = new HttpHeaders();
+    public RegisterResponse register(RegisterRequest userRequest) {
+        /*HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<RegisterRequest> requestEntity = new HttpEntity<>(request, requestHeaders);
         ResponseEntity<RegisterResponse> responseEntity = restTemplate.exchange("http://User-Service/User/register", HttpMethod.POST, requestEntity, RegisterResponse.class);
-        return responseEntity.getBody();
+        return responseEntity.getBody();*/
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(mapper.writeValueAsString(userRequest),requestHeaders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        RegisterResponse userResponse = restTemplate.postForObject("http://User-Service/User/register", request, RegisterResponse.class);
+
+        return userResponse;
     }
 
-    public GetUserResponse getUser(GetUserRequest request){
-        HttpHeaders requestHeaders = new HttpHeaders();
+    public GetUserResponse getUser(GetUserRequest userRequest){
+        /*HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<GetUserRequest> requestEntity = new HttpEntity<GetUserRequest>(request, requestHeaders);
         ResponseEntity<GetUserResponse> responseEntity = restTemplate.exchange("http://User-Service/User/getUser", HttpMethod.POST, requestEntity, GetUserResponse.class);
-        return responseEntity.getBody();
+        return responseEntity.getBody();*/
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(mapper.writeValueAsString(userRequest),requestHeaders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        GetUserResponse userResponse = restTemplate.postForObject("http://User-Service/User/getUser", request, GetUserResponse.class);
+
+        return userResponse;
     }
     /**
      * This function is used to connect to the user service to allow the user to login
      * to the system. It sends a request to the user controller and send the request
      * class to user service.
-     * @param request This class contains the email and password of the user to login.
+     * @param userRequest This class contains the email and password of the user to login.
      * @return This class contains the information if the logging process was successful.
      */
-    public LoginResponse login(LoginRequest request) {
-        HttpHeaders requestHeaders = new HttpHeaders();
+    public LoginResponse login(LoginRequest userRequest) {
+        /*HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<LoginRequest> requestEntity = new HttpEntity<>(request, requestHeaders);
         ResponseEntity<LoginResponse> responseEntity = restTemplate.exchange("http://User-Service/User/login", HttpMethod.POST, requestEntity, LoginResponse.class);
-        return responseEntity.getBody();
+        return responseEntity.getBody();*/
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true);
+
+        HttpEntity<String> request = null;
+        try {
+            request = new HttpEntity<>(mapper.writeValueAsString(userRequest),requestHeaders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        LoginResponse userResponse = restTemplate.postForObject("http://User-Service/User/login", request, LoginResponse.class);
+
+        return userResponse;
     }
 }
 
