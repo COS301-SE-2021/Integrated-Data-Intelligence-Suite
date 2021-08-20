aaed88ef8e2ccad25a4df8ca24accdc6f064cd45
u18003517-Rhuli <u18003517@tuks.co.za>
2021-08-20 03:43:11 +0200

    implemented integration tests to ImportServiceController Test

--- Import_Service/src/test/java/com/Import_Service/Import_Service/controller/ImportServiceControllerTest.java
+++ Import_Service/src/test/java/com/Import_Service/Import_Service/controller/ImportServiceControllerTest.java
@@ -1,11 +1,15 @@
 package com.Import_Service.Import_Service.controller;
 
 import com.Import_Service.Import_Service.ImportServiceApplication;
+import com.Import_Service.Import_Service.dataclass.ImportedData;
 import com.Import_Service.Import_Service.exception.InvalidImporterRequestException;
 import com.Import_Service.Import_Service.request.ImportDataRequest;
 import com.Import_Service.Import_Service.request.ImportTwitterRequest;
+import com.Import_Service.Import_Service.response.ImportDataResponse;
 import com.Import_Service.Import_Service.service.ImportServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.Gson;
 
 import org.aspectj.lang.annotation.Before;
@@ -26,12 +30,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ImportServiceController.class)
@@ -44,7 +53,7 @@ public class ImportServiceControllerTest {
     private MockMvc mockMvc;
 
 
-    @Test
+    /*@Test
     @DisplayName("When import is requested")
     public void importRequest() throws Exception {
 
@@ -54,5 +63,77 @@ public class ImportServiceControllerTest {
     @DisplayName("When dated data is requested")
     public void  datedDataRequest() throws Exception {
 
+    }*/
+
+    @Test
+    @DisplayName("When import check connection")
+    public void importRequestConnection() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/Import/importData"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+
+    @Test
+    @DisplayName("When importRequest is Null")
+    public void importDataNullRequest() throws Exception {
+
+        ImportDataRequest importDataRequest = new ImportDataRequest("testKey",50);
+
+        ObjectMapper mapper = new ObjectMapper();//new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true); //increase chances of serializing
+
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson=ow.writeValueAsString(importDataRequest);
+
+        ArrayList<ImportedData> importedData = new ArrayList<>();
+        ImportedData importedData1 = new ImportedData();
+        importedData.add(importedData1);
+
+        ImportDataResponse importDataResponse = new ImportDataResponse(importedData);
+        when(service.importData(any(ImportDataRequest.class))).thenReturn(importDataResponse);
+
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/Import/importData")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(results -> Assertions.assertFalse(results.getResolvedException() instanceof InvalidImporterRequestException));
+
+        ImportDataResponse returnClass = mapper.readValue(result.andReturn().getResponse().getContentAsString(), ImportDataResponse.class);
+
+        Assertions.assertNotNull(returnClass);
+    }
+
+
+    @Test
+    @DisplayName("When importRequest is Success")
+    public void importDataSuccessfulRequest() throws Exception {
+
+        ImportDataRequest importDataRequest = new ImportDataRequest("testKey",50);
+
+        ObjectMapper mapper = new ObjectMapper();//new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false); //root name of class, same root value of json
+        mapper.configure(SerializationFeature.EAGER_SERIALIZER_FETCH, true); //increase chances of serializing
+
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson=ow.writeValueAsString(importDataRequest);
+
+        ArrayList<ImportedData> importedData = new ArrayList<>();
+        ImportedData importedData1 = new ImportedData();
+        importedData.add(importedData1);
+
+        ImportDataResponse importDataResponse = new ImportDataResponse(importedData);
+        when(service.importData(any(ImportDataRequest.class))).thenReturn(importDataResponse);
+
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/Import/importData")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        ImportDataResponse returnClass = mapper.readValue(result.andReturn().getResponse().getContentAsString(), ImportDataResponse.class);
+
+        Assertions.assertNotNull(returnClass);
     }
 }
