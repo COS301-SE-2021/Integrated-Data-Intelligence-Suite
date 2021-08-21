05953c31c513136768127f428b92d2de41d7d362
shrey-99 <shreymandalia@gmail.com>
2021-08-21 19:25:25 +0200

    created register admin response and request classes

---
+++ User_Service/src/main/java/com/User_Service/User_Service/request/RegisterAdminRequest.java
@@ -0,0 +1,61 @@
+package com.User_Service.User_Service.request;
+
+public class RegisterAdminRequest {
+    String username;
+    String firstName;
+    String lastName;
+    String password;
+    String email;
+
+    public RegisterAdminRequest() {
+
+    }
+
+    public RegisterAdminRequest(String username, String firstName, String lastName, String password, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastname(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
