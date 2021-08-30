c4bd02f6b7703bc71c3f0f97ddeaaaa3fc26bc51
shrey-99 <shreymandalia@gmail.com>
2021-08-30 22:23:39 +0200

    created repository for APISource

---
+++ Import_Service/src/main/java/com/Import_Service/Import_Service/repository/ApiSourceRepository.java
@@ -0,0 +1,7 @@
+package com.Import_Service.Import_Service.repository;
+
+import com.Import_Service.Import_Service.dataclass.APISource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApiSourceRepository extends JpaRepository<APISource, Long> {
+}
