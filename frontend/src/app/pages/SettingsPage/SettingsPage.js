23a6a53a2a60d4bbe1980987f6b173f385551efe
abDivergent <u15219900@tuks.co.za>
2021-09-01 23:32:41 +0200

    changed settings to now show a list of data sources rather than a single data source

--- frontend/src/app/pages/SettingsPage/SettingsPage.js
+++ frontend/src/app/pages/SettingsPage/SettingsPage.js
@@ -4,16 +4,17 @@ import {
 } from 'antd';
 import { CloseCircleTwoTone } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
+import DataSourceList from '../../components/DataSourceList/DataSourceList';
 import Permissions from '../PermissionsPage/Permissions';
-import AddDataSource from '../AddDataSourcePage/AddDataSource';
+// import AddDataSource from '../AddDataSourcePage/AddDataSource';
 
 const { Content } = Layout;
 
 const setActive = (component) => {
   const options = document.getElementsByClassName('option');
-  console.log(options);
-  for (let i = 0; i < options.length; i++) {
-    console.log(options[i].id);
+  // console.log(options);
+  for (let i = 0; i < options.length; i += 1) {
+    // console.log(options[i].id);
     if (options[i].id === component) {
       options[i].className = 'option active';
     } else {
@@ -36,13 +37,15 @@ const SettingsPage = () => {
   const [component, setComponent] = useState('Permissions');
   const [user, setUser] = useState(getLocalUser());
   const history = useHistory();
+  const colorOne = '#E8E8E9';
 
   return (
       <Layout className="bodyDiv">
           <div className="header white-background" />
           <Content id="settings-container" className="outer-container" style={{ margin: '0', minHeight: '100vh' }}>
               <Row className="row">
-                  <Col style={{ padding: '30px 20px' }} className="left-column" flex="200px">
+                  <Col flex="auto" style={{ opacity: 0 }}>.</Col>
+                  <Col style={{ padding: '30px 10px' }} className="left-column" flex="160px">
 
                       { user && user.isAdmin && <div id="Permissions" className="option active" onClick={() => setComponent('Permissions')}>Permissions</div>}
                       {user && user.isAdmin && <Divider />}
@@ -53,19 +56,22 @@ const SettingsPage = () => {
                       <div id="Account" className="option" onClick={() => setComponent('Account')}>Account</div>
                       <Divider />
 
-                      <div id="Add Source" className="option" onClick={() => setComponent('Add Source')}>Add Source</div>
+                      <div id="Data Sources" className="option" onClick={() => setComponent('Data Sources')}>Data Sources</div>
                   </Col>
-                  <Col style={{ padding: '0 0px 30px 0', backgroundColor: '#eff0f0' }} className="right-column" flex="auto">
-                      <div className="top-padding"><CloseCircleTwoTone className="back-button" onClick={() => history.go(-1)} /></div>
-                      { component === 'Permissions' && user && user.isAdmin && setActive(component) && <Permissions />}
-                      { component === 'Profile' && setActive(component) && (
+                  <Col style={{ padding: '0 0px 30px 0', backgroundColor: colorOne }} className="right-column" flex="600px">
                       <div>
-                          {' '}
-                          <h1>Page not implemented</h1>
+                          <div className="component-title">
+                              {component}
+                              <CloseCircleTwoTone className="back-button" onClick={() => history.go(-1)} />
+                          </div>
+                          { component === 'Permissions' && user && user.isAdmin && setActive(component) && <Permissions />}
+                          { component === 'Profile' && setActive(component) && <div><h1>Page not implemented</h1></div>}
+                          { component === 'Account' && setActive(component) && <div><h1>Page not implemented</h1></div>}
+                          { component === 'Data Sources' && setActive(component) && <DataSourceList /> }
                       </div>
-            )}
-                      { component === 'Account' && setActive(component) && <div><h1>Page not implemented</h1></div>}
-                      { component === 'Add Source' && setActive(component) && <AddDataSource /> }
+                  </Col>
+                  <Col flex="auto" style={{ backgroundColor: colorOne, color: colorOne }}>
+                      <div>.</div>
                   </Col>
               </Row>
           </Content>
