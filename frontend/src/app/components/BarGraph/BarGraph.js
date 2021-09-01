d24d3a72ba3a2acf64425c3f7928fc034d146d5b
Myron Lopes <u18246932@tuks.co.za>
2021-09-01 02:07:24 +0200

    added mock data for bar graph

---
+++ frontend/src/app/components/BarGraph/BarGraph.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryLine, VictoryTheme } from 'victory';
+import LineGraphDataPositive from '../../Mocks/LineGraphDataPositive.json';
+
+function BarGraph(props) {
+    return (
+        <>
+            <div className={'graph-container'}>
+                <VictoryChart>
+                    <VictoryBar
+                        horizontal
+                        alignment="middle"
+                        style={{
+                            data: { fill: '#c43a31' }
+                        }}
+                        data={props.graphData}
+                        labels={['10%', '15%', '40%', '20%', '45%']}
+                    />
+
+                    <VictoryAxis
+                        standalone={false}
+                        // tickValues={['May', 'June', 'July', 'Aug', 'Sept']}
+                        style={{
+                            ticks: { stroke: 'transparent' },
+                            // tickLabels: { fill: 'transparent' }
+                        }}
+                    />
+                </VictoryChart>
+            </div>
+        </>
+    );
+}
+
+export default BarGraph;
+
