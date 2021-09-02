d52d2134f7dfe388d06bd2d6e86e0710075f6d20
Myron Lopes <u18246932@tuks.co.za>
2021-09-02 20:48:02 +0200

    reverted sidebar button colors to neon blue

--- frontend/src/app/pages/ChartPage/ChartPage.js
+++ frontend/src/app/pages/ChartPage/ChartPage.js
@@ -87,94 +87,94 @@ class ChartPage extends Component {
                     <Switch>
                         <Route exact path="/chart">
                             <Layout
-                              id="outer_layout"
-                              className="chart-page"
+                                id="outer_layout"
+                                className="chart-page"
                             >
-                                <SideBar />
+                                <SideBar/>
                                 <Layout id="inner_layout_div">
                                     <Header id="top_bar">
                                         {/* <Title level={1}>Chart Page Title</Title> */}
                                         <SearchBar
-                                          text={this.state.text}
-                                          handleTextChange={this.handleTextChange}
+                                            text={this.state.text}
+                                            handleTextChange={this.handleTextChange}
                                         />
                                         <UserInfoCard
-                                          name="s"
+                                            name="s"
                                         />
                                     </Header>
 
                                     <Content id="content_section">
                                         <Layout
-                                          id="map_card_content_layout_div"
-                                          className="map_card"
+                                            id="map_card_content_layout_div"
+                                            className="map_card"
                                         >
                                             <div
-                                              id="indicator-container"
+                                                id="indicator-container"
                                             >
                                                 <IndicatorCard
-                                                  indicatorTitle="Average Sentiment"
-                                                  indicatorValue="Very Bad"
-                                                  showArrow
-                                                  percentChange="-27%"
-                                                  graphComponent={(
-                                                      <LineGraph
-                                                        graphData={LineGraphDataNegative}
-                                                        lineColor="#EF062D"
-                                                      />
+                                                    indicatorTitle="Average Sentiment"
+                                                    indicatorValue="Very Bad"
+                                                    showArrow
+                                                    percentChange="-27%"
+                                                    graphComponent={(
+                                                        <LineGraph
+                                                            graphData={LineGraphDataNegative}
+                                                            lineColor="#EF062D"
+                                                        />
                                                     )}
                                                 />
 
                                                 <IndicatorCard
-                                                  indicatorTitle="Number of Mentions"
-                                                  indicatorValue="246K"
-                                                  percentChange="+69%"
-                                                  showArrow
-                                                  graphComponent={(
-                                                      <LineGraph
-                                                        graphData={LineGraphDataPositive}
-                                                        lineColor="#009966"
-                                                      />
+                                                    indicatorTitle="Number of Mentions"
+                                                    indicatorValue="246K"
+                                                    percentChange="+69%"
+                                                    showArrow
+                                                    graphComponent={(
+                                                        <LineGraph
+                                                            graphData={LineGraphDataPositive}
+                                                            lineColor="#009966"
+                                                        />
                                                     )}
                                                 />
                                                 <IndicatorCard
-                                                  indicatorTitle="Total Sentiment"
-                                                  showArrow={false}
-                                                  graphComponent={(
-                                                      <PieChart
-                                                        graphData={PieChartMockData}
-                                                      />
+                                                    indicatorTitle="Total Sentiment"
+                                                    showArrow={false}
+                                                    graphComponent={(
+                                                        <PieChart
+                                                            graphData={PieChartMockData}
+                                                        />
                                                     )}
                                                 />
                                             </div>
 
                                             <div id="map-card-container">
                                                 <Card
-                                                  id="map-card"
-                                                  title="World Map"
+                                                    id="map-card"
+                                                    title="World Map"
                                                 >
-                                                    <MapCard text={this.state.text} />
+                                                    <MapCard text={this.state.text}/>
                                                 </Card>
 
                                                 <div id="map-details-container">
                                                     <IndicatorCard
-                                                      cardID="top-country-indicator-card"
-                                                      indicatorTitle="Engagement by Location"
-                                                      showArrow={false}
-                                                      graphComponent={(
-                                                          <BarGraph
-                                                            graphData={BarGraphMockData}
-                                                          />
+                                                        cardID="top-country-indicator-card"
+                                                        indicatorTitle="Engagement by Location"
+                                                        showArrow={false}
+                                                        graphComponent={(
+                                                            <BarGraph
+                                                                graphData={BarGraphMockData}
+                                                            />
                                                         )}
                                                     />
-                                                    <InfoDiv infoValue="77%" />
+                                                    <InfoDiv infoValue="77%"/>
                                                     <IndicatorCard
-                                                      cardID="top-country-indicator-card"
-                                                      indicatorTitle="Engagement by Location"
-                                                      showArrow={false}
-                                                      graphComponent={(
-                                                          <BarGraph
-                                                            graphData={BarGraphMockData}
-                                                          />
+                                                        cardID="top-country-indicator-card"
+                                                        indicatorTitle="Engagement by Location"
+                                                        showArrow={false}
+                                                        graphComponent={(
+                                                            <BarGraph
+                                                                graphData={BarGraphMockData}
+                                                            />
                                                         )}
                                                     />
                                                 </div>
@@ -190,35 +190,35 @@ class ChartPage extends Component {
                                         </Layout>
 
                                         <Layout
-                                          id="word-cloud-layout"
+                                            id="word-cloud-layout"
                                         >
 
                                             <div
-                                              id="word-cloud-grid-container"
+                                                id="word-cloud-grid-container"
                                             >
 
                                                 <div
-                                                  id="word-cloud-item"
-                                                  className="word-cloud-grid-item"
+                                                    id="word-cloud-item"
+                                                    className="word-cloud-grid-item"
                                                 >
-                                                    <WordCloud />
+                                                    <WordCloud/>
                                                 </div>
 
                                                 <div
-                                                  id="stat1-item"
-                                                  className="word-cloud-grid-item"
+                                                    id="stat1-item"
+                                                    className="word-cloud-grid-item"
                                                 >
                                                     <VisxBarGroupGraph/>
                                                 </div>
                                                 <div
-                                                  id="stat2-item"
-                                                  className="word-cloud-grid-item"
+                                                    id="stat2-item"
+                                                    className="word-cloud-grid-item"
                                                 >
                                                     locations where its been mentioned
                                                 </div>
                                                 <div
-                                                  id="stat3-item"
-                                                  className="word-cloud-grid-item"
+                                                    id="stat3-item"
+                                                    className="word-cloud-grid-item"
                                                 >
                                                     timeline thingy
                                                 </div>
@@ -227,32 +227,32 @@ class ChartPage extends Component {
 
                                         {/* The Network Graph */}
                                         <Layout
-                                          id="network_graph_layout_div"
-                                          className="network_card"
+                                            id="network_graph_layout_div"
+                                            className="network_card"
                                         >
                                             <Content
-                                              id="network_graph_card_content"
-                                              className="network_card"
+                                                id="network_graph_card_content"
+                                                className="network_card"
                                             >
                                                 <NetworkGraphCard
-                                                  text={this.state.text}
-                                                  key={this.state.text}
+                                                    text={this.state.text}
+                                                    key={this.state.text}
                                                 />
                                             </Content>
                                         </Layout>
 
                                         {/* The timeline Graph */}
                                         <Layout
-                                          id="timeline_graph_div"
-                                          className="timeline_card"
+                                            id="timeline_graph_div"
+                                            className="timeline_card"
                                         >
                                             <Content
-                                              id="timeline_graph_card_content"
-                                              className="timeline_card"
+                                                id="timeline_graph_card_content"
+                                                className="timeline_card"
                                             >
                                                 <TimelineGraph
-                                                  text={this.state.text}
-                                                  key={this.state.text}
+                                                    text={this.state.text}
+                                                    key={this.state.text}
                                                 />
                                             </Content>
                                         </Layout>
@@ -264,7 +264,7 @@ class ChartPage extends Component {
                 </>
             );
         }
-        return <Redirect to="/login" />;
+        return <Redirect to="/login"/>;
     }
 }
 
