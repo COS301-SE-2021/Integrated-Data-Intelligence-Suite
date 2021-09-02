b9fa2564e2501ca301d75f31852d2882802fd830
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-02 23:11:26 +0200

    edited word cloud request object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateWordCloudGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateWordCloudGraphRequest.java
@@ -3,13 +3,13 @@ package com.Visualize_Service.Visualize_Service.request;
 import java.util.ArrayList;
 
 public class CreateWordCloudGraphRequest {
-    public ArrayList<String> wordBank;
+    public ArrayList<ArrayList> dataList;
 
-    public CreateWordCloudGraphRequest(ArrayList<String> wordBank){
-        this.wordBank = wordBank;
+    public CreateWordCloudGraphRequest(ArrayList<ArrayList> wordBank){
+        this.dataList = wordBank;
     }
 
-    public ArrayList<String> getWordBank(){
-        return wordBank;
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
     }
 }
