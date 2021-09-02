1151b5d76ce4f81f9004ed14c66396e87f107777
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-02 13:32:40 +0200

    edited nlpProperties response object

--- Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindNlpPropertiesResponse.java
+++ Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/FindNlpPropertiesResponse.java
@@ -1,29 +1,19 @@
 package com.Analyse_Service.Analyse_Service.response;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.CoreSentence;
-
 import java.util.ArrayList;
 
 public class FindNlpPropertiesResponse {
     String sentiment ;
-    ArrayList<ArrayList> partsOfSpeech;
     ArrayList<ArrayList> namedEntities;
-    //ArrayList<ArrayList> lemmanation;
 
-    public FindNlpPropertiesResponse(String sentiment, ArrayList<ArrayList> partsOfSpeech, ArrayList<ArrayList> namedEntities){
+    public FindNlpPropertiesResponse(String sentiment,  ArrayList<ArrayList> namedEntities){
         this.sentiment = sentiment;
-        this.partsOfSpeech = partsOfSpeech;
+
         this.namedEntities = namedEntities;
     }
 
     public void setSentiment(String sentiment){
         this.sentiment = sentiment;
-    }
-
-    public void setPartsOfSpeech(ArrayList<ArrayList> partsOfSpeech){
-        this.partsOfSpeech = partsOfSpeech;
     }
 
     public void setNamedEntities(ArrayList<ArrayList> namedEntities){
@@ -32,10 +22,6 @@ public class FindNlpPropertiesResponse {
 
     public String getSentiment(){
         return sentiment;
-    }
-
-    public ArrayList<ArrayList>getPartsOfSpeech(){
-        return partsOfSpeech;
     }
 
     public ArrayList<ArrayList> getNamedEntities(){
