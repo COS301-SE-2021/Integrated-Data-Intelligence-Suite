599c449d1f6af679022d91b4eec27f0745656148
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-02 10:05:19 +0200

    implemented && completed word cloud request object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateWordCloudGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreateWordCloudGraphRequest.java
@@ -1,4 +1,15 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class CreateWordCloudGraphRequest {
+    public ArrayList<String> wordBank;
+
+    public CreateWordCloudGraphRequest(ArrayList<String> wordBank){
+        this.wordBank = wordBank;
+    }
+
+    public ArrayList<String> getWordBank(){
+        return wordBank;
+    }
 }
