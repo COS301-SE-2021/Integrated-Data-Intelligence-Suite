18ad20ad3f6ae9723d611fcf8feb9cf294236213
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-02 10:01:13 +0200

    implemented && completed word cloud response object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/CreateWordCloudGraphResponse.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/response/CreateWordCloudGraphResponse.java
@@ -1,4 +1,17 @@
 package com.Visualize_Service.Visualize_Service.response;
 
+import com.Visualize_Service.Visualize_Service.dataclass.Graph;
+
+import java.util.ArrayList;
+
 public class CreateWordCloudGraphResponse {
+    public String words;
+
+    public CreateWordCloudGraphResponse(String words){
+        this.words = words;
+    }
+
+    public String getLineGraphArray(){
+        return words;
+    }
 }
