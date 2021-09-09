889e9dcc5cd7c0a1bb5cb87ea5be5a721e0dd881
shrey-99 <shreymandalia@gmail.com>
2021-09-09 23:40:25 +0200

    created response and request for send notification

---
+++ User_Service/src/main/java/com/User_Service/User_Service/request/SendNotificationRequest.java
@@ -0,0 +1,8 @@
+package com.User_Service.User_Service.request;
+
+public class SendNotificationRequest {
+    private String content;
+    private String to;
+    private String from;
+    private String subject;
+}
