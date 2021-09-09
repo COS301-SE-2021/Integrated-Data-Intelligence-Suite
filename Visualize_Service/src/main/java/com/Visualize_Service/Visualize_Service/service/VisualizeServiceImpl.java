ec394447dca43a29775d394be84eff83b2070209
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-09 22:36:01 +0200

    created and implemented GetTotalAnomalies function

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
@@ -188,6 +188,24 @@ public class VisualizeServiceImpl {
         return new GetTotalTrendsResponse(output);
     }
 
+    public GetTotalAnomaliesResponse getTotalAnomalies(GetTotalAnomaliesRequest request) throws InvalidRequestException {
+        if (request == null) {
+            throw new InvalidRequestException("CreateTimelineGraphRequest Object is null");
+        }
+        if (request.getDataList() == null) {
+            throw new InvalidRequestException("Arraylist is null");
+        }
+        ArrayList<String> reqData = request.getDataList();
+        ArrayList<Graph> output = new ArrayList<>();
+
+        WordCloudGraph out = new WordCloudGraph();
+        out.words = String.valueOf(reqData.size());
+        output.add(out);
+        System.out.println(out.words);
+        return new GetTotalAnomaliesResponse(output);
+    }
+
+
 
 
     public CreateTimelineGraphResponse createTimelineGraph(CreateTimelineGraphRequest request) throws InvalidRequestException {
