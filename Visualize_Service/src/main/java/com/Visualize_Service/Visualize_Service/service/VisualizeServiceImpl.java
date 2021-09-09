fd903064ef872c71521d4f86fbdb2662cf5e9ecb
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-09 21:58:33 +0200

    created and implemented GetMostProminentSentiment function

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
@@ -110,6 +110,67 @@ public class VisualizeServiceImpl {
         return new GetTotalInteractionResponse(null);
     }
 
+    public GetMostProminentSentimentResponse getMostProminentSentiment(GetMostProminentSentimentRequest request) throws InvalidRequestException {
+        if (request == null) {
+            throw new InvalidRequestException("CreateTimelineGraphRequest Object is null");
+        }
+        if (request.getDataList() == null) {
+            throw new InvalidRequestException("Arraylist is null");
+        }
+        String outputs = "";
+        ArrayList<ArrayList> reqData = request.getDataList();
+        ArrayList<Graph> output = new ArrayList<>();
+
+        int k = 0;
+        ArrayList<String> listSent = new ArrayList<>();
+        ArrayList<ArrayList> out = new ArrayList<>();
+        for (int i = 0; i < reqData.size(); i++) {
+            ArrayList<String> sents = (ArrayList<String>) reqData.get(i).get(4);
+            //System.out.println(locs.toString());
+
+            for (int j = 0; j < sents.size(); j++) {
+                if (listSent.isEmpty()){
+                    listSent.add(sents.get(j));
+                    ArrayList<Object> r = new ArrayList<>();
+                    r.add(sents.get(j));
+                    r.add(1);
+                    out.add(r);
+                }else {
+                    if (listSent.contains(sents.get(j))){
+                        ArrayList<Object>r =  out.get(listSent.indexOf(sents.get(j)));
+                        int val=Integer.parseInt(r.get(1).toString());
+                        val++;
+                        r.set(1,val);
+                        out.set(listSent.indexOf(sents.get(j)),r);
+                    }else {
+                        listSent.add(sents.get(j));
+                        ArrayList<Object> r = new ArrayList<>();
+                        r.add(sents.get(j));
+                        r.add(1);
+                        out.add(r);
+                    }
+                }
+            }
+        }
+
+
+        outputs = out.get(0).get(0).toString();
+        int temp = Integer.parseInt(out.get(0).get(1).toString());
+        for (ArrayList o : out) {
+            //System.out.println(o);
+            if (Integer.parseInt(o.get(1).toString()) > temp){
+                outputs = o.get(0).toString();
+                temp = Integer.parseInt(o.get(1).toString());
+            }
+        }
+
+        WordCloudGraph outW = new WordCloudGraph();
+        outW.words=outputs;
+        output.add(outW);
+        System.out.println(outputs);
+        return new GetMostProminentSentimentResponse(output);
+    }
+
 
 
     public CreateTimelineGraphResponse createTimelineGraph(CreateTimelineGraphRequest request) throws InvalidRequestException {
