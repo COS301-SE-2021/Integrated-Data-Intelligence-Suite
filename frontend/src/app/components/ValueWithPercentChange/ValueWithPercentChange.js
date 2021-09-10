5637e27eeb2b5b5cb2bc75307a5aec0edab8f633
Myron Lopes <u18246932@tuks.co.za>
2021-09-10 06:15:51 +0200

    added randomised value function for percent change

--- frontend/src/app/components/ValueWithPercentChange/ValueWithPercentChange.js
+++ frontend/src/app/components/ValueWithPercentChange/ValueWithPercentChange.js
@@ -7,20 +7,25 @@ import {
     FiArrowUpRight
 } from 'react-icons/all';
 
+const _getRandomPercentage = () => {
+    let min = 1;
+    let max = 90;
+    return Math.floor(Math.random() * (max - min) + min) + ' %';
+};
+
 export default function ValueWithPercentChange(props) {
     return (
         <>
             <div className={'value-with-percent-container'}>
                 <div className={'raw-value'}>{props.rawValue}</div>
                 <div className={'icon-with-percent-changed-by'}>
-
                     {props.isIncreasing
                         ? <FiArrowUpRight className={'icon-of-percent-increasing'}/>
                         : <FiArrowDownRight className={'icon-of-percent-decreasing'}/>
                     }
                     {props.isIncreasing
-                        ? <div className={'amount-of-percent-increasing'}>percentChangedBy</div>
-                        : <div className={'amount-of-percent-decreasing'}>percentChangedBy</div>
+                        ? <div className={'amount-of-percent-increasing'}>{_getRandomPercentage()}</div>
+                        : <div className={'amount-of-percent-decreasing'}>{_getRandomPercentage()}</div>
                     }
                 </div>
             </div>
