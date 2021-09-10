b7a51c3810a6add1d6942ecee8b29fbd60fce7cf
Myron Lopes <u18246932@tuks.co.za>
2021-09-10 11:02:34 +0200

    fixed compile bug

--- frontend/src/app/components/PieWithCustomLabels/PieWithCustomLabels.js
+++ frontend/src/app/components/PieWithCustomLabels/PieWithCustomLabels.js
@@ -2,29 +2,6 @@ import React from 'react';
 import { VictoryLabel, VictoryPie, VictoryTooltip } from 'victory';
 import PieCustomLabel from '../PieCustomLabel/PieCustomLabel';
 
-// data={[
-//     {
-//         x: 1,
-//         y: 5
-{/*    },*/
-}
-//     {
-//         x: 2,
-//         y: 4
-//     },
-//     {
-//         x: 3,
-//         y: 2
-//     },
-//     {
-//         x: 4,
-//         y: 3
-//     },
-//     {
-//         x: 5,
-//         y: 1
-//     }
-// ]}
 export default class PieWithCustomLabels extends React.Component {
     constructor(props) {
         super(props);
@@ -39,7 +16,6 @@ export default class PieWithCustomLabels extends React.Component {
                     labelRadius={120}
                     labels={({ datum }) => `# ${datum.y}`}
                     labelComponent={<PieCustomLabel/>}
-
                     data={this.props.text}
                 />
 
