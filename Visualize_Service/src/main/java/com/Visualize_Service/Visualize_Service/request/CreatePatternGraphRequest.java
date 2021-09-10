ef4fc59eabad6b6f7529bb3474ae6114004cb42c
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-10 07:14:35 +0200

    implemeted req and res objects for pattern graph

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreatePatternGraphRequest.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/request/CreatePatternGraphRequest.java
@@ -1,4 +1,15 @@
 package com.Visualize_Service.Visualize_Service.request;
 
+import java.util.ArrayList;
+
 public class CreatePatternGraphRequest {
+    public ArrayList<ArrayList> dataList;
+
+    public CreatePatternGraphRequest(ArrayList<ArrayList> dataList){
+        this.dataList = dataList;
+    }
+
+    public ArrayList<ArrayList> getDataList(){
+        return dataList;
+    }
 }
