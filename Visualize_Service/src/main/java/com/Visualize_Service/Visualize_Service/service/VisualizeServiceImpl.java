f3bc10519beeb862ed50288b8d2ddcc2dc7adb51
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-10 04:26:48 +0200

    created wordGraphPieChart function with its response and request object

--- Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
+++ Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/service/VisualizeServiceImpl.java
@@ -40,6 +40,7 @@ public class VisualizeServiceImpl {
         }
 
         ArrayList<ArrayList> outputData = new ArrayList<>();
+        setCloud();
 
 
         //***********************Overview Section*************************//
@@ -51,12 +52,12 @@ public class VisualizeServiceImpl {
 
         //Bar Graph(Average Interaction)                  <- D
         //PieChart(Overall section)                       <- D
-        // Bar Graph(total Engagement in each location)   <-
+        // Bar Graph(total Engagement in each location)   <- *
 
         //map                                             <- D
         //frequecy of tweets in trend                     <- D
 
-        //word cloud                                      <- D*
+        //word cloud                                      <- D
         //word cloud pie chart                            <-
         //word cloud sunburst                             <-
 
@@ -121,6 +122,7 @@ public class VisualizeServiceImpl {
 
         return new VisualizeDataResponse( outputData );
     }
+
 
     public GetTotalInteractionResponse getTotalInteraction(GetTotalInteractionRequest request) throws InvalidRequestException {
         if (request == null) {
@@ -380,6 +382,8 @@ public class VisualizeServiceImpl {
         return new CreateLineGraphInteractionsResponse(output);
     }
 
+
+
     //frequecy of tweets in trend
     public CreateBarGraphResponse createBarGraph(CreateBarGraphRequest request) throws InvalidRequestException{
         if (request == null) {
@@ -491,6 +495,8 @@ public class VisualizeServiceImpl {
         }
         return new CreateBarGraphExtraTwoResponse(output);
     }
+
+
 
 
     public CreatePieChartGraphResponse createPieChartGraph(CreatePieChartGraphRequest request) throws InvalidRequestException{
@@ -752,6 +758,9 @@ public class VisualizeServiceImpl {
         return new CreateNetworkGraphResponse(output);
     }
 
+
+
+
     public CreateWordCloudGraphResponse createWordCloudGraph(CreateWordCloudGraphRequest request) throws InvalidRequestException{
         if (request == null) {
             throw new InvalidRequestException("Request Object is null");
@@ -761,10 +770,6 @@ public class VisualizeServiceImpl {
         }
         ArrayList<ArrayList> reqData = request.getDataList();
         ArrayList<Graph> output = new ArrayList<>();
-
-        if(filterdCloud.isEmpty() == true) {
-            setCloud();
-        }
 
 
         ArrayList<String> wordList = new ArrayList<>();
@@ -797,7 +802,46 @@ public class VisualizeServiceImpl {
         return new CreateWordCloudGraphResponse(output,wordList);
     }
 
+    public CreateWordCloudPieChartGraphResponse createWordCloudPieChartGraph(CreateWordCloudPieChartGraphRequest request) throws InvalidRequestException{
+        if (request == null) {
+            throw new InvalidRequestException("Request Object is null");
+        }
+        /*if (request.dataList == null){
+            throw new InvalidRequestException("Arraylist object is null");
+        }
+        ArrayList<ArrayList> reqData = request.getDataList();
+        ArrayList<Graph> output = new ArrayList<>();
 
+
+        ArrayList<String> wordList = new ArrayList<>();
+        for (int i = 0; i < reqData.size(); i++) {
+            ArrayList<String> texts = (ArrayList<String>) reqData.get(i).get(5);
+            //System.out.println(locs.toString());
+
+            for (int j = 0; j < texts.size(); j++) {
+                String[] words = texts.get(j).toString().split(" ");
+                for(int k =0; k < words.length ; k++){
+                    if (filterdCloud.contains(words[k]) == false){
+                        wordList.add(words[k]);
+                    }
+                }
+            }
+        }
+
+        String text =wordList.get(0);
+        for(int i =1; i < wordList.size(); i++){
+            text = text + " " + wordList.get(i);
+        }
+
+        WordCloudGraph out = new WordCloudGraph();
+        out.words = text;
+
+        System.out.println(out.words);
+        output.add(out);*/
+
+
+        return new CreateWordCloudPieChartGraphResponse();
+    }
 
     /*************************************************HELPER***********************************************************/
 
@@ -1017,7 +1061,75 @@ public class VisualizeServiceImpl {
     }
 
     private void setCloud() {
-        filterdCloud.add("");
+        filterdCloud.add("is");	filterdCloud.add("was");	filterdCloud.add("are");	filterdCloud.add("be");	filterdCloud.add("have");
+        filterdCloud.add("had");	filterdCloud.add("were");	filterdCloud.add("can");	filterdCloud.add("said");	filterdCloud.add("use");
+        filterdCloud.add("do");	filterdCloud.add("will");	filterdCloud.add("would");	filterdCloud.add("make");	filterdCloud.add("like");
+        filterdCloud.add("has");	filterdCloud.add("look");	filterdCloud.add("write");	filterdCloud.add("go");	filterdCloud.add("see");
+        filterdCloud.add("could");	filterdCloud.add("been");	filterdCloud.add("call");	filterdCloud.add("am");	filterdCloud.add("find");
+        filterdCloud.add("did");	filterdCloud.add("get");	filterdCloud.add("come");	filterdCloud.add("made");	filterdCloud.add("may");
+        filterdCloud.add("take");	filterdCloud.add("know");	filterdCloud.add("live");	filterdCloud.add("give");	filterdCloud.add("think");
+        filterdCloud.add("say");	filterdCloud.add("help");	filterdCloud.add("tell");	filterdCloud.add("follow");	filterdCloud.add("came");
+        filterdCloud.add("want");	filterdCloud.add("show");	filterdCloud.add("set");	filterdCloud.add("put	does");
+        filterdCloud.add("must");	filterdCloud.add("ask");	filterdCloud.add("went");	filterdCloud.add("read");	filterdCloud.add("need");
+        filterdCloud.add("move");	filterdCloud.add("try");	filterdCloud.add("change");	filterdCloud.add("play");	filterdCloud.add("spell");
+        filterdCloud.add("found");	filterdCloud.add("study");	filterdCloud.add("learn");	filterdCloud.add("should");	filterdCloud.add("add");
+        filterdCloud.add("keep");	filterdCloud.add("start");	filterdCloud.add("thought");	filterdCloud.add("saw");	filterdCloud.add("turn");
+        filterdCloud.add("might");	filterdCloud.add("close");	filterdCloud.add("seem");	filterdCloud.add("open");	filterdCloud.add("begin");
+        filterdCloud.add("got");	filterdCloud.add("run");	filterdCloud.add("walk");	filterdCloud.add("began");	filterdCloud.add("grow");
+        filterdCloud.add("took");	filterdCloud.add("carry");	filterdCloud.add("hear");	filterdCloud.add("stop");	filterdCloud.add("miss");
+        filterdCloud.add("eat");	filterdCloud.add("watch");	filterdCloud.add("let");	filterdCloud.add("cut");	filterdCloud.add("talk");
+        filterdCloud.add("being");	filterdCloud.add("leave");
+
+        filterdCloud.add("word");	filterdCloud.add("time");	filterdCloud.add("number");	filterdCloud.add("way");	filterdCloud.add("people");
+        filterdCloud.add("water");	filterdCloud.add("day");	filterdCloud.add("part");	filterdCloud.add("sound");	filterdCloud.add("work");
+        filterdCloud.add("place");	filterdCloud.add("year");	filterdCloud.add("back");	filterdCloud.add("thing");	filterdCloud.add("name");
+        filterdCloud.add("sentence");	filterdCloud.add("man");	filterdCloud.add("line");	filterdCloud.add("boy");	filterdCloud.add("farm");
+        filterdCloud.add("end");	filterdCloud.add("men");	filterdCloud.add("land");	filterdCloud.add("home");	filterdCloud.add("hand");
+        filterdCloud.add("picture");	filterdCloud.add("air");	filterdCloud.add("animal");	filterdCloud.add("house");	filterdCloud.add("page");
+        filterdCloud.add("letter");	filterdCloud.add("point");	filterdCloud.add("mother");	filterdCloud.add("answer	America");
+        filterdCloud.add("world");	filterdCloud.add("food");	filterdCloud.add("country	plant	school");
+        filterdCloud.add("father");	filterdCloud.add("tree");	filterdCloud.add("city");	filterdCloud.add("earth	eye");
+        filterdCloud.add("head");	filterdCloud.add("story");	filterdCloud.add("example");	filterdCloud.add("life");	filterdCloud.add("paper");
+        filterdCloud.add("group");	filterdCloud.add("children");	filterdCloud.add("side");	filterdCloud.add("feet");	filterdCloud.add("car");
+        filterdCloud.add("mile");	filterdCloud.add("night");	filterdCloud.add("sea");	filterdCloud.add("river");	filterdCloud.add("state");
+        filterdCloud.add("book");	filterdCloud.add("idea");	filterdCloud.add("face");	filterdCloud.add("girl");
+        filterdCloud.add("mountain");	filterdCloud.add("list");	filterdCloud.add("song");	filterdCloud.add("family");
+
+        filterdCloud.add("he");	filterdCloud.add("a");	filterdCloud.add("one");	filterdCloud.add("all");	filterdCloud.add("an");
+        filterdCloud.add("each");	filterdCloud.add("other");	filterdCloud.add("many");	filterdCloud.add("some");	filterdCloud.add("two");
+        filterdCloud.add("more");	filterdCloud.add("long");	filterdCloud.add("new");	filterdCloud.add("little");	filterdCloud.add("most");
+        filterdCloud.add("good");	filterdCloud.add("great");	filterdCloud.add("right");	filterdCloud.add("mean");	filterdCloud.add("old");
+        filterdCloud.add("any");	filterdCloud.add("same");	filterdCloud.add("three");	filterdCloud.add("small");	filterdCloud.add("another");
+        filterdCloud.add("large");	filterdCloud.add("big");	filterdCloud.add("even");	filterdCloud.add("such");	filterdCloud.add("different");
+        filterdCloud.add("kind");	filterdCloud.add("still");	filterdCloud.add("high");	filterdCloud.add("every");	filterdCloud.add("own");
+        filterdCloud.add("light");	filterdCloud.add("left");	filterdCloud.add("few");	filterdCloud.add("next");	filterdCloud.add("hard");
+        filterdCloud.add("both");	filterdCloud.add("important");	filterdCloud.add("white");	filterdCloud.add("four");	filterdCloud.add("second");
+        filterdCloud.add("enough");	filterdCloud.add("above");	filterdCloud.add("young");
+
+        filterdCloud.add("not");	filterdCloud.add("when");	filterdCloud.add("there");	filterdCloud.add("how");	filterdCloud.add("up");
+        filterdCloud.add("out");	filterdCloud.add("then");	filterdCloud.add("so");	filterdCloud.add("no");	filterdCloud.add("first");
+        filterdCloud.add("now");	filterdCloud.add("only");	filterdCloud.add("very");	filterdCloud.add("just");	filterdCloud.add("where");
+        filterdCloud.add("much");	filterdCloud.add("before");	filterdCloud.add("too");	filterdCloud.add("also");	filterdCloud.add("around");
+        filterdCloud.add("well");	filterdCloud.add("here");	filterdCloud.add("why");	filterdCloud.add("again");	filterdCloud.add("off");
+        filterdCloud.add("away");	filterdCloud.add("near");	filterdCloud.add("below");	filterdCloud.add("last");	filterdCloud.add("never");
+        filterdCloud.add("always");	filterdCloud.add("together");	filterdCloud.add("often");	filterdCloud.add("once");	filterdCloud.add("later");
+        filterdCloud.add("far");	filterdCloud.add("really");	filterdCloud.add("almost");	filterdCloud.add("sometimes");	filterdCloud.add("soon");
+
+        filterdCloud.add("of");	filterdCloud.add("to");	filterdCloud.add("in");	filterdCloud.add("for");	filterdCloud.add("on");
+        filterdCloud.add("with");	filterdCloud.add("at");	filterdCloud.add("from");	filterdCloud.add("by");	filterdCloud.add("about");
+        filterdCloud.add("into");	filterdCloud.add("down");	filterdCloud.add("over");	filterdCloud.add("after");	filterdCloud.add("through");
+        filterdCloud.add("between	under");	filterdCloud.add("along");	filterdCloud.add("until");	filterdCloud.add("without");
+
+        filterdCloud.add("you");	filterdCloud.add("that");	filterdCloud.add("it");	filterdCloud.add("he");	filterdCloud.add("his");
+        filterdCloud.add("they");	filterdCloud.add("I");	filterdCloud.add("this");	filterdCloud.add("what");	filterdCloud.add("we");
+        filterdCloud.add("your");	filterdCloud.add("which");	filterdCloud.add("she");	filterdCloud.add("their");	filterdCloud.add("them");
+        filterdCloud.add("these");	filterdCloud.add("her");	filterdCloud.add("him");	filterdCloud.add("my");	filterdCloud.add("who");
+        filterdCloud.add("its");	filterdCloud.add("me");	filterdCloud.add("our");	filterdCloud.add("us");	filterdCloud.add("something");
+        filterdCloud.add("those");
+
+        filterdCloud.add("and");	filterdCloud.add("as");	filterdCloud.add("or");	filterdCloud.add("but");
+        filterdCloud.add("if");	filterdCloud.add("than");	filterdCloud.add("because");	filterdCloud.add("while");
+        filterdCloud.add("it’s");	filterdCloud.add("don’t");
     }
 
 
