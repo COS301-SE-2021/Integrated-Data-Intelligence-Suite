8e5ab20069a09bdcdcf5c43448af0f97a0b5587a
shrey-99 <shreymandalia@gmail.com>
2021-09-12 22:11:43 +0200

    added enable configurations annotation

--- User_Service/src/main/java/com/User_Service/User_Service/UserServiceApplication.java
+++ User_Service/src/main/java/com/User_Service/User_Service/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.User_Service.User_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ConfigurationPropertiesScan("com.User_Service.User_Service.config")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
