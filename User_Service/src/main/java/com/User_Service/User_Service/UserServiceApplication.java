55d8cc2be30e1a3dbbf1bf778c33ae7735ddbb30
shrey-99 <shreymandalia@gmail.com>
2021-09-13 15:21:31 +0200

    added enableScheduling annotation

--- User_Service/src/main/java/com/User_Service/User_Service/UserServiceApplication.java
+++ User_Service/src/main/java/com/User_Service/User_Service/UserServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableScheduling
 @ConfigurationPropertiesScan("com.User_Service.User_Service.config")
 public class UserServiceApplication {
 
