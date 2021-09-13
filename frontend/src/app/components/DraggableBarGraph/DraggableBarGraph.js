e66571c3b65e7b3eec8b51fab2a888b3eccdc862
abDivergent <u15219900@tuks.co.za>
2021-09-13 14:32:19 +0200

    updated how bar graph receives and updates data points

--- frontend/src/app/components/DraggableBarGraph/DraggableBarGraph.js
+++ frontend/src/app/components/DraggableBarGraph/DraggableBarGraph.js
@@ -5,6 +5,12 @@ import {
 import { data } from 'browserslist';
 import { OLD_DATA, DATA } from '../../Mocks/BarGraphMock';
 
+const index = 8;
+const colors = {
+    primary: '#5873f9',
+    secondary: '#12939A',
+}
+
 function highlightMapPoints(highlighted_points) {
     const old_highlighted_points = document.getElementsByClassName('chosen_circle');
     for (let i = 0; i < old_highlighted_points.length; i++) {
@@ -14,12 +20,11 @@ function highlightMapPoints(highlighted_points) {
     for (let i = 0; i < highlighted_points.length; i++) {
         const points_to_change = document.getElementsByClassName(highlighted_points[i].classname);
         for (let j = 0; j < points_to_change.length; j++) {
-            console.log(points_to_change[j]);
             points_to_change[j].classList.add('chosen_circle');
         }
     }
 }
-const index = 8;
+
 
 export default class DraggableBarGraph extends React.Component {
     constructor(props) {
@@ -27,62 +32,30 @@ export default class DraggableBarGraph extends React.Component {
         this.state = {
             selectionStart: null,
             selectionEnd: null,
-            average: '',
-            dataToBeDisplayed: [],
+            dataToBeDisplayed: null,
         };
     }
-    componentDidMount() {
-        if (this.props.text === '') {
-            this.setState({ dataToBeDisplayed: OLD_DATA });
-        } else if (this.props.text[index] && this.props.text[index] > 0) {
-            this.setState({ dataToBeDisplayed: DATA });
-        } else {
-            this.setState({ dataToBeDisplayed: OLD_DATA });
+    static getDerivedStateFromProps(props, state) {
+        if (props.text[index] && props.text[index].length > 0) {
+            const dataPoints = Object.values(props.text[index]).map((item, i) => ({
+                x0: i, x: i + 1, y: item.y, classname: item.x,
+            }));
+            return { dataToBeDisplayed: dataPoints };
         }
+        return { dataToBeDisplayed: DATA };
     }
+
     getAverageY() {
         if (this.state.selectionStart && this.state.selectionEnd) {
             const lowerBound = Math.floor(this.state.selectionStart);
             const upperBound = Math.ceil(this.state.selectionEnd) + 1;
 
-            let avg = 0;
+            const avg = 0;
 
-            // const highlighted_classes = []
             const lst = Object.values(this.state.dataToBeDisplayed)
-                .filter((item) => {
-                    if (item.x > lowerBound && item.x < upperBound) {
-                        avg += item.y;
-                        // highlighted_classes.push(item.classname);
-                        return true;
-                    }
-                    return false;
-                });
+                .filter((item) => item.x > lowerBound && item.x < upperBound);
 
-            avg /= lst.length;
-            this.setState({ average: avg });
-            console.log(lst);
-            console.log(avg);
             highlightMapPoints(lst);
-        }
-    }
-
-    updateGraph() {
-        if (this.props.text[index] && this.props.text[index].length > 0) {
-            console.log(this.props.text[7]);
-            console.log(this.props.text[8]);
-
-            const lst = Object.values(this.props.text[index]).map((item, i) => {
-                return {
-                    x0: i,
-                    x: i + 1,
-                    y: item.y,
-                    classname: `classname${i}`,
-                    tweet: item.x,
-                };
-            });
-            console.log(lst);
-        } else {
-            console.log('not a good check');
         }
     }
 
@@ -102,26 +75,26 @@ export default class DraggableBarGraph extends React.Component {
         return (
             <div className="uber-bar-graph-container">
                 {/* {selectionStart && <p className="bar-graph-statistic">{this.state.average}</p>} */}
-                {/* { this.props.text !== '' && this.updateGraph() } */}
+                {}
                 <div className="bar-graph-plot">
-                    { this.props.text !== '' && (
+                    { this.state.dataToBeDisplayed && (
                         <XYPlot width={300} height={200}>
                             <XAxis />
                             <YAxis />
                             <VerticalRectSeries
-                              data={Object.values(this.props.text[index]).map((item, i) => ({ x0: i, x: i + 1, y: item.y, tweet: item.x }))}
+                              data={this.state.dataToBeDisplayed}
                               stroke="white"
                               colorType="literal"
                               getColor={(d) => {
                                     if (selectionStart === null || selectionEnd === null) {
-                                        return '#1E96BE';
+                                        return colors.primary;
                                     }
                                     const inX = d.x >= selectionStart && d.x <= selectionEnd;
                                     const inX0 = d.x - 1 >= selectionStart && d.x - 1 <= selectionEnd;
                                     const inStart = selectionStart >= d.x - 1 && selectionStart <= d.x;
                                     const inEnd = selectionEnd >= d.x - 1 && selectionEnd <= d.x;
 
-                                    return inStart || inEnd || inX || inX0 ? '#12939A' : '#1E96BE';
+                                    return inStart || inEnd || inX || inX0 ? colors.secondary : colors.primary;
                                 }}
                             />
 
@@ -131,49 +104,9 @@ export default class DraggableBarGraph extends React.Component {
                               enableY={false}
                               onDrag={updateDragState}
                               onDragEnd={updateDragState}
-
                             />
                         </XYPlot>
                     )}
-                    { this.props.text === '' && (
-                        <XYPlot width={300} height={200}>
-                            <XAxis />
-                            <YAxis />
-                            <VerticalRectSeries
-                                data={DATA}
-                                stroke="white"
-                                colorType="literal"
-                                getColor={(d) => {
-                                    if (selectionStart === null || selectionEnd === null) {
-                                        return '#1E96BE';
-                                    }
-                                    const inX = d.x >= selectionStart && d.x <= selectionEnd;
-                                    const inX0 = d.x - 1 >= selectionStart && d.x - 1 <= selectionEnd;
-                                    const inStart = selectionStart >= d.x - 1 && selectionStart <= d.x;
-                                    const inEnd = selectionEnd >= d.x - 1 && selectionEnd <= d.x;
-
-                                    return inStart || inEnd || inX || inX0 ? '#12939A' : '#1E96BE';
-                                }}
-                            />
-
-                            <Highlight
-                                color="#829AE3"
-                                drag
-                                enableY={false}
-                                // onDrag={updateDragState}
-                                // onDragEnd={updateDragState}
-
-                            />
-                        </XYPlot>
-                    )}
-
-                    {/* <div> */}
-                    {/*    <b>selectionStart:</b> */}
-                    {/*    {`${Math.floor(selectionStart * 100) / 100},`} */}
-                    {/*    <b>selectionEnd:</b> */}
-                    {/*    {`${Math.floor(selectionEnd * 100) / 100},`} */}
-                    {/*    <b>Average of Y Values of Highlighted Bar Graphs</b> */}
-                    {/* </div> */}
                 </div>
             </div>
         );
