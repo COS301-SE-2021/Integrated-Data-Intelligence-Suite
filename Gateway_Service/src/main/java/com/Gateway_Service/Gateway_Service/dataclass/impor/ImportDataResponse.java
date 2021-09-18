2902fdbaad3cbb68370992fb4708f4a220e4b48f
u18003517-Rhuli <u18003517@tuks.co.za>
2021-09-18 03:35:54 +0200

    created & updated gateway dataclass object (import)

--- Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/impor/ImportDataResponse.java
+++ Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/impor/ImportDataResponse.java
@@ -1,7 +1,5 @@
 package com.Gateway_Service.Gateway_Service.dataclass.impor;
 
-import com.Import_Service.Import_Service.dataclass.ImportedData;
-
 import java.util.ArrayList;
 
 public class ImportDataResponse {
