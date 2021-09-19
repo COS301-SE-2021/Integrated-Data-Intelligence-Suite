80ddccbc50622561f3b779bb0b11353b479ed539
shrey-99 <shreymandalia@gmail.com>
2021-09-19 17:35:18 +0200

    created request and response for updating profile

---
+++ User_Service/src/main/java/com/User_Service/User_Service/request/UpdateProfileRequest.java
@@ -0,0 +1,52 @@
+package com.User_Service.User_Service.request;
+
+public class UpdateProfileRequest {
+    String username;
+    String firstName;
+    String lastName;
+    String email;
+
+    public UpdateProfileRequest() {
+
+    }
+
+    public UpdateProfileRequest(String username, String firstName, String lastName, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastname(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
