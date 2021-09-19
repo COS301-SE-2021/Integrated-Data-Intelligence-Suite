6353f9da76d7daf7a0dc010c76fed78980e1f1cb
Myron Lopes <u18246932@tuks.co.za>
2021-09-19 17:25:25 +0200

    added structure for footer

---
+++ frontend/src/app/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+
+    return (
+        <>
+            <div
+                id={'footer'}
+            >
+                Footer Content
+            </div>
+        </>
+    );
+
+}
