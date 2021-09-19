73c9d853173e60e9596ba34f205ecb4564b535aa
Myron Lopes <u18246932@tuks.co.za>
2021-09-19 17:26:48 +0200

    updated simple card to have both title on top and at the bottom

--- frontend/src/app/components/SimpleCard/SimpleCard.js
+++ frontend/src/app/components/SimpleCard/SimpleCard.js
@@ -4,12 +4,41 @@ import './SimpleCard.css';
 export default function SimpleCard(props) {
     return (
         <>
-            <div className="simple-card-container" id={props.cardID}>
-                <div className="simple-card-title">{props.cardTitle}</div>
-                <div className="simple-card-body">
-                    {props.children}
-                </div>
-            </div>
+            {
+                props.titleOnTop
+                    ? (
+                        <div
+                            className={`simple-card-container ${props.extraClassName}`}
+                            id={props.cardID}
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: '100%',
+                                gridTemplateRows: '3em auto',
+                                height: '100%',
+                                marginBottom: '3%'
+                            }}
+                        >
+                            <div className="simple-card-title">{props.cardTitle}</div>
+                            <div className="simple-card-body">{props.children}</div>
+                        </div>
+                    )
+                    : (
+                        <div
+                            className={`simple-card-container ${props.extraClassName}`}
+                            id={props.cardID}
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: '100%',
+                                gridTemplateRows: 'auto 3em',
+                                height: '100%',
+                                marginBottom: '3%'
+                            }}
+                        >
+                            <div className="simple-card-body">{props.children}</div>
+                            <div className="simple-card-title">{props.cardTitle}</div>
+                        </div>
+                    )
+            }
         </>
     );
 }
