d527dbaddfe07236bb92770a48c4565f259b20f6
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-28 18:56:57 +0200

    updated logModel to download logged files from mlflow

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/rri/LogModel.py
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/rri/LogModel.py
@@ -1,14 +1,19 @@
 
 import sys
+import os
 import mlflow
 import mlflow.spark
 from mlflow.tracking import MlflowClient
 
-import pyspark
+#import pyspark
+#from pyspark.sql import SparkSession
 from pyspark.sql import SparkSession
+import pyspark
+
 
 
 spark = SparkSession.builder.appName("logModel").getOrCreate()
+#sc = spark.sparkContext
 
 
 #0 = name
@@ -16,19 +21,23 @@ path = sys.argv[1]
 name = sys.argv[2]
 runId = sys.argv[3]
 
+print(spark.version)
 print("path: " + path)
 
 mlflow.set_tracking_uri("http://localhost:5000")
+client = MlflowClient()
 
 if (name == "LogisticRegressionModel"):
-    from pyspark.ml.tuning import TrainValidationSplitModel
+    #from pyspark.ml.tuning import TrainValidationSplitModel
+    from pyspark.ml.pipeline import PipelineModel
     print("here sys")
 
-    File = client.download_artifacts(runId, path) #"features"
-    unflavouredModel = TrainValidationSplitModel.load(File)
+    File = client.download_artifacts(runId, name, path)
+    #unflavouredModel = TrainValidationSplitModel.load(File)
+    unflavouredModel = PipelineModel.load(File)
     print("here sys")
 
-    mlflow.spark.log_model(unflavouredModel, "LogisticRegressionModel")
+    mlflow.spark.log_model(unflavouredModel, path)
     print("here sys")
 
 if (name == "DecisionTreeModel"):
