78c336406915091ec2d5cf0ed28ab42931d62542
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-28 19:21:32 +0200

    implemeted setup for report_service class

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/service/ReportServiceImpl.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/service/ReportServiceImpl.java
@@ -1,8 +1,14 @@
 package com.Report_Service.Report_Service.service;
 
+import com.Report_Service.Report_Service.request.ReportDataRequest;
+import com.Report_Service.Report_Service.response.ReportDataResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportServiceImpl {
-//work here
+
+    public ReportDataResponse reportData(ReportDataRequest request){
+
+        return new ReportDataResponse();
+    }
 }
