6e89422978662177676f8d267bbbb45309853d25
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-29 23:37:33 +0200

    updated GenerateReportPDF Response object

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/response/GenerateReportPDFResponse.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/response/GenerateReportPDFResponse.java
@@ -1,4 +1,12 @@
 package com.Report_Service.Report_Service.response;
 
 public class GenerateReportPDFResponse {
+    public byte[] pdf;
+    public GenerateReportPDFResponse(byte[] pdf){
+        this.pdf = pdf;
+    }
+
+    public byte[] getPdf() {
+        return pdf;
+    }
 }
