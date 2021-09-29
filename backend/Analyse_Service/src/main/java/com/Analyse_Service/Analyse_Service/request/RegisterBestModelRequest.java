132c03a4f8d80d90d264ef57261aa1416cbfd996
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-29 09:32:01 +0200

    implemented register best model response & request

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/RegisterBestModelRequest.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/RegisterBestModelRequest.java
@@ -1,4 +1,18 @@
 package com.Analyse_Service.Analyse_Service.request;
 
+import com.Analyse_Service.Analyse_Service.dataclass.TrainedModel;
+
+import java.util.ArrayList;
+
 public class RegisterBestModelRequest {
+
+    private ArrayList<TrainedModel> modelList = new ArrayList<>();
+
+    public RegisterBestModelRequest(ArrayList<TrainedModel> modelList){
+        this.modelList = modelList;
+    }
+
+    public ArrayList<TrainedModel> getModelList(){
+        return modelList;
+    }
 }
--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/RegisterBestModelResponse.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/RegisterBestModelResponse.java
@@ -1,4 +1,17 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import com.Analyse_Service.Analyse_Service.dataclass.TrainedModel;
+
 public class RegisterBestModelResponse {
+
+    TrainedModel bestModel;
+
+    public RegisterBestModelResponse(TrainedModel bestModel){
+        this.bestModel = bestModel;
+    }
+
+    public TrainedModel getBestModel(){
+        return bestModel;
+    }
+
 }
