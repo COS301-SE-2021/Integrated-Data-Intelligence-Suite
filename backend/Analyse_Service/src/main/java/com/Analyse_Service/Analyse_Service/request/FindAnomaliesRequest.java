636805250878d5df9dae26f3eb2ada36cdbace0c
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-29 11:50:15 +0200

    updated request objects of analysis service

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/FindAnomaliesRequest.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/request/FindAnomaliesRequest.java
@@ -5,11 +5,23 @@ import java.util.ArrayList;
 public class FindAnomaliesRequest {
     ArrayList<ArrayList> dataList;
 
+    String modelId;
+
     public FindAnomaliesRequest(ArrayList<ArrayList> dataList){
         this.dataList = dataList;
+        this.modelId = null;
+    }
+
+    public FindAnomaliesRequest(ArrayList<ArrayList> dataList, String modelId){
+        this.dataList = dataList;
+        this.modelId = modelId;
     }
 
     public ArrayList<ArrayList> getDataList(){
         return dataList;
+    }
+
+    public String getModelId(){
+        return modelId;
     }
 }
