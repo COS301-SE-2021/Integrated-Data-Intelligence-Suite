ce735cf4c7b8f72f6e1a305bcf22ee12b6a4c343
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-29 23:40:15 +0200

    updated report dataclass

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/dataclass/Report.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/dataclass/Report.java
@@ -3,6 +3,9 @@ package com.Report_Service.Report_Service.dataclass;
 import java.util.ArrayList;
 
 public class Report {
+    public String Date;
+    public String time;
+
     public String TrendSummary = "";
     public ArrayList<ArrayList> TrendData;
 
