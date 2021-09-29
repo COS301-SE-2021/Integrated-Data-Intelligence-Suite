648191c4c2f5f7dda86d9745e60ac71ee6dc5303
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-30 01:44:37 +0200

    updated ReportData Request object

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/request/ReportDataRequest.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/request/ReportDataRequest.java
@@ -1,7 +1,25 @@
 package com.Report_Service.Report_Service.request;
 
+import java.util.ArrayList;
+
 public class ReportDataRequest {
-    public ReportDataRequest(){
+    ArrayList<ArrayList> trendlist;
+    ArrayList<ArrayList> Realationshiplist;
+    ArrayList<ArrayList> Patternlist;
+    ArrayList<String> Anomalylist;
+    ArrayList<ArrayList> wordlist;
+
+    public ReportDataRequest(ArrayList<ArrayList> trendlist,
+            ArrayList<ArrayList> Realationshiplist,
+            ArrayList<ArrayList> Patternlist,
+            ArrayList<String> Anomalylist,
+            ArrayList<ArrayList> wordlist){
+
+        this.trendlist = trendlist;
+        this.Realationshiplist= Realationshiplist;
+        this.Patternlist= Patternlist;
+        this.Anomalylist= Anomalylist;
+        this.wordlist = wordlist;
 
     }
 }
