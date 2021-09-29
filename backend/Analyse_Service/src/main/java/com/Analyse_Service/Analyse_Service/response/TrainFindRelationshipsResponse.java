9048fefeb583e18bcab0ee969d70f09743ee5c8e
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-29 10:06:24 +0200

    updated response objects for training

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindRelationshipsResponse.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindRelationshipsResponse.java
@@ -1,15 +1,28 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import com.Analyse_Service.Analyse_Service.dataclass.TrainedModel;
+
 import java.util.ArrayList;
 
 public class TrainFindRelationshipsResponse {
     ArrayList<ArrayList> pattenList;
 
+    TrainedModel trainedModel;
+
     public TrainFindRelationshipsResponse(ArrayList<ArrayList> pattenList){
         this.pattenList = pattenList;
     }
 
+    public TrainFindRelationshipsResponse(ArrayList<ArrayList> pattenList, TrainedModel trainedModel){
+        this.pattenList = pattenList;
+        this.trainedModel = trainedModel;
+    }
+
     public ArrayList<ArrayList> getPattenList(){
         return this.pattenList;
+    }
+
+    public TrainedModel getTrainedModel(){
+        return trainedModel;
     }
 }
--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindTrendsResponse.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/response/TrainFindTrendsResponse.java
@@ -1,15 +1,30 @@
 package com.Analyse_Service.Analyse_Service.response;
 
+import com.Analyse_Service.Analyse_Service.dataclass.TrainedModel;
+
 import java.util.ArrayList;
 
 public class TrainFindTrendsResponse {
     ArrayList<ArrayList> pattenList;
 
+    TrainedModel trainedModel;
+
+
     public TrainFindTrendsResponse(ArrayList<ArrayList> pattenList){
         this.pattenList = pattenList;
+    }
+
+    public TrainFindTrendsResponse(ArrayList<ArrayList> pattenList, TrainedModel trainedModel){
+        this.pattenList = pattenList;
+        this.trainedModel = trainedModel;
     }
 
     public ArrayList<ArrayList> getPattenList(){
         return this.pattenList;
     }
+
+    public TrainedModel getTrainedModel(){
+        return trainedModel;
+    }
+
 }
