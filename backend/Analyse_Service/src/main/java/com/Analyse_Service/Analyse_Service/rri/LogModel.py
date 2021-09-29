4190207dcbde8597dde487f53088e93c49991c74
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-09-29 06:54:22 +0200

    finalised Log model functionality

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/rri/LogModel.py
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/rri/LogModel.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -10,47 +11,23 @@ from mlflow.tracking import MlflowClient
 from pyspark.sql import SparkSession
 import pyspark
 
-
-
 spark = SparkSession.builder.appName("logModel").getOrCreate()
-#sc = spark.sparkContext
+sc = spark.sparkContext
 
-
-#0 = name
 path = sys.argv[1]
 name = sys.argv[2]
 runId = sys.argv[3]
 
-print(spark.version)
-print("path: " + path)
-
 mlflow.set_tracking_uri("http://localhost:5000")
 client = MlflowClient()
 
-if (name == "LogisticRegressionModel"):
-    #from pyspark.ml.tuning import TrainValidationSplitModel
-    from pyspark.ml.pipeline import PipelineModel
-    print("here sys")
+#from pyspark.ml.tuning import TrainValidationSplitModel
+from pyspark.ml.pipeline import PipelineModel, Pipeline
+File = client.download_artifacts(runId, name, path)
+unflavouredModel = PipelineModel.read().load(File)
+mlflow.spark.log_model(unflavouredModel, name)
 
-    File = client.download_artifacts(runId, name, path)
-    #unflavouredModel = TrainValidationSplitModel.load(File)
-    unflavouredModel = PipelineModel.load(File)
-    print("here sys")
 
-    mlflow.spark.log_model(unflavouredModel, path)
-    print("here sys")
 
-if (name == "DecisionTreeModel"):
-    from pyspark.ml.tuning import TrainValidationSplitModel
-    File = client.download_artifacts(runId, path) #"features"
-    unflavouredModel = TrainValidationSplitModel.load(path)
-    mlflow.spark.log_model(unflavouredModel, "DecisionTreeModel")
 
-if(name == "KMeansModel"):
-    from pyspark.ml.pipeline import PipelineModel, Pipeline
-    File = client.download_artifacts(runId, path) #"features"
-    unflavouredModel = PipelineModel.read().load(File)
-    mlflow.spark.log_model(unflavouredModel, "KMeansModel")
 
-#elif
-#else:
