576e5095dec13f0e167d305d9f455a82bf305bca
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-29 20:15:32 +0200

    created GetMostProminentSentiment function in Report Service

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/service/ReportServiceImpl.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/service/ReportServiceImpl.java
@@ -6,6 +6,8 @@ import com.Report_Service.Report_Service.request.*;
 import com.Report_Service.Report_Service.response.*;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class ReportServiceImpl {
 
@@ -27,9 +29,72 @@ public class ReportServiceImpl {
             throw new InvalidRequestException("Data list is null");
         }
 
+        ArrayList<ArrayList> output = new ArrayList<>();
+        ArrayList<ArrayList> reqData = request.getDataList();
+
+        for (ArrayList l: reqData) {
+            String Entity = l.get(0).toString();
+
+        }
+
 
 
         return new GetTrendAnalysisDataResponse(null,null);
+    }
+
+    public GetMostProminentSentimentResponse getMostProminentSentiment(GetMostProminentSentimentRequest request) throws InvalidRequestException {
+        if (request == null) {
+            throw new InvalidRequestException("CreateTimelineGraphRequest Object is null");
+        }
+        if (request.getDataList() == null) {
+            throw new InvalidRequestException("Arraylist is null");
+        }
+        String outputs = "";
+        ArrayList<ArrayList> reqData = request.getDataList();
+
+        int k = 0;
+        ArrayList<String> listSent = new ArrayList<>();
+        ArrayList<ArrayList> out = new ArrayList<>();
+        for (int i = 0; i < reqData.size(); i++) {
+            ArrayList<String> sents = (ArrayList<String>) reqData.get(i).get(4);
+            //System.out.println(locs.toString());
+
+            for (int j = 0; j < sents.size(); j++) {
+                if (listSent.isEmpty()){
+                    listSent.add(sents.get(j));
+                    ArrayList<Object> r = new ArrayList<>();
+                    r.add(sents.get(j));
+                    r.add(1);
+                    out.add(r);
+                }else {
+                    if (listSent.contains(sents.get(j))){
+                        ArrayList<Object>r =  out.get(listSent.indexOf(sents.get(j)));
+                        int val=Integer.parseInt(r.get(1).toString());
+                        val++;
+                        r.set(1,val);
+                        out.set(listSent.indexOf(sents.get(j)),r);
+                    }else {
+                        listSent.add(sents.get(j));
+                        ArrayList<Object> r = new ArrayList<>();
+                        r.add(sents.get(j));
+                        r.add(1);
+                        out.add(r);
+                    }
+                }
+            }
+        }
+
+
+        outputs = out.get(0).get(0).toString();
+        int temp = Integer.parseInt(out.get(0).get(1).toString());
+        for (ArrayList o : out) {
+            //System.out.println(o);
+            if (Integer.parseInt(o.get(1).toString()) > temp){
+                outputs = o.get(0).toString();
+                temp = Integer.parseInt(o.get(1).toString());
+            }
+        }
+        return new GetMostProminentSentimentResponse(outputs);
     }
 
     public GetPatternAndRelationshipDataResponse getPatternAndRelationshipData(GetPatternAndRelationshipDataRequest request) throws InvalidRequestException {
