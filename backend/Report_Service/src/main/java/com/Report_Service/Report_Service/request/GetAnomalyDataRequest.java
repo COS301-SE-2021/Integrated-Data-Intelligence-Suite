70a0425dbdd59fe8a5eb7dbc9818572784dda9a8
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-29 22:07:04 +0200

    updated GetAnomalyData request object

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/request/GetAnomalyDataRequest.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/request/GetAnomalyDataRequest.java
@@ -3,13 +3,13 @@ package com.Report_Service.Report_Service.request;
 import java.util.ArrayList;
 
 public class GetAnomalyDataRequest {
-    public ArrayList<ArrayList> dataList;
+    public ArrayList<String> dataList;
 
-    public GetAnomalyDataRequest(ArrayList<ArrayList> dataList){
+    public GetAnomalyDataRequest(ArrayList<String> dataList){
         this.dataList = dataList;
     }
 
-    public ArrayList<ArrayList> getDataList(){
+    public ArrayList<String> getDataList(){
         return dataList;
     }
 }
