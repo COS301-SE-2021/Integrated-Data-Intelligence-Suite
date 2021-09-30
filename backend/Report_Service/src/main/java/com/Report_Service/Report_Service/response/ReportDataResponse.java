19936c676e2feb24a073917feba110d88c2875a3
Steve Mbuguje <u18008390@tuks.co.za>
2021-09-30 23:00:11 +0200

    updated report data response object

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/response/ReportDataResponse.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/response/ReportDataResponse.java
@@ -1,7 +1,12 @@
 package com.Report_Service.Report_Service.response;
 
 public class ReportDataResponse {
-    public ReportDataResponse(){
+    byte[] pdf;
+    public ReportDataResponse(byte[] pdf){
+        this.pdf = pdf;
+    }
 
+    public byte[] getPdf() {
+        return pdf;
     }
 }
