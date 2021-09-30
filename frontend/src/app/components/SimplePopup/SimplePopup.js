31c693c8de9f63a92435d0f40b3f03ccdeb6f066
Myron Lopes <u18246932@tuks.co.za>
2021-09-30 12:45:53 +0200

    added popup component

---
+++ frontend/src/app/components/SimplePopup/SimplePopup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SimplePopup.css';
+import CustomDivider from '../CustomDivider/CustomDivider';
+
+export default class SimplePopup extends React.Component {
+    handlePopup = () => {
+        this.props.closePopup();
+    };
+
+    render() {
+        return (
+            <>
+                <div className={'popup-container'}>
+                    <div className={'popup-div'}>
+                        <div className={'popup-title-bar'}>
+                            <div className={'popup-title'}>
+                                Some Title
+                            </div>
+                            <button
+                                onClick={this.handlePopup}
+                                className={'popup-close-btn'}
+                            >
+                                X
+                            </button>
+                        </div>
+                        <div className={'popup-body'}>
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
