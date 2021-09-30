84517c6aa7f09694ddb9fb6ca8f21711241b60e1
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-01 01:18:39 +0200

    added getting report functionality to report controller service

--- backend/Report_Service/src/main/java/com/Report_Service/Report_Service/controller/ReportServiceController.java
+++ backend/Report_Service/src/main/java/com/Report_Service/Report_Service/controller/ReportServiceController.java
@@ -2,7 +2,9 @@ package com.Report_Service.Report_Service.controller;
 
 import com.Report_Service.Report_Service.exception.InvalidRequestException;
 import com.Report_Service.Report_Service.exception.ReporterException;
+import com.Report_Service.Report_Service.request.GetReportDataRequest;
 import com.Report_Service.Report_Service.request.ReportDataRequest;
+import com.Report_Service.Report_Service.response.GetReportDataResponse;
 import com.Report_Service.Report_Service.response.ReportDataResponse;
 import com.Report_Service.Report_Service.service.ReportServiceImpl;
 import com.itextpdf.text.DocumentException;
@@ -26,12 +28,33 @@ public class ReportServiceController {
      */
     @PostMapping("/visualizeData")
     public @ResponseBody
-    ReportDataResponse visualizeData(@RequestBody ReportDataRequest request) throws ReporterException, DocumentException, IOException {
+    ReportDataResponse reportData(@RequestBody ReportDataRequest request) throws ReporterException, DocumentException, IOException {
         //VisualizeDataRequest request = requestEntity.getBody();
         if (request == null) {
-            throw new InvalidRequestException("FindEntitiesRequest Object is null");
+            throw new InvalidRequestException("reportData Request Object is null");
         }
 
         return service.reportData(request);
+    }
+
+    /**
+     * This method is used to facilitate communication to the Report-Service.
+     * @param request This is a request entity which contains a ReportDataRequest object.
+     * @return AnalyseDataResponse This object contains analysed data which has been processed by Report-Service.
+     * @throws Exception This is thrown if exception caught in Report-Service.
+     */
+    @PostMapping("/visualizeData")
+    public @ResponseBody
+    GetReportDataResponse getReportData(@RequestBody GetReportDataRequest request) throws ReporterException, DocumentException, IOException {
+        //VisualizeDataRequest request = requestEntity.getBody();
+        if (request == null) {
+            throw new InvalidRequestException("getReportData Request Object is null");
+        }
+
+        if (request.getReportId() == null) {
+            throw new InvalidRequestException("getReportData Request ID is null");
+        }
+
+        return service.getReportData(request);
     }
 }
