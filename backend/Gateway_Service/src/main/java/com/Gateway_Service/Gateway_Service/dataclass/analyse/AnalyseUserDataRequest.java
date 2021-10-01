27998d6f7310565ddde36fd32f57cedee9d6d0e8
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-01 06:15:11 +0200

    implemented analyse service object classes to gateway service

--- backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/analyse/AnalyseUserDataRequest.java
+++ backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/analyse/AnalyseUserDataRequest.java
@@ -1,4 +1,29 @@
 package com.Gateway_Service.Gateway_Service.dataclass.analyse;
 
+import com.Analyse_Service.Analyse_Service.dataclass.ParsedData;
+
+import java.util.ArrayList;
+
 public class AnalyseUserDataRequest {
+    ArrayList<ParsedData> dataList;
+
+    String modelId;
+
+    public AnalyseUserDataRequest(){
+
+    }
+
+    public AnalyseUserDataRequest(ArrayList<ParsedData> dataList, String modelId){
+        this.dataList = dataList;
+        this.modelId = modelId;
+    }
+
+
+    public ArrayList<ParsedData> getDataList(){
+        return this.dataList;
+    }
+
+    public String getModelId(){
+        return modelId;
+    }
 }
--- backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/analyse/TrainUserModelRequest.java
+++ backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/analyse/TrainUserModelRequest.java
@@ -1,4 +1,27 @@
 package com.Gateway_Service.Gateway_Service.dataclass.analyse;
 
+import com.Analyse_Service.Analyse_Service.dataclass.ParsedData;
+
+import java.util.ArrayList;
+
 public class TrainUserModelRequest {
+    private String modelName;
+
+    ArrayList<ParsedData> dataList;
+
+    public TrainUserModelRequest(){
+
+    }
+
+    public TrainUserModelRequest(String modelName){
+        this.modelName = modelName;
+    }
+
+    public String getModelName(){
+        return modelName;
+    }
+
+    public ArrayList<ParsedData> getDataList(){
+        return this.dataList;
+    }
 }
