3a57162a5c0b2422c8484212eb51ef9e87f63e5d
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-01 06:16:50 +0200

    implemented some report service object classes to gateway service

--- backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/report/DeleteReportDataByIdResponse.java
+++ backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/report/DeleteReportDataByIdResponse.java
@@ -1,4 +1,36 @@
 package com.Gateway_Service.Gateway_Service.dataclass.report;
 
 public class DeleteReportDataByIdResponse {
+    boolean deleted;
+
+    boolean fallback = false;
+    String fallbackMessage = "";
+
+    public DeleteReportDataByIdResponse(){
+
+    }
+
+    public DeleteReportDataByIdResponse(boolean deleted){
+        this.deleted = deleted;
+    }
+
+    public boolean getDeleted() {
+        return deleted;
+    }
+
+
+    public void setFallback(boolean fallback){
+        this.fallback = fallback;
+    }
+    public void setFallbackMessage(String fallbackMessage){
+        this.fallbackMessage = fallbackMessage;
+    }
+
+    public boolean getFallback(){
+        return this.fallback;
+    }
+
+    public String getFallbackMessage(){
+        return this.fallbackMessage;
+    }
 }
--- backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/report/GetReportDataByIdRequest.java
+++ backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/dataclass/report/GetReportDataByIdRequest.java
@@ -1,4 +1,19 @@
 package com.Gateway_Service.Gateway_Service.dataclass.report;
 
+import java.util.UUID;
+
 public class GetReportDataByIdRequest {
+    UUID reportId;
+
+    public GetReportDataByIdRequest(){
+
+    }
+
+    public GetReportDataByIdRequest(UUID reportId){
+        this.reportId= reportId;
+    }
+
+    public UUID getReportId(){
+        return reportId;
+    }
 }
