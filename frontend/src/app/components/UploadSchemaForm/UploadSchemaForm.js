120ab631052d2845453221015141c9f5fa1b833d
Myron Lopes <u18246932@tuks.co.za>
2021-10-01 04:20:10 +0200

    updated name and values of radio buttons

--- frontend/src/app/components/UploadSchemaForm/UploadSchemaForm.js
+++ frontend/src/app/components/UploadSchemaForm/UploadSchemaForm.js
@@ -24,23 +24,26 @@ export default class UploadSchemaForm extends React.Component {
             <>
                 <SimpleCard
                     cardID={'upload-schema-form-card'}
-                    cardTitle={'Step 2: xxx'}
+                    cardTitle={''}
                     titleOnTop
                 >
                     <div id={'upload-schema-form-container'}>
                         <div id={'upload-radio-btn-container'}>
+                            <div id={'upload-radio-btn-heading'}>
+                                Choose the type of data your uploading:
+                            </div>
                             <input
                                 type="radio"
-                                value="social-media"
-                                name="upload-type"
+                                value="social-media-type"
+                                name="upload-type-radio-btn"
                                 id={'social-media-radio-btn'}
                                 onClick={() => this.setSocialMediaChecked(true)}
                             />
                             Social Media
                             <input
                                 type="radio"
-                                value="news"
-                                name="upload-type"
+                                value="news-type"
+                                name="upload-type-radio-btn"
                                 id={'news-radio-btn'}
                                 onClick={() => this.setSocialMediaChecked(false)}
                             />
@@ -53,19 +56,23 @@ export default class UploadSchemaForm extends React.Component {
                                     <p>social media is checked</p>
                                     <InputBoxWithLabel
                                         inputLabel={'date'}
-
+                                        inputName={'schema-edit-box'}
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'interactions'}
+                                        inputName={'schema-edit-box'}
+
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'text'}
+                                        inputName={'schema-edit-box'}
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'location'}
+                                        inputName={'schema-edit-box'}
                                     />
                                 </div>
                             ) : (
@@ -73,18 +80,22 @@ export default class UploadSchemaForm extends React.Component {
                                     <p>news is checked</p>
                                     <InputBoxWithLabel
                                         inputLabel={'date'}
+                                        inputName={'schema-edit-box'}
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'content'}
+                                        inputName={'schema-edit-box'}
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'title'}
+                                        inputName={'schema-edit-box'}
                                     />
 
                                     <InputBoxWithLabel
                                         inputLabel={'description'}
+                                        inputName={'schema-edit-box'}
                                     />
                                 </div>
                             )
