f39051564d659b6d55771ffadc0c819720c0cbdb
Myron Lopes <u18246932@tuks.co.za>
2021-10-02 22:12:34 +0200

    added delete model popup to model cards

--- frontend/src/app/components/ModelCard/ModelCard.js
+++ frontend/src/app/components/ModelCard/ModelCard.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ModelCard.css';
 import SimpleCard from '../SimpleCard/SimpleCard';
 import { AiOutlineShareAlt, MdDelete } from 'react-icons/all';
-import { Tooltip } from '@mui/material';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import SimplePopup from '../SimplePopup/SimplePopup';
+import { useRecoilState } from 'recoil';
+import { isShowingDeletePopupState } from '../../assets/AtomStore/AtomStore';
 
 export default function ModelCard(props) {
+    const [isShowingDeletePopup, toggleDeletePopup] = useRecoilState(isShowingDeletePopupState);
 
     return (
         <>
@@ -12,29 +16,32 @@ export default function ModelCard(props) {
                 <div className={'model-card-title-bar'}>
                     <div id={'model-card-default-tag'}>Default</div>
                     <div id={'model-card-btn-container'}>
-
                         <Tooltip
                             title="Delete Model"
                             arrow
                             className={'simple-tooltip'}
-                            sx={{
-                                backgroundColor: 'black',
-                                color: 'white'
-                            }}
+                            placement="top"
                         >
-                            <button className={'model-card-delete-btn model-card-btn'}>
+                            <button
+                                className={'model-card-delete-btn model-card-btn'}
+                                onClick={() => toggleDeletePopup(true)}
+                            >
                                 <MdDelete className={'model-card-delete-icon model-card-icon'}/>
                             </button>
                         </Tooltip>
 
-                        <Tooltip title="Share Model" arrow className={'simple-tooltip'}>
+                        <Tooltip
+                            title="Share Model"
+                            arrow
+                            className={'simple-tooltip'}
+                            placement="top"
+                        >
                             <button className={'model-card-share-btn model-card-btn'}>
                                 <AiOutlineShareAlt
                                     className={'model-card-share-icon model-card-icon'}
                                 />
                             </button>
                         </Tooltip>
-
                         <input type={'radio'}/>
                     </div>
                 </div>
