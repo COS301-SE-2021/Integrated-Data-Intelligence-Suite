6c3dba5be185fe1a80e9cd2dc1213ee0a1756c87
abDivergent <u15219900@tuks.co.za>
2021-10-03 00:30:32 +0200

    added delete model popup context [ needs integration ]

--- frontend/src/app/pages/ManageModelsPage/ManageModelsPage.js
+++ frontend/src/app/pages/ManageModelsPage/ManageModelsPage.js
@@ -1,43 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Switch from 'react-bootstrap/Switch';
+import { BsCloudUpload, RiAddLine } from 'react-icons/all';
 import SideBar from '../../components/SideBar/SideBar';
 import './ManageModelsPage.css';
 import SimpleCard from '../../components/SimpleCard/SimpleCard';
 import ModelCard from '../../components/ModelCard/ModelCard';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import SimplePopup from '../../components/SimplePopup/SimplePopup';
 import {
     isShowingDeletePopupState,
     isShowingAddTrainingDataPopupState,
-    isShowingAddModelPopupState
+    isShowingAddModelPopupState,
 } from '../../assets/AtomStore/AtomStore';
-import { BsCloudUpload, RiAddLine } from 'react-icons/all';
 import '../../components/SimpleButton/SimpleButton.css';
 
 export default function ManageModelsPage() {
     const [isShowingDeletePopup, toggleDeletePopup] = useRecoilState(isShowingDeletePopupState);
     const [isShowingAddTrainingDataPopup, toggleAddTrainingDataPopup] = useRecoilState(isShowingAddTrainingDataPopupState);
-    const [isShowingAddModelPopup, toggleAddModelPopup] = useRecoilState(isShowingAddModelPopupState);
+    const toggleAddModelPopup = useSetRecoilState(isShowingAddModelPopupState);
+    const [modelId, setModelId] = useState('');
+
+    const handleAddModel = () =>{
+        // TODO function call to backend to add model to current user's liat of models
+    };
+
     const deletePopupComponent =
         (
             <SimplePopup
-                closePopup={() => toggleDeletePopup(false)}
-                popupTitle={'Delete Model'}
-                popupID={'delete-model-popup'}
+              closePopup={() => toggleDeletePopup(false)}
+              popupTitle="Delete Model"
+              popupID="delete-model-popup"
             >
-                <div id={'delete-model-popup-msg'}>Are you sure you want to delete this modal?</div>
-                {/*<div id={'delete-model-btn-container'}>*/}
-                <button id={'delete-model-popup-btn-yes'}>Yes</button>
-                <button id={'delete-model-popup-btn-no'}>No</button>
-                {/*</div>*/}
+                <div id="delete-model-popup-msg">Are you sure you want to delete this modal?</div>
+                {/* <div id={'delete-model-btn-container'}> */}
+                <button id="delete-model-popup-btn-yes">Yes</button>
+                <button id="delete-model-popup-btn-no">No</button>
+                {/* </div> */}
             </SimplePopup>
         );
 
     const addTrainingDataPopupComponent = (
         <SimplePopup
-            closePopup={() => toggleAddTrainingDataPopup(false)}
-            popupTitle={'Upload Training Data'}
+          closePopup={() => toggleAddTrainingDataPopup(false)}
+          popupTitle="Upload Training Data"
         >
             <div>
                 <div>xxxx</div>
@@ -47,11 +53,23 @@ export default function ManageModelsPage() {
 
     const addModelPopupComponent = (
         <SimplePopup
-            closePopup={() => toggleAddModelPopup(false)}
-            popupTitle={'Add Data Model'}
+          closePopup={() => toggleAddModelPopup(false)}
+          popupTitle="Add Data Model"
         >
-            <div>
-                <div>xxxx</div>
+            <div className="add-model-container">
+                <div className="input-container">
+                    <div className="label">ID</div>
+                    <input
+                      type="text"
+                      id="modelIdInput"
+                      placeholder="model id"
+                      value={modelId}
+                      onChange={(event) => setModelId(event.currentTarget.value)}
+                    />
+                </div>
+                <div className="button-container">
+                    <button onClick={()=>handleAddModel}>Add</button>
+                </div>
             </div>
         </SimplePopup>
     );
@@ -77,40 +95,40 @@ export default function ManageModelsPage() {
                             ? addModelPopupComponent
                             : null
                     }
-                    <div id={'manage-models-page-container'}>
-                        <SideBar currentPage={'6'}/>
-                        <div id={'manage-models-page-content'}>
+                    <div id="manage-models-page-container">
+                        <SideBar currentPage="6" />
+                        <div id="manage-models-page-content">
                             <SimpleCard
-                                cardID={'manage-models-card'}
-                                cardTitle={'Manage Your Models'}
-                                titleOnTop
+                              cardID="manage-models-card"
+                              cardTitle="Manage Your Models"
+                              titleOnTop
                             >
-                                <div id={'manage-models-btn-row'}>
+                                <div id="manage-models-btn-row">
                                     <button
-                                        className={'simple-btn simple-btn-hover'}
-                                        onClick={() => toggleAddTrainingDataPopup(true)}
+                                      className="simple-btn simple-btn-hover"
+                                      onClick={() => toggleAddTrainingDataPopup(true)}
                                     >
                                         <BsCloudUpload
-                                            className={'simple-btn-icon simple-btn-hover'}
+                                          className="simple-btn-icon simple-btn-hover"
                                         />
                                         Upload Training Data
                                     </button>
 
                                     <button
-                                        className={'simple-btn simple-btn-hover'}
-                                        onClick={() => toggleAddModelPopup(true)}
+                                      className="simple-btn simple-btn-hover"
+                                      onClick={() => toggleAddModelPopup(true)}
                                     >
-                                        <RiAddLine className={'simple-btn-icon simple-btn-hover'}/>
+                                        <RiAddLine className="simple-btn-icon simple-btn-hover" />
                                         Add Model
                                     </button>
                                 </div>
-                                <div id={'manage-models-card-row'}>
-                                    <ModelCard/>
-                                    <ModelCard/>
-                                    <ModelCard/>
-                                    <ModelCard/>
-                                    <ModelCard/>
-                                    <ModelCard/>
+                                <div id="manage-models-card-row">
+                                    <ModelCard />
+                                    <ModelCard />
+                                    <ModelCard />
+                                    <ModelCard />
+                                    <ModelCard />
+                                    <ModelCard />
                                 </div>
                             </SimpleCard>
                         </div>
