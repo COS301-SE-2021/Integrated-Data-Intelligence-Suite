e4f31c489c3a7e518d086eb6286c565dc66766ed
abDivergent <u15219900@tuks.co.za>
2021-10-03 15:16:09 +0200

    changed preview page to correlate to  report page

--- frontend/src/app/components/ReportPreview/ReportPreview.js
+++ frontend/src/app/components/ReportPreview/ReportPreview.js
@@ -1,13 +1,46 @@
 import React, { PureComponent } from 'react';
 import { GrClose } from 'react-icons/all';
+import {data} from "browserslist";
 
 class ReportPreview extends PureComponent {
+    componentDidMount() {
+        if (this.props.currentFile.reportID) {
+            const abortCont = new AbortController();
+
+            const requestObj = {
+                reportID: this.props.currentFile.reportID,
+                userID: this.props.currentFile.userID,
+            };
+
+            fetch(`${process.env.REACT_APP_BACKEND_HOST}/generateReport`,
+                {
+                    signal: abortCont.signal,
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(requestObj),
+                })
+                .then((res) =>{
+                    if (res.ok) {
+                        throw Error(res.error());
+                    }
+                    return res.json();
+                })
+                .catch((err) => {
+                    if (err.name === 'AbortError') console.log('Fetch Aborted');
+                });
+        }
+    }
+
     render() {
         const {
             closePopup,
             className,
             title,
+            currentFile,
         } = this.props;
+        console.log('pdf is ');
+        console.log({currentFile})
+        // console.log(<iframe title="myframe" src="http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf" />);
         return (
             <div className={`popup-container ${className}`}>
                 <div className="main-preview-container popup-body">
@@ -19,8 +52,9 @@ class ReportPreview extends PureComponent {
                     </div>
                     <div className="children-div loader">
                         <iframe
+                          id="pdf-frame"
                           title="pdf=preview"
-                          src="http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf"
+                          src={`data:application/pdf;base64,${currentFile.data}`}
                           frameBorder="10"
                           scrolling="auto"
                           height="1000vh"
