a02fcd5dc62c91276bd8e00fb900a5b290226eb4
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-10 12:42:51 +0200

    created service error objects for analyse_service

---
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/dataclass/ServiceErrorResponse.java
@@ -0,0 +1,4 @@
+package com.Analyse_Service.Analyse_Service.dataclass;
+
+public class ServiceErrorResponse {
+}
---
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/exception/ExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.Analyse_Service.Analyse_Service.exception;
+
+public class ExceptionHandler {
+}
