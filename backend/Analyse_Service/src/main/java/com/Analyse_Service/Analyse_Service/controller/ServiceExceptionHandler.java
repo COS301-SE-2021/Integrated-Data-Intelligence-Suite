4b8cdfefe1e72debd2539fbb9d4acf62a879303c
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-11 04:24:19 +0200

    updated service exception handler for analyse service

--- backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/controller/ServiceExceptionHandler.java
+++ backend/Analyse_Service/src/main/java/com/Analyse_Service/Analyse_Service/controller/ServiceExceptionHandler.java
@@ -1,7 +1,10 @@
-package com.Analyse_Service.Analyse_Service.exception;
+package com.Analyse_Service.Analyse_Service.controller;
 
 
 import com.Analyse_Service.Analyse_Service.dataclass.ServiceErrorResponse;
+import com.Analyse_Service.Analyse_Service.exception.AnalyserException;
+import com.Analyse_Service.Analyse_Service.request.GetModelByIdRequest;
+import com.Analyse_Service.Analyse_Service.response.GetModelByIdResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,23 +12,26 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @ControllerAdvice
 public class ServiceExceptionHandler {
 
-    @ExceptionHandler({AnalyserException.class})
+    @ExceptionHandler(value = AnalyserException.class)
     ResponseEntity<?> AnalyserExceptionFound(Exception exc, ServletWebRequest request) {
+    //ServiceErrorResponse AnalyserExceptionFound(HttpServletRequest request, Exception exc) {
 
         ServiceErrorResponse serviceErrorResponse = new ServiceErrorResponse();
 
         serviceErrorResponse.setTimeStamp(LocalDateTime.now());
-        serviceErrorResponse.setPathUri(request.getDescription(true));
+        serviceErrorResponse.setPathUri(request.getContextPath());
         serviceErrorResponse.setStatus(HttpStatus.BAD_REQUEST);
         serviceErrorResponse.setErrors(Arrays.asList(exc.getMessage()));
-        exc.printStackTrace();
+        //exc.printStackTrace();
 
-        return new ResponseEntity(serviceErrorResponse, new HttpHeaders(), serviceErrorResponse.getStatus());
+        return new ResponseEntity<>(serviceErrorResponse, new HttpHeaders(), serviceErrorResponse.getStatus());
+        //return serviceErrorResponse;
     }
 }
