e0afc1f32ff8c8f74b353b68248ce20beac849fd
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-11 12:54:13 +0200

    completed ReportService exception handling in gateway

--- backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ReportService.java
+++ backend/Gateway_Service/src/main/java/com/Gateway_Service/Gateway_Service/service/ReportService.java
@@ -1,16 +1,19 @@
 package com.Gateway_Service.Gateway_Service.service;
 
+import com.Gateway_Service.Gateway_Service.dataclass.parse.AddSocialMediaPropertiesResponse;
 import com.Gateway_Service.Gateway_Service.dataclass.parse.ParseImportedDataRequest;
 import com.Gateway_Service.Gateway_Service.dataclass.parse.ParseImportedDataResponse;
 import com.Gateway_Service.Gateway_Service.dataclass.report.*;
+import com.Gateway_Service.Gateway_Service.exception.ParserException;
+import com.Gateway_Service.Gateway_Service.exception.ReporterException;
 import com.Gateway_Service.Gateway_Service.rri.RestTemplateErrorHandler;
+import com.Gateway_Service.Gateway_Service.rri.ServiceErrorResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,7 +30,7 @@ public class ReportService {
      * @return ReportDataResponse This object contains report data returned by Report-Service
      */
     //@HystrixCommand(fallbackMethod = "parseImportedDataFallback")
-    public ReportDataResponse reportData(ReportDataRequest reportRequest) {
+    public ReportDataResponse reportData(ReportDataRequest reportRequest) throws ReporterException {
 
         restTemplate.setErrorHandler(new RestTemplateErrorHandler());
 
@@ -44,9 +47,25 @@ public class ReportService {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        ReportDataResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/reportData", request, ReportDataResponse.class);
+        //ReportDataResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/reportData", request, ReportDataResponse.class);
 
-        return reportResponse;
+        ResponseEntity<?> reportResponse = null;
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/reportData", HttpMethod.POST,request,new ParameterizedTypeReference<ServiceErrorResponse>() {});
+
+        if(reportResponse.getBody().getClass() == ServiceErrorResponse.class ) {
+            ServiceErrorResponse serviceErrorResponse = (ServiceErrorResponse) reportResponse.getBody();
+            if(serviceErrorResponse.getErrors() != null) {
+                String errors = serviceErrorResponse.getErrors().get(0);
+                for(int i=1; i < serviceErrorResponse.getErrors().size(); i++){
+                    errors = "; " + errors;
+                }
+
+                throw new ReporterException(errors);
+            }
+        }
+
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/reportData",HttpMethod.POST,request,new ParameterizedTypeReference<ReportDataResponse>() {});
+        return (ReportDataResponse) reportResponse.getBody();
     }
 
 
@@ -56,7 +75,7 @@ public class ReportService {
      * @return ReportDataResponse This object contains report data returned by Report-Service
      */
     //@HystrixCommand(fallbackMethod = "parseImportedDataFallback")
-    public GetReportDataByIdResponse getReportDataById(GetReportDataByIdRequest reportRequest) {
+    public GetReportDataByIdResponse getReportDataById(GetReportDataByIdRequest reportRequest) throws ReporterException {
 
         restTemplate.setErrorHandler(new RestTemplateErrorHandler());
 
@@ -73,9 +92,25 @@ public class ReportService {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        GetReportDataByIdResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/getReportDataById", request, GetReportDataByIdResponse.class);
+        //GetReportDataByIdResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/getReportDataById", request, GetReportDataByIdResponse.class);
 
-        return reportResponse;
+        ResponseEntity<?> reportResponse = null;
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/getReportDataById", HttpMethod.POST,request,new ParameterizedTypeReference<ServiceErrorResponse>() {});
+
+        if(reportResponse.getBody().getClass() == ServiceErrorResponse.class ) {
+            ServiceErrorResponse serviceErrorResponse = (ServiceErrorResponse) reportResponse.getBody();
+            if(serviceErrorResponse.getErrors() != null) {
+                String errors = serviceErrorResponse.getErrors().get(0);
+                for(int i=1; i < serviceErrorResponse.getErrors().size(); i++){
+                    errors = "; " + errors;
+                }
+
+                throw new ReporterException(errors);
+            }
+        }
+
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/getReportDataById",HttpMethod.POST,request,new ParameterizedTypeReference<GetReportDataByIdResponse>() {});
+        return (GetReportDataByIdResponse) reportResponse.getBody();
     }
 
 
@@ -85,7 +120,7 @@ public class ReportService {
      * @return ReportDataResponse This object contains report data returned by Report-Service
      */
     //@HystrixCommand(fallbackMethod = "parseImportedDataFallback")
-    public DeleteReportDataByIdResponse deleteReportDataById(DeleteReportDataByIdRequest reportRequest) {
+    public DeleteReportDataByIdResponse deleteReportDataById(DeleteReportDataByIdRequest reportRequest) throws ReporterException {
 
         restTemplate.setErrorHandler(new RestTemplateErrorHandler());
 
@@ -102,9 +137,25 @@ public class ReportService {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        DeleteReportDataByIdResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/deleteReportDataById", request, DeleteReportDataByIdResponse.class);
+        //DeleteReportDataByIdResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/deleteReportDataById", request, DeleteReportDataByIdResponse.class);
 
-        return reportResponse;
+        ResponseEntity<?> reportResponse = null;
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/deleteReportDataById", HttpMethod.POST,request,new ParameterizedTypeReference<ServiceErrorResponse>() {});
+
+        if(reportResponse.getBody().getClass() == ServiceErrorResponse.class ) {
+            ServiceErrorResponse serviceErrorResponse = (ServiceErrorResponse) reportResponse.getBody();
+            if(serviceErrorResponse.getErrors() != null) {
+                String errors = serviceErrorResponse.getErrors().get(0);
+                for(int i=1; i < serviceErrorResponse.getErrors().size(); i++){
+                    errors = "; " + errors;
+                }
+
+                throw new ReporterException(errors);
+            }
+        }
+
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/deleteReportDataById",HttpMethod.POST,request,new ParameterizedTypeReference<DeleteReportDataByIdResponse>() {});
+        return (DeleteReportDataByIdResponse) reportResponse.getBody();
     }
 
     /**
@@ -113,7 +164,7 @@ public class ReportService {
      * @return ReportDataResponse This object contains report data returned by Report-Service
      */
     //@HystrixCommand(fallbackMethod = "parseImportedDataFallback")
-    public ShareReportResponse shareReport(ShareReportRequest reportRequest) {
+    public ShareReportResponse shareReport(ShareReportRequest reportRequest) throws ReporterException {
 
         restTemplate.setErrorHandler(new RestTemplateErrorHandler());
 
@@ -130,9 +181,25 @@ public class ReportService {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        ShareReportResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/shareReport", request, ShareReportResponse.class);
+        //ShareReportResponse reportResponse = restTemplate.postForObject("http://Report-Service/Report/shareReport", request, ShareReportResponse.class);
 
-        return reportResponse;
+        ResponseEntity<?> reportResponse = null;
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/shareReport", HttpMethod.POST,request,new ParameterizedTypeReference<ServiceErrorResponse>() {});
+
+        if(reportResponse.getBody().getClass() == ServiceErrorResponse.class ) {
+            ServiceErrorResponse serviceErrorResponse = (ServiceErrorResponse) reportResponse.getBody();
+            if(serviceErrorResponse.getErrors() != null) {
+                String errors = serviceErrorResponse.getErrors().get(0);
+                for(int i=1; i < serviceErrorResponse.getErrors().size(); i++){
+                    errors = "; " + errors;
+                }
+
+                throw new ReporterException(errors);
+            }
+        }
+
+        reportResponse = restTemplate.exchange("http://Report-Service/Report/shareReport",HttpMethod.POST,request,new ParameterizedTypeReference<ShareReportResponse>() {});
+        return (ShareReportResponse) reportResponse.getBody();
     }
 
     /************************************************ FALLBACK ********************************************************/
