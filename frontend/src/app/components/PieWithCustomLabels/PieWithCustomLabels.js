58c7d671ddf3aaad7da1a872df2653b81520b022
abDivergent <u15219900@tuks.co.za>
2021-10-11 19:50:00 +0200

    updated graphs in overview graph section to match new recoil state changes

--- frontend/src/app/components/PieWithCustomLabels/PieWithCustomLabels.js
+++ frontend/src/app/components/PieWithCustomLabels/PieWithCustomLabels.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { VictoryLabel, VictoryPie, VictoryTooltip } from 'victory';
+import { VictoryPie } from 'victory';
 import PieCustomLabel from '../PieCustomLabel/PieCustomLabel';
 
-export default class PieWithCustomLabels extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    _dataToDisplay(data_from_backend) {
-        console.log('pie chart with custom label');
-        console.log(data_from_backend);
-        return data_from_backend;
-    }
-
-    render() {
-        return (
-            <>
-                <VictoryPie
-                    animate={{ duration: 1000 }}
-                    style={{ labels: { fill: 'white' } }}
-                    innerRadius={100}
-                    // labelRadius={120}
-                    // labels={({ datum }) => `#${datum.y}`}
-                    labelComponent={<PieCustomLabel/>}
-                    data={this._dataToDisplay(this.props.text)}
-                    colorScale={['#00b074', 'red']}
-                />
-            </>
-        );
-    }
+export default function PieWithCustomLabels({ text }) {
+    return (
+        <>
+            <VictoryPie
+              animate={{ duration: 1000 }}
+              style={{ labels: { fill: 'white' } }}
+              innerRadius={100}
+              labelComponent={<PieCustomLabel />}
+              data={text}
+              colorScale={['#00b074', 'red']}
+            />
+        </>
+    );
 }
