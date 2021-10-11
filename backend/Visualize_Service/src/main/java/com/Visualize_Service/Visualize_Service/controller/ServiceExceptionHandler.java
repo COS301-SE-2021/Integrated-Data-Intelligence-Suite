8d8b281db0f0f627f3c7f6d9b1c3944068a054f4
u18003517-Rhuli <82459444+u18003517-Rhuli@users.noreply.github.com>
2021-10-11 10:09:38 +0200

    created and implemented service error handler object for visualise service

---
+++ backend/Visualize_Service/src/main/java/com/Visualize_Service/Visualize_Service/controller/ServiceExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.Visualize_Service.Visualize_Service.controller;
+
+import com.Visualize_Service.Visualize_Service.dataclass.ServiceErrorResponse;
+import com.Visualize_Service.Visualize_Service.exception.VisualizerException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@ControllerAdvice
+public class ServiceExceptionHandler {
+    @ExceptionHandler(value = VisualizerException.class)
+    ResponseEntity<?> VisualizerExceptionFound(Exception exc, ServletWebRequest request) {
+        //ServiceErrorResponse AnalyserExceptionFound(HttpServletRequest request, Exception exc) {
+
+        ServiceErrorResponse serviceErrorResponse = new ServiceErrorResponse();
+
+        serviceErrorResponse.setTimeStamp(LocalDateTime.now());
+        serviceErrorResponse.setPathUri(request.getContextPath());
+        serviceErrorResponse.setStatus(HttpStatus.BAD_REQUEST);
+        serviceErrorResponse.setErrors(Arrays.asList(exc.getMessage()));
+        //exc.printStackTrace();
+
+        return new ResponseEntity<>(serviceErrorResponse, new HttpHeaders(), serviceErrorResponse.getStatus());
+        //return serviceErrorResponse;
+    }
+}
